bef16e9e5c76f81a7356e68353a2525559f81860
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-01 20:43:12 -0600

    Enhanced the box component

--- source/modules/common/components/box/box.ts
+++ source/modules/common/components/box/box.ts
@@ -1,10 +1,16 @@
-import styled from 'styled-components';
-import { color, ColorProps, layout, LayoutProps, position, PositionProps, shadow, ShadowProps, space, SpaceProps } from 'styled-system';
+import styled, { CSSProperties } from 'styled-components';
+import { borders, BordersProps, color, ColorProps, layout, LayoutProps, position, PositionProps, shadow, ShadowProps, space, SpaceProps, system } from 'styled-system';
 
-export const Box = styled.div<ColorProps | LayoutProps | PositionProps | ShadowProps | SpaceProps>`
+export const Box = styled.div<{
+  cursor: CSSProperties['cursor'];
+} | BordersProps | ColorProps | LayoutProps | PositionProps | ShadowProps | SpaceProps>`
+  ${borders};
   ${color};
   ${layout};
   ${position};
   ${shadow};
   ${space};
+  ${system({
+    cursor: true
+  })};
 `;
