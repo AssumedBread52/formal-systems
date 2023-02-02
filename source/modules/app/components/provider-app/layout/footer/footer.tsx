d847d8d6d17ffa610e4ac3a643be812248ff6fa2
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-01 20:45:40 -0600

    Footer shows the dependecies used in application

--- source/modules/app/components/provider-app/layout/footer/footer.tsx
+++ source/modules/app/components/provider-app/layout/footer/footer.tsx
@@ -1,9 +1,14 @@
+import packageJson from '#/package.json';
 import { ReactElement } from 'react';
+import { DependencyList } from './dependency-list/dependency-list';
 
 export const Footer = (): ReactElement => {
+  const { dependencies, devDependencies } = packageJson;
+
   return (
     <footer>
-      Footer
+      <DependencyList label='Production Dependencies' list={dependencies} />
+      <DependencyList label='Development Dependencies' list={devDependencies} />
     </footer>
   );
 };
