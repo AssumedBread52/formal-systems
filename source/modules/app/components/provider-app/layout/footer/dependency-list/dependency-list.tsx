ca46baa64a81f5bf6e675c0edb2bfd790425863c
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-01 20:45:24 -0600

    Added a component to display a dependency list

---
+++ source/modules/app/components/provider-app/layout/footer/dependency-list/dependency-list.tsx
@@ -0,0 +1,24 @@
+import { DependencyListProps } from '@/app/types';
+import { Flex } from '@/common/components/flex/flex';
+import { Grid } from '@/common/components/grid/grid';
+import { ReactElement } from 'react';
+import { DependencyItem } from './dependency-item/dependency-item';
+
+export const DependencyList = (props: DependencyListProps): ReactElement => {
+  const { label, list } = props;
+
+  return (
+    <Grid display='grid' gridGap='0.5rem' gridTemplateColumns='6rem auto' gridTemplateRows='auto' p='1rem'>
+      <Flex display='flex' alignItems='center' justifyContent='center'>
+        {label}
+      </Flex>
+      <Grid display='grid' gridGap='1rem' gridTemplateColumns='repeat(auto-fill, minmax(9rem, 1fr))'>
+        {Object.keys(list).map((key: string): ReactElement => {
+          return (
+            <DependencyItem key={key} packageName={key} version={list[key]} />
+          );
+        })}
+      </Grid>
+    </Grid>
+  );
+};
