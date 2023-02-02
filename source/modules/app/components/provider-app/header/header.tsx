d8de5f748fa1b20b3bd98e1404fff2f2725fc07d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-01 18:10:49 -0600

    Rework header

--- source/modules/app/components/provider-app/header/header.tsx
+++ source/modules/app/components/provider-app/header/header.tsx
@@ -1,9 +1,26 @@
+import { Flex } from '#/modules/common/components/flex/flex';
+import { useIsDocumentScrolled } from '@/app/hooks';
+import { Box } from '@/common/components/box/box';
 import { ReactElement } from 'react';
 
 export const Header = (): ReactElement => {
+  const isDocumentScrolled = useIsDocumentScrolled();
+
   return (
-    <header>
-      Header
-    </header>
+    <Box backgroundColor='white' position='sticky' top='0' left='0' width='100%' boxShadow={isDocumentScrolled ? '0px 4px 8px 0px #757d8829' : '0px 1px 2px 0px #757d8829'}>
+      <header>
+        <Flex alignItems='center' display='flex' px='2'>
+          <h2>
+            Formal Systems
+          </h2>
+          <Box mx='auto' />
+          <a>Link to Info</a>
+          <Box mx='0.5rem' />
+          <a>Link to Sign In/Sign Out</a>
+          <Box mx='0.5rem' />
+          <a>Link to Sign Up/Edit Profile</a>
+        </Flex>
+      </header>
+    </Box>
   );
 };
