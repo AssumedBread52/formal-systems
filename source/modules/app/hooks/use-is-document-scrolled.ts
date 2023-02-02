54466f5b7acfb14b11a718c2f7af1d528629ac16
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-01 18:33:37 -0600

    Switch to object destructuring

--- source/modules/app/hooks/use-is-document-scrolled.ts
+++ source/modules/app/hooks/use-is-document-scrolled.ts
@@ -4,14 +4,18 @@ export const useIsDocumentScrolled = (): boolean => {
   const [isDocumentScrolled, setIsDocumentScrolled] = useState<boolean>(false);
 
   useEffect((): (() => void) => {
+    const { addEventListener, removeEventListener } = window;
+
     const update = (): void => {
-      setIsDocumentScrolled(window.scrollY > 0);
+      const { scrollY } = window;
+
+      setIsDocumentScrolled(scrollY > 0);
     };
 
-    window.addEventListener('scroll', update);
+    addEventListener('scroll', update);
 
     return (): void => {
-      window.removeEventListener('scroll', update);
+      removeEventListener('scroll', update);
     };
   }, []);
 
