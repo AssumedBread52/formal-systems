ff9983813c899741116a9ce9a42fdd4ab7ae9e55
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-02 20:07:15 -0600

    Add routing and clean up header

--- source/modules/app/components/provider-app/layout/header/header.tsx
+++ source/modules/app/components/provider-app/layout/header/header.tsx
@@ -2,18 +2,32 @@ import { Flex } from '#/modules/common/components/flex/flex';
 import { useIsDocumentScrolled } from '@/app/hooks';
 import { Box } from '@/common/components/box/box';
 import Link from 'next/link';
-import { ReactElement } from 'react';
+import { useRouter } from 'next/router';
+import { MouseEvent, ReactElement } from 'react';
 
 export const Header = (): ReactElement => {
+  const router = useRouter();
+
   const isDocumentScrolled = useIsDocumentScrolled();
 
+  const clickHandler = (event: MouseEvent<HTMLDivElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    router.push('/');
+  };
+
+  const boxShadow = isDocumentScrolled ? '0px 4px 8px 0px rgb(128 128 128 / 25%)' : '0px 1px 2px 0px rgb(128 128 128 / 25%)';
+
   return (
-    <Box backgroundColor='white' position='sticky' top='0' left='0' width='100%' boxShadow={isDocumentScrolled ? '0px 4px 8px 0px #757d8829' : '0px 1px 2px 0px #757d8829'}>
+    <Box backgroundColor='white' position='sticky' top='0' left='0' width='100%' boxShadow={boxShadow} zIndex={1}>
       <header>
         <Flex alignItems='center' display='flex' px='2'>
-          <h2>
-            Formal Systems
-          </h2>
+          <Box cursor='pointer' onClick={clickHandler}>
+            <h2>
+              Formal Systems
+            </h2>
+          </Box>
           <Box mx='auto' />
           <Link href='/info'>
             Info
