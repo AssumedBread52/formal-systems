b2d0079bd5540681965b54097aefe693bdb4ffd4
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-03 11:32:34 -0600

    Updating formal systems info

--- source/modules/app/components/info/formal-systems/formal-systems.tsx
+++ source/modules/app/components/info/formal-systems/formal-systems.tsx
@@ -9,14 +9,15 @@ export const FormalSystems = (): ReactElement => {
       </h3>
       <p>
         <RenderMath inline>
-          A <em>formal system</em> is a triple $$\langle CN,VR,\Gamma\rangle$$ where $$\Gamma$$ is a set of statements. The
-          members of $$\Gamma$$ are called <em>axiomatic statements</em>. Sometimes we will refer to a formal system by just
-          $$\Gamma$$ when $$CN$$ and $$VR$$ are understood.
+          A formal system is a triple $$\langle CN,VR,\Gamma\rangle$$ where $$\Gamma$$ is a set of statements. The members of
+          $$\Gamma$$ are called axiomatic statements. Sometimes a formal system is referred to by just $$\Gamma$$ when $$CN$$
+          and $$VR$$ are understood.
         </RenderMath>
         <br />
+        <br />
         <RenderMath inline>
-          Given a formal system $$\Gamma$$, the <em>closure</em> of a pre-statement $$\langle D,T,H,A\rangle$$ is the smallest
-          set $$C$$ of expressions such that:
+          Given a formal system, the closure of a pre-statement $$\langle D,T,H,A\rangle$$ is the smallest set $$C$$ of
+          expressions such that:
         </RenderMath>
         <RenderMath>
           \begin{'{'}split{'}'}
@@ -29,10 +30,10 @@ export const FormalSystems = (): ReactElement => {
         </RenderMath>
         <br />
         <RenderMath inline>
-          A pre-statement $$\langle D,T,H,A\rangle$$ is <em>provable</em> if $$A\in C$$ i.e. if its assertion belongs to its
-          closure. A statement is <em>provable</em> if it is the reduct of a provable pre-statement. The <em>universe</em> of
-          a formal system is the collection of all of its provable statements. Note that the set of axiomatic statements
-          $$\Gamma$$ in a formal system is a subset of its universe.
+          A pre-statement $$\langle D,T,H,A\rangle$$ is provable if its assertion belongs to its closure, $$A\in C$$. A
+          statement is provable if it is the reduct of a provable pre-statement. The universe of a formal system is the
+          collection of all of its provable statements. Note that the set of axiomatic statements $$\Gamma$$ is a subset of its
+          universe.
         </RenderMath>
       </p>
     </section>
