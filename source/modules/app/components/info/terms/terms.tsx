04994ddbc375302ec910081bddb86d16258d2424
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-03 12:25:26 -0600

    Cleaned up terms

--- source/modules/app/components/info/terms/terms.tsx
+++ source/modules/app/components/info/terms/terms.tsx
@@ -9,38 +9,43 @@ export const Terms = (): ReactElement => {
       </h3>
       <p>
         <RenderMath inline>
-          A formal system has a set of <em>symbols</em> denoted by $$SM$$. A precise set-theoretical definition of this set is
+          A formal system has a set of symbols denoted by $$SM$$. A precise set-theoretical definition of this set is
           unimportant; a symbol could be considered a primitive or atomic element if we wish. We assume this set is divided
-          into two disjoint subsets: a set $$CN$$ of <em>constants</em> and a set $$VR$$ of <em>variables</em>. $$CN$$ and
-          $$VR$$ are each assumed to consist of countably many symbols which may be arranged in finite or simple infinite
-          sequences $$c_0$$, $$c_1$$, $$\dots$$ and $$v_0$$, $$v_1$$, $$\dots$$ respectively, without repeating terms. We will
-          represent arbitrary symbols by metavariables $$\alpha$$, $$\beta$$, etc.
+          into two disjoint subsets: a set $$CN$$ of constant symbols and a set $$VR$$ of variable symbols. $$CN$$ and $$VR$$
+          are each assumed to consist of countably many symbols which may be arranged in finite or simple infinite sequences
+          $$c_0$$, $$c_1$$, $$\dots$$ and $$v_0$$, $$v_1$$, $$\dots$$ respectively, without repeating terms. We will represent
+          arbitrary symbols by metavariables $$\alpha$$, $$\beta$$, etc.
         </RenderMath>
         <br />
+        <br />
         <RenderMath inline>
-          Finite sequences all terms of which are symbols are called <em>expressions</em>. $$EX$$ is the set of all
-          expressions; thus
+          Finite sequences all terms of which are symbols are called expressions. $$EX$$ is the set of all expressions; thus
         </RenderMath>
         <RenderMath>
-          EX=\cup_{'{'}n\in\omega{'}'}{'{'}^n{'}'}SM.
+          EX=\bigcup_{'{'}n\in\omega{'}'}{'{'}^n{'}'}SM
         </RenderMath>
-        <br />
-        <RenderMath inline>
-          A <em>constant-prefixed expression</em> is an expression of non-zero length whose first term is a constant. We
-          denote the set of all constant-prefixed expressions by $$EX_C=\{'{'}e\in EX\vert(\vert e\vert\gt0\land e_0\in CN$$.
+        A constant-prefixed expression is an expression of non-zero length whose first term is a constant. We denote the set of
+        all constant-prefixed expressions by
+        <RenderMath>
+          EX_C=\{'{'}e\in EX:\vert e\vert\gt0\land e_0\in CN\{'}'}
         </RenderMath>
-        <br />
-        <RenderMath inline>
-          A <em>constant-variable pair</em> is an expression of length $$2$$ whose first term is a constant and whose second
-          term is a variable. We denote the set of all constant-variable pairs by
-          $$EX_2=\{'{'}e\in EX_C\vert(\vert e\vert=2\land e_1\in VR$$.
+        A constant-variable pair is an expression of length two whose first term is a constant symbol and whose second term is a
+        variable symbol. We denote the set of all constant-variable pairs by
+        <RenderMath>
+          EX_2=\{'{'}e\in EX_C:\vert e\vert=2\land e_1\in VR\{'}'}
         </RenderMath>
-        <br />
         <RenderMath inline>
-          We denote by $$\mathcal{'{'}V{'}'}(e)$$ the set of all variables in an expression e\in EX, i.e. the set of all
-          $$\alpha\in VR$$ such that $$\alpha=e_n$$ for some $$n\lt\vert e\vert$$. We also denote (with abuse of notation) by
-          $$\mathcal{'{'}V{'}'}(E)$$ the set of all variables in a collection of expressions $$E\subseteq EX$$, i.e.
-          $$\cup_{'{'}e\in E{'}'}\mathcal{'{'}V{'}'}(e)$$.
+          We denote by $$\mathcal{'{'}V{'}'}(e)$$ the set of all variables in a given expression.
+        </RenderMath>
+        <RenderMath>
+          \mathcal{'{'}V{'}'}(e)=\{'{'}\alpha\in VR:\exists n(\alpha=e_n)\{'}'}
+        </RenderMath>
+        <RenderMath inline>
+          We also denote (with abuse of notation) by $$\mathcal{'{'}V{'}'}(E)$$ the set of all variables in a collection of
+          expressions $$E\subseteq EX$$.
+        </RenderMath>
+        <RenderMath>
+          \mathcal{'{'}V{'}'}(E)=\bigcup_{'{'}e\in E{'}'}\mathcal{'{'}V{'}'}(e)
         </RenderMath>
       </p>
     </section>
