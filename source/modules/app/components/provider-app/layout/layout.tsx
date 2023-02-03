886c8d838b61a31a09c3876384a0c320705ab39b
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-02 20:01:58 -0600

    Removed proof of concept style

--- source/modules/app/components/provider-app/layout/layout.tsx
+++ source/modules/app/components/provider-app/layout/layout.tsx
@@ -10,7 +10,7 @@ export const Layout = (props: PropsWithChildren<{}>): ReactElement => {
     <Fragment>
       <GlobalStyle />
       <Header />
-      <main style={{ height: '1000px' }}>
+      <main>
         {children}
       </main>
       <Footer />
