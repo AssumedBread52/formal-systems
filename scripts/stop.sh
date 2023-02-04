0bd501254fe6dd4f4d12a31643089684ab3c9413
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-04 08:57:30 -0600

    Add option to clear out database data

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+if [ "$1" == "--help" ]; then
+  echo "Usage: stop.sh"
+  echo "  Stops the development environment. Stopped containers are removed as well as"
+  echo "  the constructed network."
+  echo "  --clean = Will removed generated database files that are normally preserved"
+  echo "            between sessions. WARNING: all data in the database will be lost so"
+  echo "            be certain."
+
+  exit
+fi
+
 GROUP_ID=$(id -g) USER_ID=$(id -u) USER_NAME=$(whoami) docker-compose down
+
+if [ "$1" == "--clean" ]; then
+  rm database-files/lib-postgresql-data/* -r
+fi
