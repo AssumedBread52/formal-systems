a893a67f7e12315920ecdbf8ea539d0b2426c73c
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-05 15:55:06 -0600

    Use common helper

--- source/modules/app/api/health-check.ts
+++ source/modules/app/api/health-check.ts
@@ -1,23 +1,15 @@
 import { StatusCodes } from '@/app/constants';
-import { MongoClient } from 'mongodb';
+import { getMongoClient } from '@/common/helpers';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export const healthCheck = async (_: NextApiRequest, response: NextApiResponse<void>): Promise<void> => {
   try {
-    const username = process.env.MONGO_USERNAME;
-    const password = process.env.MONGO_PASSWORD;
-    const hostname = process.env.MONGO_HOSTNAME;
-    const encodedPassword = encodeURIComponent(password ?? '');
-    const credentials = `${username}:${encodedPassword}`;
-    const connectionString = `mongodb://${credentials}@${hostname}/formal-systems?authSource=admin`;
-
-    const client = await MongoClient.connect(connectionString);
+    const client = await getMongoClient();
 
     await client.close();
 
     response.status(StatusCodes.EmptyResponseSuccess).end();
-  } catch (error: any) {
-    console.log(error);
+  } catch {
     response.status(StatusCodes.DatabaseError).end();
   }
 };
