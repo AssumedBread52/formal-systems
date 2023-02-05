85505bfb812f96acd6f239ffaea5977e16187b89
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-05 15:55:58 -0600

    Add server user type

---
+++ source/modules/user/types/index.ts
@@ -0,0 +1,1 @@
+export type { ServerUser } from './server-user';
---
+++ source/modules/user/types/server-user.ts
@@ -0,0 +1,6 @@
+export type ServerUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  hashedPassword: string;
+};
