c1d2fd9df6770e2d796a1c3f73b7f2ce1cb5d1a6
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-05 07:50:59 -0600

    Connect to mongodb as a part of health check

--- source/modules/app/api/health-check.ts
+++ source/modules/app/api/health-check.ts
@@ -1,6 +1,16 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { StatusCodes } from '@/app/constants';
+import { MongoClient } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export const healthCheck = async (_: NextApiRequest, response: NextApiResponse<void>): Promise<void> => {
-  response.status(StatusCodes.EmptyResponseSuccess).end();
+  try {
+    const client = await MongoClient.connect(`mongodb://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD ?? '')}@${process.env.MONGO_HOSTNAME}/formal-systems?authSource=admin`);
+
+    await client.close();
+
+    response.status(StatusCodes.EmptyResponseSuccess).end();
+  } catch (error: any) {
+    console.log(error);
+    response.status(StatusCodes.DatabaseError).end();
+  }
 };
