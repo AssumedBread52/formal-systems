d4293e35f95d1e53480d2266641ed98543ad93fd
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-07 17:06:49 -0600

    Added the last name field

--- source/modules/user/components/sign-up-page/sign-up-page.tsx
+++ source/modules/user/components/sign-up-page/sign-up-page.tsx
@@ -7,14 +7,22 @@ import { FocusEvent, FormEvent, ReactElement, useState } from 'react';
 
 export const signUpPage = (): ReactElement => {
   const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
 
   const [firstNameTouched, setFirstNameTouched] = useState<boolean>(false);
+  const [lastNameTouched, setLastNameTouched] = useState<boolean>(false);
 
   const firstNameBlurHandler = (event: FocusEvent<HTMLInputElement>): void => {
     event.preventDefault();
     event.stopPropagation();
 
     setFirstNameTouched(true);
+  };
+  const lastNameBlurHandler = (event: FocusEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setLastNameTouched(true);
   };
 
   const firstNameInputHandler = (event: FormEvent<HTMLInputElement>): void => {
@@ -23,17 +31,26 @@ export const signUpPage = (): ReactElement => {
 
     setFirstName(event.currentTarget.value);
   };
+  const lastNameInputHandler = (event: FormEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setLastName(event.currentTarget.value);
+  };
 
   const firstNameHasError = !hasText(firstName);
+  const lastNameHasError = !hasText(lastName);
 
   const firstNameShowError = firstNameTouched && firstNameHasError;
+  const lastNameShowError = lastNameTouched && lastNameHasError;
 
   const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     event.stopPropagation();
 
     console.log('sign-up-payload', {
-      firstName
+      firstName,
+      lastName
     });
   };
 
@@ -55,6 +72,19 @@ export const signUpPage = (): ReactElement => {
             </Flex>
             <Typography as='p' color='red' fontSize='0.75rem' height='1rem' width='100%' m='0'>
               {firstNameShowError && 'Please enter your first name.'}
+            </Typography>
+          </Flex>
+          <Flex display='flex' flexWrap='wrap' justifyContent='space-between'>
+            <Flex flexBasis='25%'>
+              <label htmlFor='last-name'>
+                Last Name
+              </label>
+            </Flex>
+            <Flex flexBasis='65%'>
+              <Input id='last-name' type='text' width='100%' value={lastName} onBlur={lastNameBlurHandler} onInput={lastNameInputHandler} />
+            </Flex>
+            <Typography as='p' color='red' fontSize='0.75rem' height='1rem' width='100%' m='0'>
+              {lastNameShowError && 'Please enter your last name.'}
             </Typography>
           </Flex>
         </form>
