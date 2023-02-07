110d42a3c6ec5c8d3294f7474deea43d6391466e
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-07 16:30:34 -0600

    Hooked up end point description with api slice

--- source/modules/app/api-slice.ts
+++ source/modules/app/api-slice.ts
@@ -1,8 +1,14 @@
+import { signUpUser } from '@/user/end-points';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: '/api'
   }),
-  endpoints: () => ({})
+  endpoints: (builder) => {
+    return {
+      signUpUser: signUpUser(builder)
+    };
+  },
+  tagTypes: ['session-user']
 });
