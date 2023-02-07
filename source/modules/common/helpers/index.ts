1bb0a1363026e0cb80236019e75241e86727fdf5
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-07 16:01:13 -0600

    Added a non-empty text helper

--- source/modules/common/helpers/index.ts
+++ source/modules/common/helpers/index.ts
@@ -1,3 +1,4 @@
 export { buildMongoUrl } from './build-mongo-url';
+export { hasText } from './has-text';
 export { sendErrorResponse } from './send-error-response';
 export { sendMethodTypeErrorResponse } from './send-method-type-error-response';
