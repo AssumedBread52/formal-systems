14f34574ddfd18e9954e4adb9f27d9ba89d45791
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-07 16:41:36 -0600

    Typeset the page title

--- source/modules/user/components/sign-up-page/sign-up-page.tsx
+++ source/modules/user/components/sign-up-page/sign-up-page.tsx
@@ -1,9 +1,15 @@
+import { Typography } from '@/common/components/typography/typography';
+import { Box } from '@/common/components/box/box';
 import { ReactElement } from 'react';
 
 export const signUpPage = (): ReactElement => {
   return (
-    <div>
-      Sign Up Page!
-    </div>
+    <Box mx='auto' width='32rem'>
+      <section>
+        <Typography as='h2' textAlign='center'>
+          Sign Up
+        </Typography>
+      </section>
+    </Box>
   );
 };
