75ebd4430347a03fd376b34ebcf0f1e9c69b3908
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-07 16:47:13 -0600

    Added form skeleton

--- source/modules/user/components/sign-up-page/sign-up-page.tsx
+++ source/modules/user/components/sign-up-page/sign-up-page.tsx
@@ -1,14 +1,23 @@
 import { Typography } from '@/common/components/typography/typography';
 import { Box } from '@/common/components/box/box';
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement } from 'react';
 
 export const signUpPage = (): ReactElement => {
+  const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log('submit form');
+  };
+
   return (
     <Box mx='auto' width='32rem'>
       <section>
         <Typography as='h2' textAlign='center'>
           Sign Up
         </Typography>
+        <form onSubmit={submitHandler}>
+        </form>
       </section>
     </Box>
   );
