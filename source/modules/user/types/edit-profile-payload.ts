49b570bdfe5ed79bf0672a0936ee05bb49886deb
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-09 17:04:59 -0600

    Added type for updating user request

---
+++ source/modules/user/types/edit-profile-payload.ts
@@ -0,0 +1,3 @@
+import { SignUpPayload } from './sign-up-payload';
+
+export type EditProfilePayload = Partial<SignUpPayload>;
--- source/modules/user/types/index.ts
+++ source/modules/user/types/index.ts
@@ -1,2 +1,3 @@
+export type { EditProfilePayload } from './edit-profile-payload';
 export type { ServerUser } from './server-user';
 export type { SignUpPayload } from './sign-up-payload';
