a33a337be60fcd34e9e9a41637412b22ebf13e9f
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-09 17:32:27 -0600

    Session user data should include email

--- source/modules/user/types/index.ts
+++ source/modules/user/types/index.ts
@@ -1,3 +1,4 @@
 export type { EditProfilePayload } from './edit-profile-payload';
 export type { ServerUser } from './server-user';
+export type { SessionUser } from './session-user';
 export type { SignUpPayload } from './sign-up-payload';
---
+++ source/modules/user/types/session-user.ts
@@ -0,0 +1,5 @@
+export type SessionUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
