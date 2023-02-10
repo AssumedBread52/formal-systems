e40367fa826f65daf85965c6ad874cf0048601b4
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-10 15:38:31 -0600

    Buttons should have a pointer for a cursor

--- source/modules/common/components/button/button.ts
+++ source/modules/common/components/button/button.ts
@@ -1,9 +1,11 @@
 import styled, { CSSProperties } from 'styled-components';
-import { space, SpaceProps, system, typography, TypographyProps } from 'styled-system';
+import { layout, LayoutProps, space, SpaceProps, system, typography, TypographyProps } from 'styled-system';
 
 export const Button = styled.button<{
   cursor: CSSProperties['cursor'];
-} | SpaceProps | TypographyProps>`
+} | LayoutProps | SpaceProps | TypographyProps>`
+  cursor: pointer;
+  ${layout};
   ${space};
   ${typography};
   ${system({
