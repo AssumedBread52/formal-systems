3d3d5520a3b294724db03d60ba0798146d82566d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-12 16:03:30 -0600

    Added related theme values

--- source/modules/app/constants/theme.ts
+++ source/modules/app/constants/theme.ts
@@ -1,13 +1,18 @@
 export const theme = {
   colors: {
-    headerBackground: 'rgb(0, 21, 41)',
-    headerText: 'rgba(255, 255, 255, 75%)'
+    nonMainBackground: 'rgb(0, 21, 41)',
+    nonMainText: 'rgba(255, 255, 255, 75%)'
+  },
+  radii: {
+    card: '8px'
   },
   shadows: {
     headerScrolled: '0px 4px 8px 0px rgb(128 128 128)',
-    headerUnscrolled: '0px 2px 4px 0px rgb(128 128 128)'
+    headerUnscrolled: '0px 2px 4px 0px rgb(128 128 128)',
+    centeredHovered: '0px 0px 8px 0px rgb(128 128 128)',
+    centeredUnhovered: '0px 0px 4px 0px rgb(128 128 128)'
   },
-  sizes: ['0rem', '0.5rem', '1rem', '2rem', '4rem'],
+  sizes: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem'],
   space: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem'],
   transitions: {
     headerShadow: 'all 100ms ease-in-out',
