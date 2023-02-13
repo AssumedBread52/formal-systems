33996273b49b714c88f33151417639d9b6588046
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-13 15:59:31 -0600

    Updated base layout and added to theme

--- source/modules/app/components/provider-app/layout/layout.tsx
+++ source/modules/app/components/provider-app/layout/layout.tsx
@@ -1,6 +1,6 @@
+import { theme } from '@/app/constants';
 import { useIsDocumentScrolled, useMainHeight } from '@/app/hooks';
 import { Box } from '@/common/components/box/box';
-import { Flex } from '@/common/components/flex/flex';
 import { Fragment, PropsWithChildren, ReactElement } from 'react';
 import { Footer } from './footer/footer';
 import { GlobalStyle } from './global-style/global-style';
@@ -14,19 +14,21 @@ export const Layout = (props: PropsWithChildren<{}>): ReactElement => {
   const isDocumentScrolled = useIsDocumentScrolled();
 
   const boxShadow = isDocumentScrolled ? 'headerScrolled' : 'headerUnscrolled';
+  
+  const marginIndex = 4;
 
   return (
     <Fragment>
       <GlobalStyle />
-      <Box ref={headerRef} backgroundColor='nonMainBackground' color='nonMainText' boxShadow={boxShadow} transition='headerShadow' position='sticky' top='0' zIndex='nonMain'>
+      <Box ref={headerRef} backgroundColor='nonMainBackground' color='nonMainText' boxShadow={boxShadow} transition='headerShadow' minWidth='7' position='sticky' top='0' zIndex='nonMain'>
         <Header />
       </Box>
-      <Flex display='flex' justifyContent='center' minHeight={`calc(100vh - ${height}px)`}>
+      <Box my={marginIndex} minHeight={`calc(100vh - ${height}px - ${theme.space[marginIndex]} - ${theme.space[marginIndex]})`}>
         <main>
           {children}
         </main>
-      </Flex>
-      <Box ref={footerRef} position='relative' backgroundColor='nonMainBackground' color='nonMainText' zIndex='nonMain'>
+      </Box>
+      <Box ref={footerRef} backgroundColor='nonMainBackground' color='nonMainText' minWidth='7' position='relative' zIndex='nonMain'>
         <Footer />
       </Box>
     </Fragment>
--- source/modules/app/constants/theme.ts
+++ source/modules/app/constants/theme.ts
@@ -3,6 +3,9 @@ export const theme = {
     nonMainBackground: 'rgb(0, 21, 41)',
     nonMainText: 'rgba(255, 255, 255, 75%)'
   },
+  fontSizes: {
+    formButton: '1rem'
+  },
   radii: {
     card: '8px'
   },
@@ -12,7 +15,7 @@ export const theme = {
     centeredHovered: '0px 0px 8px 0px rgb(128 128 128)',
     centeredUnhovered: '0px 0px 4px 0px rgb(128 128 128)'
   },
-  sizes: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem'],
+  sizes: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem', '16rem', '32rem'],  
   space: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem'],
   transitions: {
     headerShadow: 'all 100ms ease-in-out',
