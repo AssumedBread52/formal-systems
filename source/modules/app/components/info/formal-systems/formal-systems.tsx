cd9f46be4b64c8db4449ab6e52ac73601860f08e
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-14 18:56:40 -0600

    Formal System info update

--- source/modules/app/components/info/formal-systems/formal-systems.tsx
+++ source/modules/app/components/info/formal-systems/formal-systems.tsx
@@ -8,33 +8,28 @@ export const FormalSystems = (): ReactElement => {
         Formal Systems
       </h3>
       <p>
-        <RenderMath inline>
-          A formal system is a triple $$\langle CN,VR,\Gamma\rangle$$ where $$\Gamma$$ is a set of statements. The members of
-          $$\Gamma$$ are called axiomatic statements. Sometimes a formal system is referred to by just $$\Gamma$$ when $$CN$$
-          and $$VR$$ are understood.
-        </RenderMath>
-        <br />
-        <br />
-        <RenderMath inline>
-          Given a formal system, the closure of a pre-statement $$\langle D,T,H,A\rangle$$ is the smallest set $$C$$ of
-          expressions such that:
-        </RenderMath>
+        A formal system is a triple of a constant symbols set, a variable symbols set, and axiomatic statements set. If the
+        symbol sets are understood the formal system can be identified only by its axiomatic statements.
+        <RenderMath content='\langle CN,VR,\Gamma\rangle' />
+        The closure of a pre-statement is constructed in the context of a given formal system. The closure of a pre-statement
+        begins with its hypotheses.
+        <RenderMath content='T\cup H\subseteq C' />
+        Expressions are added to the closure by first selecting an axiomatic statement and appropriate substitution map.
+        <RenderMath content='\langle D_M^\prime,T_M^\prime,H^\prime,A^\prime\rangle\in\Gamma' />
+        <RenderMath content='F:\mathcal{V}(T_M^\prime)\to EX' />
+        There are two conditions which must be met: 1. the simultaneously substitution hypotheses of the axiomatic statement
+        must already be in the closure and 2. there cannot be any distinct variable restriction violations.
         <RenderMath>
-          \begin{'{'}split{'}'}
-          1.&T\cup H\subseteq C;~\text{'{'}and{'}'}\\
-          {'2.&\\text{If for some axiomatic statement}~\\langle D_M\',T_M\',H\',A\'\\rangle\\in\\Gamma~\\text{and for some substitution}~\\sigma~\\text{we have}\\\\'}
-          {'&a.~\\sigma(T_M\'\\cup H\')\\subseteq C;~\\text{and}\\\\'}
-          {'&b.~\\text{for all}~\\{\\alpha,\\beta\\}\\in D_M\',~\\text{for all}~\\gamma\\in\\mathcal{V}(\\sigma(\\langle\\alpha\\rangle)),~\\text{and for all}~\\delta\\in\\mathcal{V}(\\sigma(\\langle\\beta\\rangle)),~\\text{we have}~\\{\\gamma,\\delta\\}\\in D;\\\\'}
-          {'&\\text{then}~\\sigma(A\')\\in C.'}
-          \end{'{'}split{'}'}
+          \begin{'{'}aligned{'}'}
+          1.~&\sigma_F(T_M^\prime\cup H^\prime)\subseteq C\\
+          2.~&\forall\{'{'}\alpha,\beta\{'}'}\in D_M^\prime(\forall\gamma\in\sigma_F(\langle\alpha\rangle),\delta\in\sigma_F(\langle\beta\rangle)(\{'{'}\gamma,\delta\{'}'}\in D))
+          \end{'{'}aligned{'}'}
         </RenderMath>
-        <br />
-        <RenderMath inline>
-          A pre-statement $$\langle D,T,H,A\rangle$$ is provable if its assertion belongs to its closure, $$A\in C$$. A
-          statement is provable if it is the reduct of a provable pre-statement. The universe of a formal system is the
-          collection of all of its provable statements. Note that the set of axiomatic statements $$\Gamma$$ is a subset of its
-          universe.
-        </RenderMath>
+        If the conditions are met, then the simultaneously substituted assertion belongs to the closure.
+        <RenderMath content='\sigma_F(A^\prime)\in C' />
+        If the closure of a pre-statement contains its assertion, then the pre-statement is said to be provable. A provable
+        statement is the reduct of a provable pre-statement. The universe of a formal system is the set of all provable
+        statements. The axiomatic statements of a formal system are a subset of the universe of the formal system.
       </p>
     </section>
   );
