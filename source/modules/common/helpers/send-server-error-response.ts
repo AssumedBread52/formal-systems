3467af9ff2ab632b6fbb481895963d9951876c4c
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-15 17:53:03 -0600

    Be able to send error responses for formal systems

--- source/modules/common/helpers/send-server-error-response.ts
+++ source/modules/common/helpers/send-server-error-response.ts
@@ -1,9 +1,10 @@
 import { StatusCodes } from '@/common/constants';
 import { ErrorResponse } from '@/common/types';
+import { FormalSystemServerTasks } from '@/formal-system/constants';
 import { UserServerTasks } from '@/user/constants';
 import { NextApiResponse } from 'next';
 import { sendErrorResponse } from './send-error-response';
 
-export const sendServerErrorResponse = (response: NextApiResponse<ErrorResponse>, task: UserServerTasks): void => {
+export const sendServerErrorResponse = (response: NextApiResponse<ErrorResponse>, task: FormalSystemServerTasks | UserServerTasks): void => {
   return sendErrorResponse(response, StatusCodes.ServerError, `Server failed to ${task}.`);
 };
