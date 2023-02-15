9ca7ebbdad21a6bd543074ca726d198f0b424de0
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-14 20:33:20 -0600

    Added to formal systems page

--- source/modules/common/components/hyper-link/hyper-link.ts
+++ source/modules/common/components/hyper-link/hyper-link.ts
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import styled from 'styled-components';
-import { color, ColorProps } from 'styled-system';
+import { color, ColorProps, position, PositionProps, typography, TypographyProps } from 'styled-system';
 
-export const HyperLink = styled(Link)<ColorProps>`
+export const HyperLink = styled(Link)<ColorProps | PositionProps | TypographyProps>`
   ${color};
+  ${position};
+  ${typography};
 `;
--- source/modules/formal-system/components/formal-systems-page/formal-systems-page.tsx
+++ source/modules/formal-system/components/formal-systems-page/formal-systems-page.tsx
@@ -1,14 +1,43 @@
 import { Box } from '@/common/components/box/box';
+import { Flex } from '@/common/components/flex/flex';
+import { HyperLink } from '@/common/components/hyper-link/hyper-link';
+import { Input } from '@/common/components/input/input';
 import { Typography } from '@/common/components/typography/typography';
+import { useSession } from 'next-auth/react';
 import { ReactElement } from 'react';
 
 export const FormalSystemsPage = (): ReactElement => {
+  const { status } = useSession();
+
+  const isAuthenticated = 'authenticated' === status;
+
   return (
-    <Box px='4'>
+    <Box px='5'>
       <section>
-        <Typography as='h1' textAlign='center' my='3'>
+        <Typography as='h1' textAlign='center' position='relative' my='3'>
           Formal Systems
+          {isAuthenticated && (
+            <HyperLink position='absolute' right='0' fontSize='formButton' href='/'>
+              Create New
+            </HyperLink>
+          )}
         </Typography>
+        <Flex display='flex' mx='auto' width='7'>
+          <label htmlFor='keywords'>
+            Search
+          </label>
+          <Box mx='1' />
+          <Input id='keywords' type='text' width='100%' />
+        </Flex>
+        <div>
+          Pagination Controls
+        </div>
+        <div>
+          Results Area
+        </div>
+        <div>
+          Pagination Controls
+        </div>
       </section>
     </Box>
   );
