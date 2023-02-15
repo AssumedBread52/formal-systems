60c0e2ce522b5aac355220dcc60a5cef05fbbef0
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-14 18:28:01 -0600

    Updated terms

--- source/modules/app/components/info/terms/terms.tsx
+++ source/modules/app/components/info/terms/terms.tsx
@@ -8,45 +8,26 @@ export const Terms = (): ReactElement => {
         Constants, Variables, and Expressions
       </h3>
       <p>
-        <RenderMath inline>
-          A formal system has a set of symbols denoted by $$SM$$. A precise set-theoretical definition of this set is
-          unimportant; a symbol could be considered a primitive or atomic element if we wish. We assume this set is divided
-          into two disjoint subsets: a set $$CN$$ of constant symbols and a set $$VR$$ of variable symbols. $$CN$$ and $$VR$$
-          are each assumed to consist of countably many symbols which may be arranged in finite or simple infinite sequences
-          $$c_0$$, $$c_1$$, $$\dots$$ and $$v_0$$, $$v_1$$, $$\dots$$ respectively, without repeating terms. We will represent
-          arbitrary symbols by metavariables $$\alpha$$, $$\beta$$, etc.
-        </RenderMath>
-        <br />
-        <br />
-        <RenderMath inline>
-          Finite sequences all terms of which are symbols are called expressions. $$EX$$ is the set of all expressions; thus
-        </RenderMath>
-        <RenderMath>
-          EX=\bigcup_{'{'}n\in\omega{'}'}{'{'}^n{'}'}SM
-        </RenderMath>
-        A constant-prefixed expression is an expression of non-zero length whose first term is a constant symbol. We denote the
-        set of all constant-prefixed expressions by
-        <RenderMath>
-          EX_C=\{'{'}e\in EX:\vert e\vert\gt0\land e_0\in CN\{'}'}
-        </RenderMath>
-        A constant-variable pair is an expression of length two whose first term is a constant symbol and whose second term is a
-        variable symbol. We denote the set of all constant-variable pairs by
-        <RenderMath>
-          EX_2=\{'{'}e\in EX_C:\vert e\vert=2\land e_1\in VR\{'}'}
-        </RenderMath>
-        <RenderMath inline>
-          We denote by $$\mathcal{'{'}V{'}'}(e)$$ the set of all variable symbols in a given expression.
-        </RenderMath>
-        <RenderMath>
-          \mathcal{'{'}V{'}'}(e)=\{'{'}\alpha\in VR:\exists n(\alpha=e_n)\{'}'}
-        </RenderMath>
-        <RenderMath inline>
-          We also denote (with abuse of notation) by $$\mathcal{'{'}V{'}'}(E)$$ the set of all variables in a collection of
-          expressions $$E\subseteq EX$$.
-        </RenderMath>
-        <RenderMath>
-          \mathcal{'{'}V{'}'}(E)=\bigcup_{'{'}e\in E{'}'}\mathcal{'{'}V{'}'}(e)
-        </RenderMath>
+        A formal system is constructed over a finite set of symbols. This set can be divided into a pair of disjoint subsets: a
+        set of constant symbols and variable symbols. This means every symbol in the formal system is either a constant symbol
+        or a variable symbol but not both.
+        <RenderMath content='SM=CN\cup VR' />
+        <RenderMath content='CN\cap VR=\emptyset' />
+        Arbitrary symbol variables will be represented by metavariables.
+        <RenderMath content='\alpha,\beta,\dots' />
+        An expression is a finite sequence of symbols.
+        <RenderMath content='EX=\bigcup_{n\in\omega}{^n}SM' />
+        A constant prefixed expression is an expression of non-zero length whose first term is a constant symbol.
+        <RenderMath content='EX_C=\{e\in EX::\vert e\vert\gt0\land e_0\in CN\}' />
+        A constant variable pair is an expression of length two whose first term is a constant symbol and whose second term is a
+        variable symbol.
+        <RenderMath content='EX_2=\{e\in EX_C::\vert e\vert=2\land e_1\in VR\}' />
+        From every expression we can construct a set containing all the variable symbols that appear in the expression.
+        <RenderMath content='\mathcal{V}:EX\to\mathcal{P}(VR)' />
+        <RenderMath content='\mathcal{V}(e)=\{\alpha\in VR::\exists n(\alpha=e_n)\}' />
+        For the sake of brevity sometimes we will apply this function to a set of expressions.
+        <RenderMath content='\mathcal{V}:\mathcal{P}(EX)\to\mathcal{P}(VR)' />
+        <RenderMath content='\mathcal{V}(E)=\bigcup_{e\in E}\mathcal{V}(e)' />
       </p>
     </section>
   );
