71ce8c9cbb434f0663670c434dec6dba69b67e20
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-15 21:02:58 -0600

    Add update formal system payload

--- source/modules/formal-system/types/index.ts
+++ source/modules/formal-system/types/index.ts
@@ -1,3 +1,4 @@
 export type { ClientFormalSystem } from './client-formal-system';
 export type { NewFormalSystemPayload } from './new-formal-system-payload';
 export type { ServerFormalSystem } from './server-formal-system';
+export type { UpdateFormalSystemPayload } from './update-formal-system-payload';
---
+++ source/modules/formal-system/types/update-formal-system-payload.ts
@@ -0,0 +1,5 @@
+export type UpdateFormalSystemPayload = {
+  id: string;
+  name?: string;
+  description?: string;
+};
