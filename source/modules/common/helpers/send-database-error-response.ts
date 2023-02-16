9c1b7a1c8eb4d1aab0186f0a160eb155adf5177f
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-15 20:33:34 -0600

    Formal system task included in database error

--- source/modules/common/helpers/send-database-error-response.ts
+++ source/modules/common/helpers/send-database-error-response.ts
@@ -1,9 +1,10 @@
 import { StatusCodes } from '@/common/constants';
 import { ErrorResponse } from '@/common/types';
+import { FormalSystemServerTasks } from '@/formal-system/constants';
 import { UserServerTasks } from '@/user/constants';
 import { NextApiResponse } from 'next';
 import { sendErrorResponse } from './send-error-response';
 
-export const sendDatabaseErrorResponse = (response: NextApiResponse<ErrorResponse>, task: UserServerTasks): void => {
+export const sendDatabaseErrorResponse = (response: NextApiResponse<ErrorResponse>, task: FormalSystemServerTasks | UserServerTasks): void => {
   return sendErrorResponse(response, StatusCodes.DatabaseError, `Database failed to ${task}.`);
 };
