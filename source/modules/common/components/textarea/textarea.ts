6d7f67b12c2bc16272ed00ba0edff8b7d604de9d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-16 10:41:06 -0600

    Fix export name

--- source/modules/common/components/textarea/textarea.ts
+++ source/modules/common/components/textarea/textarea.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { layout, LayoutProps } from 'styled-system';
 
-export const Input = styled.textarea<LayoutProps>`
+export const Textarea = styled.textarea<LayoutProps>`
   ${layout};
 `;
