263800dd309c908bf4503235c7516f7527116a70
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-15 20:57:29 -0600

    Added new server task

--- source/modules/formal-system/constants/formal-system-server-tasks.ts
+++ source/modules/formal-system/constants/formal-system-server-tasks.ts
@@ -1,4 +1,5 @@
 export enum FormalSystemServerTasks {
   Create = 'create a new formal system',
-  ReadPaginated = 'read paginated formal system data'
+  ReadPaginated = 'read paginated formal system data',
+  Update = 'update the specified formal system'
 };
