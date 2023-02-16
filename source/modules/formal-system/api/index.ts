5e60ee6a6dc07a601a194a747b8916f23d7bfeb6
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-16 05:17:54 -0600

    Added ablility to delete formal systems

--- source/modules/formal-system/api/index.ts
+++ source/modules/formal-system/api/index.ts
@@ -1,3 +1,4 @@
 export { createFormalSystem } from './create-formal-system';
+export { deleteFormalSystem } from './delete-formal-system';
 export { readFormalSystems } from './read-formal-systems';
 export { updateFormalSystem } from './update-formal-system';
