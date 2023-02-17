59bdfef1c9b4eb7584b7a45dca0445a2542c4f99
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-17 13:35:30 -0600

    Remove the disabled attribute until needed

--- source/modules/common/components/textarea-field/textarea-field.tsx
+++ source/modules/common/components/textarea-field/textarea-field.tsx
@@ -36,7 +36,7 @@ export const TextareaField = (props: TextareaFieldProps): ReactElement => {
         </label>
       </Flex>
       <Flex flexBasis='70%'>
-        <Textarea id={id} disabled={!updateValue} rows={10} width='100%' value={value} onBlur={blurHandler} onInput={inputHandler} />
+        <Textarea id={id} rows={10} width='100%' value={value} onBlur={blurHandler} onInput={inputHandler} />
       </Flex>
       <Typography as='p' color='red' fontSize='0.75rem' height='1rem' width='100%' m='0'>
         {showError && `Please enter a valid ${lowerCaseLabel}.`}
