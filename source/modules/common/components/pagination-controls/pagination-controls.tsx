d12b1b454f88a30b953f639db9ef6a92318e74e9
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-18 22:10:34 -0600

    Use new numeric select field component

--- source/modules/common/components/pagination-controls/pagination-controls.tsx
+++ source/modules/common/components/pagination-controls/pagination-controls.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@/common/components/box/box';
 import { Flex } from '@/common/components/flex/flex';
-import { Select } from '@/common/components/select/select';
-import { Dispatch, FormEvent, ReactElement, SetStateAction, useMemo } from 'react';
+import { Dispatch, ReactElement, SetStateAction } from 'react';
 import { KeywordsField } from './keywords-field/keywords-field';
+import { NumericSelectField } from './numeric-select-field/numeric-select-field';
 
 export const PaginationControls = (props: {
   total: number;
@@ -14,67 +14,17 @@ export const PaginationControls = (props: {
 }): ReactElement => {
   const { total, page, count, updatePage, updateCount, updateKeywords } = props;
 
-  const pageOptions = useMemo((): number[] => {
-    const options = Array.from(Array(Math.ceil(Math.max(total, 1) / count) + 1).keys());
+  const pageOptionCount = Math.ceil(Math.max(total, 1) / count);
 
-    options.shift();
-
-    return options;
-  }, [count, total]);
-
-  const countOptions = useMemo((): number[] => {
-    const options = Array.from(Array(Math.max(10, Math.min(total, 100)) + 1).keys());
-
-    options.shift();
-
-    return options;
-  }, [total]);
-
-  const pageInputHandler = (event: FormEvent<HTMLSelectElement>): void => {
-    event.preventDefault();
-    event.stopPropagation();
-    
-    updatePage(parseInt(event.currentTarget.value));
-  };
-
-  const countInputHandler = (event: FormEvent<HTMLSelectElement>): void => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    updateCount(parseInt(event.currentTarget.value));
-  };
+  const countOptionCount = Math.min(20, Math.max(2, Math.ceil(total / 5)));
 
   return (
     <Flex display='flex' mx='auto' my='3' width='7'>
       <KeywordsField updateKeywords={updateKeywords} />
       <Box mx='2' />
-      <label htmlFor='page'>
-        Page
-      </label>
+      <NumericSelectField label='Page' total={total} interval={1} optionCount={pageOptionCount} value={page} setValue={updatePage} />
       <Box mx='1' />
-      <Select id='page' name='page' disabled={2 > pageOptions.length || 0 === total} minWidth='4' value={page} onInput={pageInputHandler}>
-        {pageOptions.map((pageOption: number): ReactElement => {
-          return (
-            <option key={pageOption} value={pageOption}>
-              {pageOption}
-            </option>
-          );
-        })}
-      </Select>
-      <Box mx='1' />
-      <label htmlFor='count'>
-        Count
-      </label>
-      <Box mx='1' />
-      <Select id='count' name='count' disabled={2 > countOptions.length || 0 === total} minWidth='4' value={count} onInput={countInputHandler}>
-        {countOptions.map((countOption: number): ReactElement => {
-          return (
-            <option key={countOption} value={countOption}>
-              {countOption}
-            </option>
-          );
-        })}
-      </Select>
+      <NumericSelectField label='Count' total={total} interval={5} optionCount={countOptionCount} value={count} setValue={updateCount} />
     </Flex>
   );
 };
