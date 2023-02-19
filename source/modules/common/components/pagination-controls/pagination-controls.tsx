a4942ff71d06c52d4d4106bfc3c6aaaad9e9f385
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-19 12:10:15 -0600

    Display total with the pagination controls

--- source/modules/common/components/pagination-controls/pagination-controls.tsx
+++ source/modules/common/components/pagination-controls/pagination-controls.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@/common/components/box/box';
 import { Flex } from '@/common/components/flex/flex';
 import { PaginationControlsProps } from '@/common/types';
-import { ReactElement } from 'react';
+import { Fragment, ReactElement } from 'react';
 import { KeywordsField } from './keywords-field/keywords-field';
 import { NumericSelectField } from './numeric-select-field/numeric-select-field';
 
@@ -13,12 +13,17 @@ export const PaginationControls = (props: PaginationControlsProps): ReactElement
   const countOptionCount = Math.min(20, Math.max(2, Math.ceil(total / 5)));
 
   return (
-    <Flex display='flex' mx='auto' my='3' width='7'>
-      <KeywordsField updateKeywords={updateKeywords} />
-      <Box mx='2' />
-      <NumericSelectField label='Page' total={total} interval={1} optionCount={pageOptionCount} value={page} setValue={updatePage} />
-      <Box mx='1' />
-      <NumericSelectField label='Count' total={total} interval={5} optionCount={countOptionCount} value={count} setValue={updateCount} />
-    </Flex>
+    <Fragment>
+      <Flex display='flex' mx='auto' my='2' width='7'>
+        <KeywordsField updateKeywords={updateKeywords} />
+      </Flex>
+      <Flex display='flex' mx='auto' my='2' width='7'>
+        Total: {total}
+        <Box mx='auto' />
+        <NumericSelectField label='Page' total={total} interval={1} optionCount={pageOptionCount} value={page} setValue={updatePage} />
+        <Box mx='2' />
+        <NumericSelectField label='Count' total={total} interval={5} optionCount={countOptionCount} value={count} setValue={updateCount} />
+      </Flex>
+    </Fragment>
   );
 };
