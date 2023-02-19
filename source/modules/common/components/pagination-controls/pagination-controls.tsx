a4690467322cb236e7a56ca2743b3c66d38a97c7
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-18 21:32:48 -0600

    Simplify component using new component

--- source/modules/common/components/pagination-controls/pagination-controls.tsx
+++ source/modules/common/components/pagination-controls/pagination-controls.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@/common/components/box/box';
 import { Flex } from '@/common/components/flex/flex';
-import { Input } from '@/common/components/input/input';
+import { KeywordsField } from '@/common/components/keywords-field/keywords-field';
 import { Select } from '@/common/components/select/select';
-import { Dispatch, FormEvent, ReactElement, SetStateAction, useEffect, useMemo, useState } from 'react';
+import { Dispatch, FormEvent, ReactElement, SetStateAction, useMemo } from 'react';
 
 export const PaginationControls = (props: {
   total: number;
@@ -13,22 +13,6 @@ export const PaginationControls = (props: {
   updateKeywords: Dispatch<SetStateAction<string[]>>;
 }): ReactElement => {
   const { total, page, count, updatePage, updateCount, updateKeywords } = props;
-
-  const [keywords, setKeywords] = useState<string>('');
-
-  useEffect((): (() => void) => {
-    const keywordList = keywords.trim().split(' ').filter((keyword: string): boolean => {
-      return 0 !== keyword.length;
-    });
-
-    const delayedUpdate = setTimeout((): void => {
-      updateKeywords(keywordList);
-    }, 300);
-
-    return (): void => {
-      clearTimeout(delayedUpdate);
-    };
-  }, [keywords]);
 
   const pageOptions = useMemo((): number[] => {
     const options = Array.from(Array(Math.ceil(Math.max(total, 1) / count) + 1).keys());
@@ -60,26 +44,15 @@ export const PaginationControls = (props: {
     updateCount(parseInt(event.currentTarget.value));
   };
 
-  const keywordInputHandler = (event: FormEvent<HTMLInputElement>): void => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    setKeywords(event.currentTarget.value);
-  };
-
   return (
     <Flex display='flex' mx='auto' my='3' width='7'>
-      <label htmlFor='keywords'>
-        Search
-      </label>
-      <Box mx='1' />
-      <Input id='keywords' type='search' width='100%' value={keywords} onInput={keywordInputHandler} />
+      <KeywordsField updateKeywords={updateKeywords} />
       <Box mx='2' />
       <label htmlFor='page'>
         Page
       </label>
       <Box mx='1' />
-      <Select id='page' name='page' disabled={2 > pageOptions.length} minWidth='4' value={page} onInput={pageInputHandler}>
+      <Select id='page' name='page' disabled={2 > pageOptions.length || 0 === total} minWidth='4' value={page} onInput={pageInputHandler}>
         {pageOptions.map((pageOption: number): ReactElement => {
           return (
             <option key={pageOption} value={pageOption}>
@@ -93,7 +66,7 @@ export const PaginationControls = (props: {
         Count
       </label>
       <Box mx='1' />
-      <Select id='count' name='count' disabled={2 > countOptions.length} minWidth='4' value={count} onInput={countInputHandler}>
+      <Select id='count' name='count' disabled={2 > countOptions.length || 0 === total} minWidth='4' value={count} onInput={countInputHandler}>
         {countOptions.map((countOption: number): ReactElement => {
           return (
             <option key={countOption} value={countOption}>
