f042217eccf9a332753520691b59feeeda178d80
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-19 21:13:46 -0600

    Added the deletion inteface

--- source/modules/formal-system/components/formal-systems-page/formal-systems-list/formal-system-item/formal-system-item.tsx
+++ source/modules/formal-system/components/formal-systems-page/formal-systems-list/formal-system-item/formal-system-item.tsx
@@ -1,15 +1,33 @@
 import { Box } from '@/common/components/box/box';
+import { Button } from '@/common/components/button/button';
 import { Typography } from '@/common/components/typography/typography';
 import { ClientFormalSystem } from '@/formal-system/types';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { MouseEvent, ReactElement, useState } from 'react';
 
 export const FormalSystemItem = (props: ClientFormalSystem): ReactElement => {
-  const { title, description } = props;
+  const { title, urlPath, description, createdByUserId } = props;
 
   const { status } = useSession();
 
   const [entered, setEntered] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const clickHandler = (event: MouseEvent<HTMLDivElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    router.push(`/${urlPath}`);
+  };
+
+  const deleteHandler = (event: MouseEvent<HTMLButtonElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    router.push(`/${urlPath}/confirm-delete`);
+  };
 
   const mouseEnterHandler = (event: MouseEvent<HTMLDivElement>): void => {
     event.preventDefault();
@@ -28,10 +46,12 @@ export const FormalSystemItem = (props: ClientFormalSystem): ReactElement => {
   const shadow = entered ? 'mainHovered' : 'mainUnhovered';
 
   return (
-    <Box boxShadow={shadow} borderRadius='card' cursor='pointer' my='2' p='3' position='relative' onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+    <Box boxShadow={shadow} borderRadius='card' cursor='pointer' my='2' p='3' position='relative' onClick={clickHandler} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
       {'authenticated' === status && (
         <Box position='absolute' top='3' right='3'>
-          Delete
+          <Button onClick={deleteHandler}>
+            Delete
+          </Button>
         </Box>
       )}
       <Typography as='h4' m='0'>
@@ -40,7 +60,7 @@ export const FormalSystemItem = (props: ClientFormalSystem): ReactElement => {
       <Typography as='p'>
         {description}
       </Typography>
-      Created By Signature
+      Created By Signature: {createdByUserId}
     </Box>
   );
 };
