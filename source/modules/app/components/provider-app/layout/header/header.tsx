820977d7064021d2add8aecd3448d34b722a20eb
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-20 19:28:56 -0600

    Simplify home page link and add titles

--- source/modules/app/components/provider-app/layout/header/header.tsx
+++ source/modules/app/components/provider-app/layout/header/header.tsx
@@ -4,56 +4,47 @@ import { HyperLink } from '@/common/components/hyper-link/hyper-link';
 import { Typography } from '@/common/components/typography/typography';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { MouseEvent, ReactElement } from 'react';
+import { ReactElement } from 'react';
 
 export const Header = (): ReactElement => {
-  const { pathname, push } = useRouter();
+  const { pathname } = useRouter();
 
   const { status } = useSession();
-
-  const clickHandler = (event: MouseEvent<HTMLDivElement>): void => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    push('/');
-  };
 
   const isAuthenticated = 'authenticated' === status;
 
   return (
     <header>
       <Flex alignItems='center' display='flex' px='2' height='4'>
-        <Box cursor='pointer' mx='2' onClick={clickHandler}>
-          <Typography as='h2' my='auto'>
-            <HyperLink color={pathname === '/' ? 'white' : 'inherit'} href='/'>
-              Formal Systems
-            </HyperLink>
-          </Typography>
-        </Box>
+        <Typography as='h2' my='auto'>
+          <HyperLink title='Home-page' color={pathname === '/' ? 'white' : 'inherit'} href='/'>
+            Formal Systems
+          </HyperLink>
+        </Typography>
         <Box mx='auto' />
         <Box mx='2'>
-          <HyperLink color={pathname === '/info' ? 'white' : 'inherit'} href='/info'>
+          <HyperLink title='What is a formal system?' color={pathname === '/info' ? 'white' : 'inherit'} href='/info'>
             Info
           </HyperLink>
         </Box>
         <Box mx='2'>
           {isAuthenticated && (
-            <HyperLink color={pathname === '/sign-out' ? 'white' : 'inherit'} href='/sign-out'>
+            <HyperLink title='Confirm sign out' color={pathname === '/sign-out' ? 'white' : 'inherit'} href='/sign-out'>
               Sign Out
             </HyperLink>
           ) || (
-            <HyperLink color={pathname === '/sign-in' ? 'white' : 'inherit'} href='/sign-in'>
+            <HyperLink title='Sign in form' color={pathname === '/sign-in' ? 'white' : 'inherit'} href='/sign-in'>
               Sign In
             </HyperLink>
           )}
         </Box>
         <Box mx='2'>
           {isAuthenticated && (
-            <HyperLink color={pathname === '/edit-profile' ? 'white' : 'inherit'} href='/edit-profile'>
+            <HyperLink title='Edit profile form' color={pathname === '/edit-profile' ? 'white' : 'inherit'} href='/edit-profile'>
               Edit Profile
             </HyperLink>
           ) || (
-            <HyperLink color={pathname === '/sign-up' ? 'white' : 'inherit'} href='/sign-up'>
+            <HyperLink title='Sign up form' color={pathname === '/sign-up' ? 'white' : 'inherit'} href='/sign-up'>
               Sign Up
             </HyperLink>
           )}
