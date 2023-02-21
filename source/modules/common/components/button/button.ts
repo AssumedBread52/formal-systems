4f32e422bd3ae554942e6c049913b1dfe928322d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-20 19:27:43 -0600

    Added position properties to buttons

--- source/modules/common/components/button/button.ts
+++ source/modules/common/components/button/button.ts
@@ -1,11 +1,12 @@
 import styled, { CSSProperties } from 'styled-components';
-import { layout, LayoutProps, space, SpaceProps, system, typography, TypographyProps } from 'styled-system';
+import { layout, LayoutProps, position, PositionProps, space, SpaceProps, system, typography, TypographyProps } from 'styled-system';
 
 export const Button = styled.button<{
   cursor: CSSProperties['cursor'];
-} | LayoutProps | SpaceProps | TypographyProps>`
+} | LayoutProps | PositionProps | SpaceProps | TypographyProps>`
   cursor: pointer;
   ${layout};
+  ${position};
   ${space};
   ${typography};
   ${system({
