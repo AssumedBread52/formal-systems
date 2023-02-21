ba13fac68c66945caf41d764dd8b57a3ba02ad37
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-20 19:27:13 -0600

    Add more theme values

--- source/modules/app/constants/theme.ts
+++ source/modules/app/constants/theme.ts
@@ -1,21 +1,28 @@
 export const theme = {
+  borders: {
+    vertical: '2px rgba(0, 0, 0, 0.25) solid',
+    horizontal: '2px black solid'
+  },
+  borderWidths: ['0px', '1px', '2px'],
   colors: {
     nonMainBackground: 'rgb(0, 21, 41)',
     nonMainText: 'rgba(255, 255, 255, 75%)'
   },
   fontSizes: {
+    errorMessage: '0.75rem',
     formButton: '1rem'
   },
   radii: {
-    card: '8px'
+    card: '8px',
+    circular: '50%'
   },
   shadows: {
     headerScrolled: '0px 4px 8px 0px rgb(128 128 128)',
     headerUnscrolled: '0px 2px 4px 0px rgb(128 128 128)',
-    mainHovered: '0px 0px 5.2px 0px rgb(128 128 128 / 50%)',
+    mainHovered: '0px 0px 5.2px 1.2px rgb(128 128 128 / 50%)',
     mainUnhovered: '0px 0px 0px 1.2px rgb(128 128 128 / 25%)',
-    footerHovered: '0px 0px 8px 0px rgb(128 128 128)',
-    footerUnhovered: '0px 0px 4px 0px rgb(128 128 128)'
+    footerHovered: '0px 0px 5.2px 1.2px rgb(128 128 128 / 75%)',
+    footerUnhovered: '0px 0px 0px 1.2px rgb(128 128 128 / 50%)'
   },
   sizes: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem', '16rem', '32rem'],
   space: ['0rem', '0.5rem', '1rem', '2rem', '4rem', '8rem'],
