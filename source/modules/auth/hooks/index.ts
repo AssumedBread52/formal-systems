e5842df16c585fe934e5bc58ac0568acb682014d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-21 21:53:18 -0600

    Expose new hook

--- source/modules/auth/hooks/index.ts
+++ source/modules/auth/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useSignInUser } from './use-sign-in-user';
 export { useSignInUserMutation } from './use-sign-in-user-mutation';
+export { useSignOutUser } from './use-sign-out-user';
 export { useSignOutUserMutation } from './use-sign-out-user-mutation';
