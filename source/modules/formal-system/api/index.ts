47f202442f2572f7e021fe68e171be28407fbd0e
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-22 21:06:43 -0600

    Added server side props method for edit and delete

--- source/modules/formal-system/api/index.ts
+++ source/modules/formal-system/api/index.ts
@@ -1,4 +1,5 @@
 export { createFormalSystem } from './create-formal-system';
 export { deleteFormalSystem } from './delete-formal-system';
+export { readFormalSystemByUrlPath } from './read-formal-system-by-url-path';
 export { readFormalSystems } from './read-formal-systems';
 export { updateFormalSystem } from './update-formal-system';
---
+++ source/modules/formal-system/api/read-formal-system-by-url-path.ts
@@ -0,0 +1,66 @@
+import { authConfiguration } from '@/auth/constants';
+import { buildMongoUrl, extractUrlPathValue } from '@/common/helpers';
+import { ClientFormalSystem, ServerFormalSystem } from '@/formal-system/types';
+import { MongoClient } from 'mongodb';
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+import { getServerSession } from 'next-auth';
+
+export const readFormalSystemByUrlPath = async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<ClientFormalSystem>> => {
+  try {
+    const { req, res } = context;
+
+    const formalSystemUrlPath = extractUrlPathValue(context, 'formal-system-url-path');
+
+    const session = await getServerSession(req, res, authConfiguration);
+
+    if (!session) {
+      return {
+        redirect: {
+          destination: `/${formalSystemUrlPath}`,
+          permanent: false
+        }
+      };
+    }
+
+    const { id } = session;
+
+    const client = await MongoClient.connect(buildMongoUrl());
+
+    const formalSystemCollection = client.db().collection<ServerFormalSystem>('formal-systems');
+
+    const formalSystem = await formalSystemCollection.findOne({
+      urlPath: formalSystemUrlPath,
+      createdByUserId: id
+    });
+
+    await client.close();
+
+    if (!formalSystem) {
+      return {
+        redirect: {
+          destination: `/${formalSystemUrlPath}`,
+          permanent: false
+        }
+      };
+    }
+
+    const { _id, title, urlPath, description, createdByUserId } = formalSystem;
+
+    return {
+      props: {
+        id: _id.toString(),
+        title,
+        urlPath,
+        description,
+        createdByUserId
+      }
+    };
+  } catch {
+    return {
+      redirect: {
+        destination: '/404',
+        permanent: false
+      }
+    };
+  }
+};
