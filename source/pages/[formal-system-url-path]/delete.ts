bd370106930b3a58dc8748d96eb5d0646429b208
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-22 21:20:14 -0600

    Expose delete and update pages

---
+++ source/pages/[formal-system-url-path]/delete.ts
@@ -0,0 +1,2 @@
+export { readFormalSystemByUrlPath as getServerSideProps } from '@/formal-system/api';
+export { DeleteFormalSystemPage as default } from '@/formal-system/components/delete-formal-system-page/delete-formal-system-page';
---
+++ source/pages/[formal-system-url-path]/edit.ts
@@ -0,0 +1,2 @@
+export { readFormalSystemByUrlPath as getServerSideProps } from '@/formal-system/api';
+export { UpdateFormalSystemPage as default } from '@/formal-system/components/update-formal-system-page/update-formal-system-page';
