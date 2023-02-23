301f9d50f8d4102b1440e1fdd8c7bae678c0dc9f
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-22 20:58:57 -0600

    Added type for tag types on api slice

--- source/modules/app/types/index.ts
+++ source/modules/app/types/index.ts
@@ -1,3 +1,4 @@
 export type { DependencyItemProps } from './dependency-item-props';
 export type { DependencyListProps } from './dependency-list-props';
 export type { RenderMathProps } from './render-math-props';
+export type { TagTypes } from './tag-types';
