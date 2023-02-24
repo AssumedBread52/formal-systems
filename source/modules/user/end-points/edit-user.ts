7a2a5a9e3f49667d677c577b864d4365ff2f6160
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-24 15:31:45 -0600

    Editing user should invalidate the cache

--- source/modules/user/end-points/edit-user.ts
+++ source/modules/user/end-points/edit-user.ts
@@ -2,7 +2,7 @@ import { TagTypes } from '@/app/types';
 import { IdResponse } from '@/common/types';
 import { EditProfilePayload } from '@/user/types';
 import { BaseQueryFn, FetchArgs } from '@reduxjs/toolkit/dist/query';
-import { EndpointBuilder, MutationDefinition } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
+import { EndpointBuilder, MutationDefinition, TagDescription } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 
 export const editUser = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>): MutationDefinition<EditProfilePayload, BaseQueryFn, TagTypes, IdResponse, 'api'> => {
   return builder.mutation<IdResponse, EditProfilePayload>({
@@ -12,6 +12,17 @@ export const editUser = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>)
         method: 'PATCH',
         body: userData
       };
+    },
+    invalidatesTags: (result?: IdResponse): TagDescription<TagTypes>[] => {
+      if (!result) {
+        return [];
+      }
+
+      const { id } = result;
+
+      return [
+        { type: 'user', id }
+      ];
     }
   });
 };
