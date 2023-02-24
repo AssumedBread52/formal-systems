a8a76ec48600dbab449e600def855c6a8135a09d
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-23 19:56:31 -0600

    Rework to use form actions

--- source/modules/formal-system/components/delete-formal-system-page/delete-formal-system-page.tsx
+++ source/modules/formal-system/components/delete-formal-system-page/delete-formal-system-page.tsx
@@ -1,19 +1,16 @@
 import { Box } from '@/common/components/box/box';
-import { Button } from '@/common/components/button/button';
-import { CancelButton } from '@/common/components/cancel-button/cancel-button';
-import { Flex } from '@/common/components/flex/flex';
-import { LoadingSpinner } from '@/common/components/loading-spinner/loading-spinner';
+import { FormActions } from '@/common/components/form-actions/form-actions';
 import { Typography } from '@/common/components/typography/typography';
 import { useDeleteFormalSystem } from '@/formal-system/hooks';
 import { ClientFormalSystem } from '@/formal-system/types';
-import { MouseEvent, ReactElement } from 'react';
+import { FormEvent, ReactElement } from 'react';
 
 export const DeleteFormalSystemPage = (props: ClientFormalSystem): ReactElement => {
   const { id, title } = props;
 
   const { deleteFormalSystem, errorMessage, isLoading } = useDeleteFormalSystem();
 
-  const deleteClickHandler = (event: MouseEvent<HTMLButtonElement>): void => {
+  const submitHandler = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     event.stopPropagation();
 
@@ -28,21 +25,9 @@ export const DeleteFormalSystemPage = (props: ClientFormalSystem): ReactElement 
         <Typography as='h1' textAlign='center' my='3'>
           Delete Formal System: {title}
         </Typography>
-        <Flex display='flex' flexDirection='column' alignItems='center' my='2'>
-          <Button fontSize='formButton' height='3' width='5' position='relative' onClick={deleteClickHandler}>
-            Delete
-            {isLoading && (
-              <Box position='absolute' top='0' left='5' mx='2'>
-                <LoadingSpinner />
-              </Box>
-            )}
-          </Button>
-          <Box my='1' />
-          <CancelButton />
-          <Typography as='p' color='red' height='2'>
-            {errorMessage}
-          </Typography>
-        </Flex>
+        <form onSubmit={submitHandler}>
+          <FormActions disableSubmit={false} submitTitle='Delete' isLoading={isLoading} errorMessage={errorMessage} />
+        </form>
       </section>
     </Box>
   );
