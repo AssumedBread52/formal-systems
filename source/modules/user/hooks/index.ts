80e92ba72cda230ad222f0b6fba814fe1af3b1fd
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-24 15:32:15 -0600

    Extract hook

--- source/modules/user/hooks/index.ts
+++ source/modules/user/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useEditUser } from './use-edit-user';
 export { useEditUserMutation } from './use-edit-user-mutation';
+export { useReadUserByIdQuery } from './use-read-user-by-id-query';
 export { useSignUpUser } from './use-sign-up-user';
 export { useSignUpUserMutation } from './use-sign-up-user-mutation';
