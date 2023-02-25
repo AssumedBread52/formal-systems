e33b650b9e6745248a4f33b59bbd089d79726dbb
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-25 06:29:47 -0600

    Read by url path should not take auth into account

--- source/modules/formal-system/api/read-formal-system-by-url-path.ts
+++ source/modules/formal-system/api/read-formal-system-by-url-path.ts
@@ -1,36 +1,18 @@
-import { authConfiguration } from '@/auth/constants';
 import { buildMongoUrl, extractUrlPathValue } from '@/common/helpers';
 import { ClientFormalSystem, ServerFormalSystem } from '@/formal-system/types';
 import { MongoClient } from 'mongodb';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
-import { getServerSession } from 'next-auth';
 
 export const readFormalSystemByUrlPath = async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<ClientFormalSystem>> => {
   try {
-    const { req, res } = context;
-
     const formalSystemUrlPath = extractUrlPathValue(context, 'formal-system-url-path');
-
-    const session = await getServerSession(req, res, authConfiguration);
-
-    if (!session) {
-      return {
-        redirect: {
-          destination: `/${formalSystemUrlPath}`,
-          permanent: false
-        }
-      };
-    }
-
-    const { id } = session;
 
     const client = await MongoClient.connect(buildMongoUrl());
 
     const formalSystemCollection = client.db().collection<ServerFormalSystem>('formal-systems');
 
     const formalSystem = await formalSystemCollection.findOne({
-      urlPath: formalSystemUrlPath,
-      createdByUserId: id
+      urlPath: formalSystemUrlPath
     });
 
     await client.close();
