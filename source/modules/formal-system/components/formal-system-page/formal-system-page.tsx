8c824810899a1bfff0401da04994391fcd673f46
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-25 06:35:35 -0600

    Added edit and delete buttons

--- source/modules/formal-system/components/formal-system-page/formal-system-page.tsx
+++ source/modules/formal-system/components/formal-system-page/formal-system-page.tsx
@@ -1,18 +1,37 @@
 import { Box } from '@/common/components/box/box';
+import { Flex } from '@/common/components/flex/flex';
+import { HyperLink } from '@/common/components/hyper-link/hyper-link';
 import { Typography } from '@/common/components/typography/typography';
 import { ClientFormalSystem } from '@/formal-system/types';
 import { CreatedBySignature } from '@/user/components/created-by-signature/created-by-signature';
-import { ReactElement } from 'react';
+import { useSession } from 'next-auth/react';
+import { Fragment, ReactElement } from 'react';
 
 export const FormalSystemPage = (props: ClientFormalSystem): ReactElement => {
-  const { title, description, createdByUserId } = props;
+  const { title, urlPath, description, createdByUserId } = props;
+
+  const { data, status } = useSession();
 
   return (
     <Box px='5' minWidth='8'>
       <section>
-        <Typography as='h1'>
-          {title}
-        </Typography>
+        <Flex display='flex' alignItems='center'>
+          <Typography as='h1' m='0'>
+            {title}
+          </Typography>
+          <Box mx='auto' />
+          {'authenticated' === status && createdByUserId === data.id && (
+            <Fragment>
+              <HyperLink title={`Edit ${title}`} href={`/${urlPath}/edit`}>
+                Edit
+              </HyperLink>
+              <Box mx='1' />
+              <HyperLink title={`Delete ${title}`} href={`/${urlPath}/delete`}>
+                Delete
+              </HyperLink>
+            </Fragment>
+          )}
+        </Flex>
         <Typography as='p'>
           {description}
         </Typography>
