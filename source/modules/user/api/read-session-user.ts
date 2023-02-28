cd673a21d5669270c168035b77712c673aaba41b
Antonio Sanchez <asanchez16756@gmail.com>
2023-02-27 21:52:12 -0600

    Simplify get server side props using handler

--- source/modules/user/api/read-session-user.ts
+++ source/modules/user/api/read-session-user.ts
@@ -1,17 +1,17 @@
-import { authConfiguration } from '@/auth/constants';
-import { buildMongoUrl } from '@/common/helpers';
-import { ServerUser, SessionUser } from '@/user/types';
-import { MongoClient, ObjectId } from 'mongodb';
+import { SessionUser } from '@/user/types';
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
-import { getServerSession } from 'next-auth';
 
 export const readSessionUser = async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<SessionUser>> => {
   try {
-    const { req, res } = context;
+    const { req } = context;
 
-    const session = await getServerSession(req, res, authConfiguration);
+    const response = await fetch('http://localhost:3000/api/user/session', {
+      headers: {
+        cookie: req.headers.cookie ?? ''
+      }
+    });
 
-    if (!session) {
+    if (!response.ok) {
       return {
         redirect: {
           destination: '/sign-up',
@@ -20,35 +20,8 @@ export const readSessionUser = async (context: GetServerSidePropsContext): Promi
       };
     }
 
-    const { id } = session;
-
-    const client = await MongoClient.connect(buildMongoUrl());
-
-    const userCollection = client.db().collection<ServerUser>('users');
-
-    const sessionUser = await userCollection.findOne({
-      _id: new ObjectId(id)
-    });
-
-    await client.close();
-
-    if (!sessionUser) {
-      return {
-        redirect: {
-          destination: '/404',
-          permanent: false
-        }
-      };
-    }
-
-    const { firstName, lastName, email } = sessionUser;
-
     return {
-      props: {
-        firstName,
-        lastName,
-        email
-      }
+      props: await response.json()
     };
   } catch {
     return {
