acb59461ca7f887cc17e5112436607440a75d1c1
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-06 11:53:19 -0600

    Remove trailing spaces

--- source/modules/common-back-end/classes/mongo-database.ts
+++ source/modules/common-back-end/classes/mongo-database.ts
@@ -24,11 +24,11 @@ export class MongoDatabase {
     const username = process.env.MONGO_USERNAME;
     const password = process.env.MONGO_PASSWORD;
     const hostname = process.env.MONGO_HOSTNAME;
-  
+
     const encodedPassword = encodeURIComponent(password ?? '');
-  
+
     const credentials = `${username}:${encodedPassword}`;
-  
+
     return `mongodb://${credentials}@${hostname}/formal-systems?authSource=admin`;
   };
 
