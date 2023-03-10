6cc406b35df660f735480da223bbd84da505c4a2
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-10 17:48:04 -0600

    Do not abort on error

--- back-end/modules/main.ts
+++ back-end/modules/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main-module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(MainModule);
+  const app = await NestFactory.create(MainModule, {
+    abortOnError: false
+  });
 
   await app.listen(5000);
 }
