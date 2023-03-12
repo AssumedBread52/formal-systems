7742512f623a59ecbaa9361882242b45e32876cd
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-12 08:25:39 -0500

    Hooked up mongoose nestjs module

--- back-end/modules/main-module.ts
+++ back-end/modules/main-module.ts
@@ -1,9 +1,16 @@
 import { AppModule } from '@/app/app-module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const scheme = process.env.MONGO_SCHEME;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const hostname = process.env.MONGO_HOSTNAME;
 
 @Module({
   imports: [
-    AppModule
+    AppModule,
+    MongooseModule.forRoot(`${scheme}://${username}:${password}@${hostname}/formal-systems?authSource=admin`)
   ]
 })
 export class MainModule {
