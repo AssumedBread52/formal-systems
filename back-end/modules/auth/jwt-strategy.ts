59706e4a00bbc9f8a4a02daf2f00865e4f3519e2
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-13 20:06:30 -0500

    Add strategy for json web token

---
+++ back-end/modules/auth/jwt-strategy.ts
@@ -0,0 +1,29 @@
+import { UserDocument } from '@/user/user-schema';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth-service';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret
+    });
+  }
+
+  async validate(payload: { _id: string; }): Promise<UserDocument | null> {
+    const { _id } = payload;
+
+    const user = await this.authService.validateUserById(_id);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+};
