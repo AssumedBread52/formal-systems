7d832cbbd98a464e9368d6eac2e521be442492a9
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-25 10:15:28 -0500

    Remove clean from down script

--- scripts/down.sh
+++ scripts/down.sh
@@ -4,17 +4,10 @@ if [ "$1" = "--help" ]; then
   echo "Usage: down.sh"
   echo "  Stops the development environment. Stopped containers are removed as well as"
   echo "  the constructed network."
-  echo "  --clean = Will removed generated database files that are normally preserved"
-  echo "            between sessions. WARNING: all data in the database will be lost so"
-  echo "            be certain."
 
   exit
 fi
 
 if [ ! -z "$(docker network ls | awk '/formal-systems/ {print $2}')" ]; then
   GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose down
-fi
-
-if [ "$1" = "--clean" ] && [ "$(ls -A database/data)" ]; then
-  rm database/data/.[!.]* database/data/* -r
 fi
