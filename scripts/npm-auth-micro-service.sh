5d28c08a44efeaadbaaf235e4db21e822b0f2a99
Antonio Sanchez <asanchez16756@gmail.com>
2023-03-30 15:56:47 -0500

    Adjust service names

--- scripts/npm-auth-micro-service.sh
+++ scripts/npm-auth-micro-service.sh
@@ -2,7 +2,7 @@
 
 NETWORK=$(docker network ls | awk '/formal-systems/ {print $2}')
 
-GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-auth-micro-service $@
+GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-micro-service-auth $@
 
 if [ -z $NETWORK ]; then
   GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose down
--- scripts/npm-user-micro-service.sh
+++ scripts/npm-user-micro-service.sh
@@ -2,7 +2,7 @@
 
 NETWORK=$(docker network ls | awk '/formal-systems/ {print $2}')
 
-GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-user-micro-service $@
+GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-micro-service-user $@
 
 if [ -z $NETWORK ]; then
   GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose down
