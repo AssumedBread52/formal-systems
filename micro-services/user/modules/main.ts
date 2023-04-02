bf7c8582ae31f5eb2e78da976b249b52200643a4
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-02 07:37:22 -0500

    Start user micro service

---
+++ micro-services/user/modules/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { MainModule } from './main.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(MainModule, {
+    abortOnError: false
+  });
+
+  await app.listen(5002);
+};
+
+bootstrap();
