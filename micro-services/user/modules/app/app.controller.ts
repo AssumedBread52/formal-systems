cf56c0d7fbfcbf03632c4891e279a6a9f665f1ee
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-02 07:35:49 -0500

    Create health-check api

---
+++ micro-services/user/modules/app/app.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+
+@Controller('app')
+export class AppController {
+  @Get('health-check')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  runHealthCheck(): void {
+  }
+};
