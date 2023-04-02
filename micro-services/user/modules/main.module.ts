06635cc902604ee87081d5c520facb6ce24a9b98
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-02 07:37:10 -0500

    Link user micro service modules together

---
+++ micro-services/user/modules/main.module.ts
@@ -0,0 +1,21 @@
+import { AppModule } from '@/app/app.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+const scheme = process.env.MONGO_SCHEME;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const hostname = process.env.MONGO_HOSTNAME;
+
+const encodedPassword = encodeURIComponent(password ?? '');
+
+@Module({
+  imports: [
+    AppModule,
+    MongooseModule.forRoot(`${scheme}://${username}:${encodedPassword}@${hostname}/formal-systems?authSource=admin`),
+    UserModule
+  ]
+})
+export class MainModule {
+};
