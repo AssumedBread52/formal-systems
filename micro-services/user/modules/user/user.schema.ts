c433d8352584c857b9d815223f68b7d7454219a3
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-02 07:34:14 -0500

    Add user schema to user micro service

---
+++ micro-services/user/modules/user/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop()
+  firstName: string = '';
+  @Prop()
+  lastName: string = '';
+  @Prop()
+  email: string = '';
+  @Prop()
+  hashedPassword: string = '';
+};
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+export type UserDocument = HydratedDocument<User>;
