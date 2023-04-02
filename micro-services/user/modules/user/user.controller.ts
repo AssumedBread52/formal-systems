a9974ef2cc32f3332a1e00993108c37d60ed7885
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-02 07:35:15 -0500

    Expose end-points for user module

---
+++ micro-services/user/modules/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserDocument } from './user.schema';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<UserDocument | null> {
+    return this.userService.readById(id);
+  }
+};
