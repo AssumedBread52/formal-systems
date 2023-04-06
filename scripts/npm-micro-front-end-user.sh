27243179a037bf1e94545d38e6c35b82249585a1
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-06 14:46:15 -0500

    Add npm script for front ends

---
+++ scripts/npm-micro-front-end-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NETWORK=$(docker network ls | awk '/formal-systems/ {print $2}')
+
+GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-micro-front-end-user $@
+
+if [ -z $NETWORK ]; then
+  GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose down
+fi
