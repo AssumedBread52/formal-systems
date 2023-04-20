0e6b6e7cfb8cfd9eb079c6b9f48068199bb54611
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-20 14:10:10 -0500

    Take label for user signature

--- micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
+++ micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
@@ -1,12 +1,15 @@
+import { UserSignatureProps } from '@/user/types';
 import { Typography } from 'antd';
 import { ReactElement } from 'react';
 
 const { Text } = Typography;
 
-export const UserSignature = (): ReactElement => {
+export const UserSignature = (props: UserSignatureProps): ReactElement => {
+  const { label } = props;
+
   return (
     <Text>
-      User Signature!
+      {label}
     </Text>
   );
 };
