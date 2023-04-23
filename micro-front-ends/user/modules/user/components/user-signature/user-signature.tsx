3c17dd5f535f126422e27997fa45ef50d127974e
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-23 08:02:08 -0500

    Use data for signature

--- micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
+++ micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
@@ -1,3 +1,4 @@
+import { useReadUserByIdQuery } from '@/user/hooks';
 import { UserSignatureProps } from '@/user/types';
 import { Card } from 'antd';
 import { ReactElement } from 'react';
@@ -7,7 +8,12 @@ const { Meta } = Card;
 export const UserSignature = (props: UserSignatureProps): ReactElement => {
   const { label = 'Test Label', userId = 'Test User ID' } = props;
 
+  const { data, isError, isSuccess } = useReadUserByIdQuery(userId);
+
+  const title = isSuccess ? label : (isError ? 'Error' : '');
+  const description = isSuccess ? `${data.firstName} ${data.lastName}` : (!isError ? 'Loading...' : '');
+
   return (
-    <Meta title={label} description={userId} />
+    <Meta title={title} description={description} />
   );
 };
