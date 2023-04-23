d84f9e5ef598131cd0f5b9822dabf2748bf2fe74
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-23 15:00:55 -0500

    Remove get by id from user controller

--- back-end/modules/user/user-controller.ts
+++ back-end/modules/user/user-controller.ts
@@ -1,22 +1,16 @@
 import { SessionUser } from '@/auth/decorators';
 import { JwtAuthGuard } from '@/auth/guards';
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserDocument } from './user-schema';
-import { UserService } from './user-service';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {
+  constructor() {
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('session')
   getProfile(@SessionUser() sessionUser: UserDocument): UserDocument {
     return sessionUser;
-  }
-
-  @Get(':id')
-  async getById(@Param('id') id: string): Promise<UserDocument | null> {
-    return this.userService.readById(id);
   }
 };
