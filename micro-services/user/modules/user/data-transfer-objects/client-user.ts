8729525bf956ccd53a806c67652441cf028bca3a
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-23 08:34:22 -0500

    Add data transfer object type

---
+++ micro-services/user/modules/user/data-transfer-objects/client-user.ts
@@ -0,0 +1,22 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { UserDocument } from '../user.schema';
+
+export class ClientUser {
+  @IsNotEmpty()
+  id: string = '';
+  @IsNotEmpty()
+  firstName: string = '';
+  @IsNotEmpty()
+  lastName: string = '';
+  @IsEmail()
+  email: string = '';
+
+  constructor(userDocument: UserDocument) {
+    const { _id, firstName, lastName, email } = userDocument;
+
+    this.id = _id.toString();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+};
