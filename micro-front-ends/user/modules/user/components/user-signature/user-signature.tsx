e7221d1c1fdcf370c9849d9ae2b6ccea70be0004
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-24 16:39:37 -0500

    Improve user signature

--- micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
+++ micro-front-ends/user/modules/user/components/user-signature/user-signature.tsx
@@ -1,19 +1,36 @@
 import { useReadUserByIdQuery } from '@/user/hooks';
 import { UserSignatureProps } from '@/user/types';
-import { Card } from 'antd';
+import { Card, Typography } from 'antd';
 import { ReactElement } from 'react';
 
 const { Meta } = Card;
+const { Text } = Typography;
 
 export const UserSignature = (props: UserSignatureProps): ReactElement => {
   const { label = 'Test Label', userId = 'Test User ID' } = props;
 
   const { data, isError, isSuccess } = useReadUserByIdQuery(userId);
 
-  const title = isSuccess ? label : (isError ? 'Error' : '');
-  const description = isSuccess ? `${data.firstName} ${data.lastName}` : (!isError ? 'Loading...' : '');
+  let title = '';
+  let description = 'Loading...';
+  if (isSuccess) {
+    const { firstName, lastName } = data;
+
+    title = label;
+    description = `${firstName} ${lastName}`;
+  } else if (isError) {
+    title = 'Error';
+    description = 'Failed to pull user data.';
+  }
 
   return (
-    <Meta title={title} description={description} />
+    <Meta
+      title={title}
+      description={
+        <Text>
+          {description}
+        </Text>
+      }
+    />
   );
 };
