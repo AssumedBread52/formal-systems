ffd1e8fea33384624bb848679412dce9dcd6aa55
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-25 16:20:04 -0500

    Switch user micro service to use function

--- scripts/up.sh
+++ scripts/up.sh
@@ -18,14 +18,12 @@ fi
 
 ENVIRONMENT_VARIABLES_CKSM_FILE=check-sums/environment-variables.cksm
 DATABASE_CKSM_FILE=check-sums/database.cksm
-MICRO_SERVICE_USER_NODE_MODULES_CKSM_FILE=check-sums/micro-service-user.cksm
 BACK_END_NODE_MODULES_CKSM_FILE=check-sums/back-end-node-modules.cksm
 MICRO_FRONT_END_AUTH_NODE_MODULES_CKSM_FILE=check-sums/micro-front-end-auth.cksm
 MICRO_FRONT_END_USER_NODE_MODULES_CKSM_FILE=check-sums/micro-front-end-user.cksm
 APPLICATION_NODE_MODULES_CKSM_FILE=check-sums/application-node-modules.cksm
 CURRENT_ENVIRONMENT_VARIABLES_CKSM=$(cat scripts/generate-environment-variables.sh | sha1sum)
 CURRENT_DATABASE_CKSM=$(cat database/initialization-scripts/* | sha1sum)
-CURRENT_MICRO_SERVICE_USER_NODE_MODULES_CKSM=$(cat micro-services/user/package-lock.json | sha1sum)
 CURRENT_BACK_END_NODE_MODULES_CKSM=$(cat back-end/package-lock.json | sha1sum)
 CURRENT_MICRO_FRONT_END_AUTH_NODE_MODULES_CKSM=$(cat micro-front-ends/auth/package-lock.json | sha1sum)
 CURRENT_MICRO_FRONT_END_USER_NODE_MODULES_CKSM=$(cat micro-front-ends/user/package-lock.json | sha1sum)
@@ -36,7 +34,6 @@ if [ ! -d check-sums ]; then
 else
   OLD_ENVIRONMENT_VARIABLES_CKSM=$(cat $ENVIRONMENT_VARIABLES_CKSM_FILE)
   OLD_INITIALIZE_DATABASE_CKSM=$(cat $DATABASE_CKSM_FILE)
-  OLD_MICRO_SERVICE_USER_NODE_MODULES_CKSM=$(cat $MICRO_SERVICE_USER_NODE_MODULES_CKSM_FILE)
   OLD_BACK_END_NODE_MODULES_CKSM=$(cat $BACK_END_NODE_MODULES_CKSM_FILE)
   OLD_MICRO_FRONT_END_AUTH_NODE_MODULES_CKSM=$(cat $MICRO_FRONT_END_AUTH_NODE_MODULES_CKSM_FILE)
   OLD_MICRO_FRONT_END_USER_NODE_MODULES_CKSM=$(cat $MICRO_FRONT_END_USER_NODE_MODULES_CKSM_FILE)
@@ -95,18 +92,7 @@ function resolve_node_dependencies() {
 }
 
 resolve_node_dependencies service auth .nest
-
-if [ ! -d micro-services/user/node_modules ]; then
-  GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-micro-service-user install
-
-  echo -n "$CURRENT_MICRO_SERVICE_USER_NODE_MODULES_CKSM" > $MICRO_SERVICE_USER_NODE_MODULES_CKSM_FILE
-elif [ ! "$OLD_MICRO_SERVICE_USER_NODE_MODULES_CKSM" = "$CURRENT_MICRO_SERVICE_USER_NODE_MODULES_CKSM" ]; then
-  rm -rf micro-services/user/node_modules micro-services/user/.nest
-
-  GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-micro-service-user install
-
-  echo -n "$CURRENT_MICRO_SERVICE_USER_NODE_MODULES_CKSM" > $MICRO_SERVICE_USER_NODE_MODULES_CKSM_FILE
-fi
+resolve_node_dependencies service user .nest
 
 if [ ! -d back-end/node_modules ]; then
   GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-back-end install
