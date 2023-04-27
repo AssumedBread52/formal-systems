dbfbb971bf8bb2f18e6083af113e4f508b2a42d8
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-27 15:40:53 -0500

    Hook up end point to the api slice

--- micro-front-ends/auth/modules/auth/auth-api.ts
+++ micro-front-ends/auth/modules/auth/auth-api.ts
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { signInUser } from '@/auth/end-points';
 
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:5001/auth'
   }),
-  endpoints: () => {
+  endpoints: (builder) => {
     return {
+      signInUser: signInUser(builder)
     };
   },
   reducerPath: 'auth'
