6d8bd7fcb1f0b78d65b975155dee6ef69aac4bdc
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-28 14:28:23 -0500

    Add types

--- micro-front-ends/auth/modules/auth/end-points/sign-in-user.ts
+++ micro-front-ends/auth/modules/auth/end-points/sign-in-user.ts
@@ -1,9 +1,9 @@
+import { SignInPayload, TokenPayload } from '@/auth/types';
 import { BaseQueryFn, FetchArgs } from '@reduxjs/toolkit/dist/query';
 import { EndpointBuilder, MutationDefinition } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
-import { SignInPayload } from '@/auth/types';
 
-export const signInUser = (builder: EndpointBuilder<BaseQueryFn, never, 'auth'>): MutationDefinition<SignInPayload, BaseQueryFn, never, boolean, 'auth'> => {
-  return builder.mutation({
+export const signInUser = (builder: EndpointBuilder<BaseQueryFn, 'auth', 'auth'>): MutationDefinition<SignInPayload, BaseQueryFn, 'auth', TokenPayload, 'auth'> => {
+  return builder.mutation<TokenPayload, SignInPayload>({
     query: (signInPayload: SignInPayload): FetchArgs => {
       return {
         url: '/login',
