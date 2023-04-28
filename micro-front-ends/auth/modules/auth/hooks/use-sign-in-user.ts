75ad65f26946f2cddd70fe1838c4297a1ac4650e
Antonio Sanchez <asanchez16756@gmail.com>
2023-04-28 14:28:11 -0500

    Add return type

--- micro-front-ends/auth/modules/auth/hooks/use-sign-in-user.ts
+++ micro-front-ends/auth/modules/auth/hooks/use-sign-in-user.ts
@@ -1,7 +1,14 @@
+import { SignInPayload, TokenPayload } from '@/auth/types';
+import { BaseQueryFn, MutationDefinition } from '@reduxjs/toolkit/dist/query';
+import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 import { useRouteOnSuccess } from './use-route-on-success';
 import { useSignInUserMutation } from './use-sign-in-user-mutation';
 
-export const useSignInUser = () => {
+export const useSignInUser = (): {
+  signInUser: MutationTrigger<MutationDefinition<SignInPayload, BaseQueryFn, 'auth', TokenPayload, 'auth'>>;
+  errorMessage: string;
+  isLoading: boolean;
+} => {
   const [signInUser, { isError, isLoading, isSuccess }] = useSignInUserMutation();
 
   useRouteOnSuccess(isSuccess);
