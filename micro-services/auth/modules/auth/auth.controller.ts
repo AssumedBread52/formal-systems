52f0f6a6518959cf23af7f651b39cf0eb6659a2a
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-01 17:42:42 -0500

    Implemented login method in auth service

--- micro-services/auth/modules/auth/auth.controller.ts
+++ micro-services/auth/modules/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { JwtPayload, SignInPayload } from './data-transfer-objects';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {
+  }
+
   @Post('login')
-  login(): {
-    token: string;
-  } {
-    return {
-      token: 'Generated token!'
-    };
+  login(@Body(new ValidationPipe()) signInPayload: SignInPayload): Promise<JwtPayload> {
+    const { email, password } = signInPayload;
+
+    return this.authService.signIn(email, password);
   }
 };
