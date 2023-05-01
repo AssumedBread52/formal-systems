334277fd10b57ba1d3ca9fe9d4bad34995e9b4ca
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-01 17:16:48 -0500

    Recieve messages from redis looking up user by email

--- micro-services/user/modules/user/user.controller.ts
+++ micro-services/user/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ClientUser } from './data-transfer-objects';
+import { UserDocument } from './user.schema';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -16,5 +18,16 @@ export class UserController {
     }
 
     return new ClientUser(user);
+  }
+
+  @MessagePattern('READ_USER_BY_EMAIL')
+  async getByEmail(@Payload() email: string): Promise<UserDocument> {
+    const user = await this.userService.readByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
   }
 };
