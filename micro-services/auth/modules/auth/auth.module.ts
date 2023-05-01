eadf796b9780c365acaa0e27f333694d9e717792
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-01 17:41:58 -0500

    Hook up jwt module

--- micro-services/auth/modules/auth/auth.module.ts
+++ micro-services/auth/modules/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -15,7 +16,14 @@ import { AuthService } from './auth.service';
           password: process.env.REDIS_PASSWORD
         }
       }
-    ])
+    ]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JSON_WEB_TOKEN_SECRET,
+      signOptions: {
+        expiresIn: '60s'
+      }
+    })
   ],
   controllers: [
     AuthController
