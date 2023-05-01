b1cac731ea09e1155e4c39d8a97691b7b0238012
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-01 17:27:52 -0500

    Hookup clients module

--- micro-services/auth/modules/auth/auth.module.ts
+++ micro-services/auth/modules/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MESSAGING_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          host: process.env.REDIS_HOSTNAME,
+          port: 6379,
+          password: process.env.REDIS_PASSWORD
+        }
+      }
+    ])
+  ],
   controllers: [
     AuthController
   ],
