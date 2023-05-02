957f8bd4c5b972bdc89c72a08141c844647b7460
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-02 16:57:28 -0500

    Hook up strategy to user module

--- micro-services/user/modules/user/user.module.ts
+++ micro-services/user/modules/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtStrategy } from './strategies';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
@@ -14,6 +15,7 @@ import { UserService } from './user.service';
     UserController
   ],
   providers: [
+    JwtStrategy,
     UserService
   ]
 })
