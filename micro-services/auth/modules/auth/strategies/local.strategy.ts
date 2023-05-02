74a2012cfa0342261e55920b5e18e7590eb17f41
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-02 15:51:32 -0500

    Remove type cast

--- micro-services/auth/modules/auth/strategies/local.strategy.ts
+++ micro-services/auth/modules/auth/strategies/local.strategy.ts
@@ -2,7 +2,7 @@ import { AuthService } from '@/auth/auth.service';
 import { ServerUser } from '@/auth/data-transfer-objects';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { IStrategyOptions, Strategy } from 'passport-local';
+import { Strategy } from 'passport-local';
 import { BaseStrategy } from './base.strategy';
 
 @Injectable()
@@ -10,7 +10,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) implements BaseStr
   constructor(private authService: AuthService) {
     super({
       usernameField: 'email'
-    } as IStrategyOptions);
+    });
   }
 
   validate(email: string, password: string): Promise<ServerUser> {
