79ac36be7b533aedb24f9afc8677796102879833
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-02 15:46:11 -0500

    Add guard to the sign in path

--- micro-services/auth/modules/auth/auth.controller.ts
+++ micro-services/auth/modules/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtPayload, ServerUser } from './data-transfer-objects';
 import { SessionUser } from './decorators';
+import { LocalGuard } from './guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {
   }
 
+  @UseGuards(LocalGuard)
   @Post('sign-in')
   signIn(@SessionUser() sessionUser: ServerUser): Promise<JwtPayload> {
     return this.authService.signIn(sessionUser);
