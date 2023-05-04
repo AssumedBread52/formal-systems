88e7507d6514484706b6a2e55432a43a6a11fc62
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-03 19:38:05 -0500

    Import types from micro front end

--- application/@mf-types/auth/_types/hooks/index.d.ts
+++ application/@mf-types/auth/_types/hooks/index.d.ts
@@ -1,3 +1,5 @@
 export { useRouteOnSuccess } from './use-route-on-success';
 export { useSignInUser } from './use-sign-in-user';
 export { useSignInUserMutation } from './use-sign-in-user-mutation';
+export { useSignUpUser } from './use-sign-up-user';
+export { useSignUpUserMutation } from './use-sign-up-user-mutation';
--- application/@mf-types/auth/_types/types/index.d.ts
+++ application/@mf-types/auth/_types/types/index.d.ts
@@ -1,2 +1,3 @@
 export type { SignInPayload } from './sign-in-payload';
+export type { SignUpPayload } from './sign-up-payload';
 export type { TokenPayload } from './token-payload';
