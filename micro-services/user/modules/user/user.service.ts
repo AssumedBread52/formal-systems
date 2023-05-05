6e663a2e5cf6242c40b170b0e36002d095ea905a
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-05 15:13:31 -0500

    Add update method

--- micro-services/user/modules/user/user.service.ts
+++ micro-services/user/modules/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { hash } from 'bcryptjs';
 import { Model } from 'mongoose';
+import { EditProfilePayload } from './data-transfer-objects';
 import { User, UserDocument } from './user.schema';
 
 @Injectable()
@@ -22,5 +24,18 @@ export class UserService {
 
   readById(id: string): Promise<UserDocument | null> {
     return this.userModel.findById(id).exec();
+  }
+
+  async update(sessionUser: UserDocument, editProfilePayload: EditProfilePayload): Promise<UserDocument> {
+    const { firstName, lastName, email, password } = editProfilePayload;
+
+    sessionUser.firstName = firstName;
+    sessionUser.lastName = lastName;
+    sessionUser.email = email;
+    if (password) {
+      sessionUser.hashedPassword = await hash(password, 12);
+    }
+
+    return sessionUser.save();
   }
 };
