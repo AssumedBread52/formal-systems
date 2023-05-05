fdc538d1ca342d5c8faeabf3e23bff3801e5305e
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-04 19:42:34 -0500

    Add sign out end point

---
+++ micro-front-ends/auth/modules/auth/end-points/sign-out-user.ts
@@ -0,0 +1,13 @@
+import { BaseQueryFn, FetchArgs } from '@reduxjs/toolkit/dist/query';
+import { EndpointBuilder, MutationDefinition } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
+
+export const signOutUser = (builder: EndpointBuilder<BaseQueryFn, 'auth', 'auth'>): MutationDefinition<void, BaseQueryFn, 'auth', void, 'auth'> => {
+  return builder.mutation<void, void>({
+    query: (): FetchArgs => {
+      return {
+        url: '/sign-in',
+        method: 'POST'
+      };
+    }
+  });
+};
