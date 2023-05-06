bc9dfbff7f587630da189c117324d4fb54b4920f
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-06 18:52:59 -0500

    If token present refresh on mount

--- micro-front-ends/auth/modules/auth/components/auth-provider/auth-provider.tsx
+++ micro-front-ends/auth/modules/auth/components/auth-provider/auth-provider.tsx
@@ -1,9 +1,18 @@
+import { authApi } from '@/auth/auth-api';
 import { authStore } from '@/auth/auth-store';
-import { PropsWithChildren, ReactElement } from 'react';
+import { PropsWithChildren, ReactElement, useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 export const AuthProvider = (props: PropsWithChildren): ReactElement => {
   const { children } = props;
+
+  useEffect((): void => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      authStore.dispatch(authApi.endpoints.refreshToken.initiate());
+    }
+  }, []);
 
   return (
     <Provider store={authStore}>
