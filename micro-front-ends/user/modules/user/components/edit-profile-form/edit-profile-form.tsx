7f94a6e95869f6efe5e9357b1c17862c85eaea5f
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-05 20:22:54 -0500

    Add edit profile form

---
+++ micro-front-ends/user/modules/user/components/edit-profile-form/edit-profile-form.tsx
@@ -0,0 +1,70 @@
+import { useEditProfile, useReadSessionUser } from '@/user/hooks';
+import { EditProfilePayload } from '@/user/types';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Alert, Button, Card, Form, FormInstance, Input, Space, Spin } from 'antd';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+
+const { Item } = Form;
+const { Password } = Input;
+
+const TypedForm = Form<EditProfilePayload>;
+
+export const EditProfileForm = () => {
+  const formRef = useRef<FormInstance<EditProfilePayload>>(null);
+
+  const { back } = useRouter();
+
+  const { data, loading } = useReadSessionUser();
+
+  const { editProfile, errorMessage, isLoading } = useEditProfile();
+
+  const finishHandler = (editProfilePayload: EditProfilePayload): void => {
+    editProfile(editProfilePayload);
+  };
+
+  const resetHandler = (): void => {
+    if (formRef.current) {
+      formRef.current.resetFields();
+    }
+  };
+
+  return (
+    <Spin spinning={isLoading} size='large' indicator={<LoadingOutlined spin />}>
+      <Card title='Edit Profile' loading={loading} headStyle={{ textAlign: 'center' }} style={{ maxWidth: '600px', width: '50vw', marginLeft: 'auto', marginRight: 'auto' }}>
+        <TypedForm ref={formRef} labelCol={{ span: 8 }} initialValues={data} onFinish={finishHandler}>
+          <Item label='First Name' name='newFirstName'>
+            <Input />
+          </Item>
+          <Item label='Last Name' name='newLastName'>
+            <Input />
+          </Item>
+          <Item label='E-mail' name='newEmail' rules={[
+            { type: 'email', message: 'Invalid format' }
+          ]}>
+            <Input />
+          </Item>
+          <Item label='Password' name='newPassword'>
+            <Password />
+          </Item>
+          <Item wrapperCol={{ offset: 8 }}>
+            <Space wrap>
+              <Button type='primary' htmlType='submit'>
+                Submit
+              </Button>
+              <Button htmlType='button' onClick={resetHandler}>
+                Reset
+              </Button>
+              <Button htmlType='button' onClick={back}>
+                Cancel
+              </Button>
+            </Space>
+          </Item>
+        </TypedForm>
+        {errorMessage && (
+          <Alert message='Error' description={errorMessage} type='error' showIcon closable />
+        )}
+      </Card>
+    </Spin>
+  );
+};
---
+++ micro-front-ends/user/pages/edit-profile-form.ts
@@ -0,0 +1,1 @@
+export { EditProfileForm as default } from '@/user/components/edit-profile-form/edit-profile-form';
