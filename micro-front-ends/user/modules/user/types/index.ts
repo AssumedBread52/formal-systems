89dfaae251a6260dca037d466aa619ce20277b84
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-05 19:55:54 -0500

    Add type for edit profile request

--- micro-front-ends/user/modules/user/types/index.ts
+++ micro-front-ends/user/modules/user/types/index.ts
@@ -1,2 +1,3 @@
 export type { ClientUser } from './client-user';
+export type { EditProfilePayload } from './edit-profile-payload';
 export type { UserSignatureProps } from './user-signature-props';
