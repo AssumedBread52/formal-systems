a33a653cd7d0e750883f3dd72578d0cc4aa30163
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-05 20:36:58 -0500

    Attach authorization token to all requests

--- micro-front-ends/auth/modules/auth/auth-api.ts
+++ micro-front-ends/auth/modules/auth/auth-api.ts
@@ -3,7 +3,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5001/auth'
+    baseUrl: 'http://localhost:5001/auth',
+    prepareHeaders: (headers: Headers): void => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+    }
   }),
   endpoints: (builder) => {
     return {
