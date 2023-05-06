0ab34790c136dd044f18fa6ebf5b303891fa7a5b
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-05 19:51:08 -0500

    Typed the return

--- micro-front-ends/user/modules/user/hooks/use-read-user-by-id.ts
+++ micro-front-ends/user/modules/user/hooks/use-read-user-by-id.ts
@@ -1,6 +1,10 @@
-import { useReadUserByIdQuery } from "./use-read-user-by-id-query";
+import { ClientUser } from '@/user/types';
+import { useReadUserByIdQuery } from './use-read-user-by-id-query';
 
-export const useReadUserById = (userId: string) => {
+export const useReadUserById = (userId: string): {
+  data?: ClientUser;
+  loading: boolean;
+} => {
   const { data, isError, isSuccess } = useReadUserByIdQuery(userId);
 
   return {
