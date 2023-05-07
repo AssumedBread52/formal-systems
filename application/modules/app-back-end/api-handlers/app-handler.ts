80dc0b0316de6ffd6c8a7d8e6b28aa20c1ab258f
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 17:06:43 -0500

    Simplify health check

--- application/modules/app-back-end/api-handlers/app-handler.ts
+++ application/modules/app-back-end/api-handlers/app-handler.ts
@@ -1,14 +1,8 @@
-import { MongoDatabase } from '@/common-back-end/classes';
 import { Get, HttpCode } from 'next-api-decorators';
 
 export class AppHandler {
-  private mongoDatabase: MongoDatabase = new MongoDatabase();
-
   @Get()
   @HttpCode(204)
-  async healthCheck(): Promise<void> {
-    const db = await this.mongoDatabase.getDb();
-
-    await db.stats();
+  runhealthCheck(): void {
   }
 };
