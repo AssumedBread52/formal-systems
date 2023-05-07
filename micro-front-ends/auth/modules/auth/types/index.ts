f0ad0c4aadd0160ecd0b42fe5519070eb91e9148
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 14:32:41 -0500

    Add new type for new component

--- micro-front-ends/auth/modules/auth/types/index.ts
+++ micro-front-ends/auth/modules/auth/types/index.ts
@@ -1,4 +1,5 @@
 export type { IdPayload } from './id-payload';
+export type { ProtectedContentProps } from './protected-content-props';
 export type { SignInPayload } from './sign-in-payload';
 export type { SignUpPayload } from './sign-up-payload';
 export type { TokenPayload } from './token-payload';
