05534c7379c442cdc3902b78ef656197865efccb
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 16:27:35 -0500

    Added the ability to invert auth check

--- micro-front-ends/auth/modules/auth/components/protected-content/protected-content.tsx
+++ micro-front-ends/auth/modules/auth/components/protected-content/protected-content.tsx
@@ -3,13 +3,13 @@ import { ProtectedContentProps } from '@/auth/types';
 import { Fragment, PropsWithChildren, ReactElement } from 'react';
 
 export const ProtectedContent = (props: PropsWithChildren<ProtectedContentProps>): ReactElement => {
-  const { userId, children } = props;
+  const { invert, userId, children } = props;
 
   const authorized = useReadSessionUserId(userId);
 
   return (
     <Fragment>
-      {authorized && children}
+      {authorized && !invert && children}
     </Fragment>
   );
 };
