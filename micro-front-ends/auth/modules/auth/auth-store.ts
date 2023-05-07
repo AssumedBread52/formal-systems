5ea591a6e624a2523d2c9f121a5430878a56ad7e
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 12:52:22 -0500

    Move refresh token rejection to generic middleware

--- micro-front-ends/auth/modules/auth/auth-store.ts
+++ micro-front-ends/auth/modules/auth/auth-store.ts
@@ -15,7 +15,6 @@ const saveToken = (token: string): void => {
 
 const refreshTokenMiddleware = createListenerMiddleware();
 const signInMiddleware = createListenerMiddleware();
-const expireMiddleware = createListenerMiddleware();
 
 refreshTokenMiddleware.startListening({
   matcher: authApi.endpoints.refreshToken.matchFulfilled,
@@ -37,23 +36,12 @@ signInMiddleware.startListening({
   }
 });
 
-expireMiddleware.startListening({
-  matcher: authApi.endpoints.refreshToken.matchRejected,
-  effect: () => {
-    localStorage.removeItem('token');
-
-    if (refreshTimeout) {
-      clearTimeout(refreshTimeout);
-    }
-  }
-})
-
 export const authStore = configureStore({
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware).concat(signInMiddleware.middleware).concat(refreshTokenMiddleware.middleware).concat(expireMiddleware.middleware).concat((_) => {
+    return getDefaultMiddleware().concat(authApi.middleware).concat(signInMiddleware.middleware).concat(refreshTokenMiddleware.middleware).concat((_) => {
       return (next) => {
         return (action) => {
-          if (authApi.endpoints.signOutUser.matchFulfilled(action)) {
+          if (authApi.endpoints.refreshToken.matchRejected(action) || authApi.endpoints.signOutUser.matchFulfilled(action)) {
             localStorage.removeItem('token');
 
             if (refreshTimeout) {
