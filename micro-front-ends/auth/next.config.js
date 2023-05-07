9367369f53db303c83fbf5b4dac67de2ca235e49
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 14:33:21 -0500

    Expose component for importing

--- micro-front-ends/auth/next.config.js
+++ micro-front-ends/auth/next.config.js
@@ -15,6 +15,7 @@ module.exports = {
     config.plugins.push(new NextFederationPlugin({
       exposes: {
         './auth-provider': './modules/auth/components/auth-provider/auth-provider',
+        './protected-content': './modules/auth/components/protected-content/protected-content',
         './sign-in-form': './modules/auth/components/sign-in-form/sign-in-form',
         './sign-out-form': './modules/auth/components/sign-out-form/sign-out-form',
         './sign-up-form': './modules/auth/components/sign-up-form/sign-up-form'
@@ -26,6 +27,7 @@ module.exports = {
       federationConfig: {
         exposes: {
           './auth-provider': './modules/auth/components/auth-provider/auth-provider',
+          './protected-content': './modules/auth/components/protected-content/protected-content',
           './sign-in-form': './modules/auth/components/sign-in-form/sign-in-form',
           './sign-out-form': './modules/auth/components/sign-out-form/sign-out-form',
           './sign-up-form': './modules/auth/components/sign-up-form/sign-up-form'
@@ -38,6 +40,7 @@ module.exports = {
       federationConfig: {
         exposes: {
           './auth-provider': './modules/auth/components/auth-provider/auth-provider',
+          './protected-content': './modules/auth/components/protected-content/protected-content',
           './sign-in-form': './modules/auth/components/sign-in-form/sign-in-form',
           './sign-out-form': './modules/auth/components/sign-out-form/sign-out-form',
           './sign-up-form': './modules/auth/components/sign-up-form/sign-up-form'
