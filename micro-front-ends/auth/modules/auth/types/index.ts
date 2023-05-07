058570e0f3a2368ba256674483b484f369d7afef
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-07 14:30:24 -0500

    Add id payload type

--- micro-front-ends/auth/modules/auth/types/index.ts
+++ micro-front-ends/auth/modules/auth/types/index.ts
@@ -1,3 +1,4 @@
+export type { IdPayload } from './id-payload';
 export type { SignInPayload } from './sign-in-payload';
 export type { SignUpPayload } from './sign-up-payload';
 export type { TokenPayload } from './token-payload';
