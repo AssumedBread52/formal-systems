f3356f35ab54c434849145e748044b667bd49503
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-08 21:06:08 -0500

    Add mongoose module to the service

--- micro-services/formal-system/modules/main.module.ts
+++ micro-services/formal-system/modules/main.module.ts
@@ -1,9 +1,18 @@
 import { AppModule } from '@/app/app.module';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const scheme = process.env.MONGO_SCHEME;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const hostname = process.env.MONGO_HOSTNAME;
+
+const encodedPassword = encodeURIComponent(password ?? '');
 
 @Module({
   imports: [
-    AppModule
+    AppModule,
+    MongooseModule.forRoot(`${scheme}://${username}:${encodedPassword}@${hostname}/formal-systems?authSource=admin`)
   ]
 })
 export class MainModule {
