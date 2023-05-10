bbc5d8e7ea35552c7536f3bd609ba51332a36b5d
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-09 20:17:55 -0500

    Add save route in formal system service

--- micro-services/formal-system/modules/formal-system/formal-system.controller.ts
+++ micro-services/formal-system/modules/formal-system/formal-system.controller.ts
@@ -1,5 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { NewFormalSystemPayload } from './data-transfer-objects';
+import { SessionUserId } from './decorators';
+import { FormalSystemService } from './formal-system.service';
+import { JwtGuard } from './guards';
 
 @Controller('formal-system')
 export class FormalSystemController {
+  constructor(private formalSystemService: FormalSystemService) {
+  }
+
+  @UseGuards(JwtGuard)
+  @Post()
+  createFormalSystem(@SessionUserId() sessionUserId: string, @Body(new ValidationPipe()) newFormalSystemPayload: NewFormalSystemPayload) {
+    const { title, description } = newFormalSystemPayload;
+
+    this.formalSystemService.create({
+      title,
+      urlPath: this.formalSystemService.buildUrlPath(title),
+      description,
+      createdByUserId: sessionUserId
+    });
+  }
 };
--- micro-services/formal-system/modules/formal-system/formal-system.service.ts
+++ micro-services/formal-system/modules/formal-system/formal-system.service.ts
@@ -1,5 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FormalSystem, FormalSystemDocument } from './formal-system.schema';
 
 @Injectable()
 export class FormalSystemService {
+  constructor(@InjectModel(FormalSystem.name) private formalSystemModel: Model<FormalSystemDocument>) {
+  }
+
+  buildUrlPath(title: string) {
+    return title.split(' ').join('-').toLowerCase();
+  }
+
+  create(formalSystem: FormalSystem): Promise<FormalSystemDocument> {
+    const newFormalSystem = new this.formalSystemModel(formalSystem);
+
+    return newFormalSystem.save();
+  }
 };
