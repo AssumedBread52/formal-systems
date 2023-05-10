4a6c82160dfde78204f5f7de8c1e1306d0a0e6da
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-09 19:46:45 -0500

    Add formal system feature to module

--- micro-services/formal-system/modules/formal-system/formal-system.module.ts
+++ micro-services/formal-system/modules/formal-system/formal-system.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FormalSystemController } from './formal-system.controller';
+import { FormalSystem, FormalSystemSchema } from './formal-system.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: FormalSystem.name, schema: FormalSystemSchema }
+    ])
+  ],
   controllers: [
     FormalSystemController
   ]
