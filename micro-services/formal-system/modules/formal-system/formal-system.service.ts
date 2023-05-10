644e1d1f43c4fbda0727e376f89af6ebc6b2d46c
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-10 09:48:16 -0500

    Add conflict checking in service

--- micro-services/formal-system/modules/formal-system/formal-system.service.ts
+++ micro-services/formal-system/modules/formal-system/formal-system.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { FormalSystem, FormalSystemDocument } from './formal-system.schema';
@@ -8,9 +8,27 @@ export class FormalSystemService {
   constructor(@InjectModel(FormalSystem.name) private formalSystemModel: Model<FormalSystemDocument>) {
   }
 
-  create(formalSystem: FormalSystem): Promise<FormalSystemDocument> {
+  async checkForConflict(urlPath: string): Promise<void> {
+    const collision = await this.readByUrlPath(urlPath);
+
+    if (collision) {
+      throw new ConflictException();
+    }
+  }
+
+  async create(formalSystem: FormalSystem): Promise<FormalSystemDocument> {
+    const { urlPath } = formalSystem;
+
+    await this.checkForConflict(urlPath);
+
     const newFormalSystem = new this.formalSystemModel(formalSystem);
 
     return newFormalSystem.save();
+  }
+
+  readByUrlPath(urlPath: string): Promise<FormalSystemDocument | null> {
+    return this.formalSystemModel.findOne({
+      urlPath
+    }).exec();
   }
 };
