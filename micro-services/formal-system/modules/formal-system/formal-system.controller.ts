02235e0064b01c8c12b6c951d24e33396d9addfb
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-09 19:36:48 -0500

    Add controller to the formal system service

---
+++ micro-services/formal-system/modules/formal-system/formal-system.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('formal-system')
+export class FormalSystemController {
+};
--- micro-services/formal-system/modules/formal-system/formal-system.module.ts
+++ micro-services/formal-system/modules/formal-system/formal-system.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { FormalSystemController } from './formal-system.controller';
 
 @Module({
+  controllers: [
+    FormalSystemController
+  ]
 })
 export class FormalSystemModule {
 };
