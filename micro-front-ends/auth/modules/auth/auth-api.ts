fefcf09e676d5fb76f66d028dcf1f15bad046033
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-11 16:14:38 -0500

    Get proper port numbers

--- micro-front-ends/auth/modules/auth/auth-api.ts
+++ micro-front-ends/auth/modules/auth/auth-api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:${process.env.MICRO_SERVICE_PORT_AUTH}/auth`,
+    baseUrl: `http://localhost:${process.env.NEXT_PUBLIC_MICRO_SERVICE_PORT_AUTH}/auth`,
     prepareHeaders: (headers: Headers): void => {
       const token = localStorage.getItem('token');
 
--- micro-front-ends/user/modules/user/user-api.ts
+++ micro-front-ends/user/modules/user/user-api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:${process.env.MICRO_SERVICE_PORT_USER}/user`,
+    baseUrl: `http://localhost:${process.env.NEXT_PUBLIC_MICRO_SERVICE_PORT_USER}/user`,
     prepareHeaders: (headers: Headers): void => {
       const token = localStorage.getItem('token');
 
