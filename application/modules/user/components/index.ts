0ff56f3cb6afdc1cd3a67b52c17880ff0355705e
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-13 12:00:28 -0500

    Remove unused imported component

--- application/modules/user/components/index.ts
+++ application/modules/user/components/index.ts
@@ -1,3 +1,2 @@
 export { EditProfileForm } from './edit-profile-form/edit-profile-form';
 export { UserProvider } from './user-provider/user-provider';
-export { UserSignature } from './user-signature/user-signature';
--- application/modules/user/components/user-signature/user-signature.ts
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-export const UserSignature = dynamic(async () => {
-  const { UserSignature } = await import('user/user-signature');
-
-  return UserSignature;
-});
