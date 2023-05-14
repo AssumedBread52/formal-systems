f94e3738fd07b763b32bdb66201d9cb6b2e96ff9
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-14 15:09:43 -0500

    Allow request to be skipped

--- micro-front-ends/system/modules/system/hooks/use-read-system-by-url-path.ts
+++ micro-front-ends/system/modules/system/hooks/use-read-system-by-url-path.ts
@@ -1,12 +1,14 @@
 import { ClientSystem } from '@/system/types';
 import { useReadSystemByUrlPathQuery } from './use-read-system-by-url-path-query';
 
-export const useReadSystemByUrlPath = (urlPath: string): {
+export const useReadSystemByUrlPath = (urlPath: string, skip?: boolean): {
   data?: ClientSystem;
   errorMessage: string;
   loading: boolean;
 } => {
-  const { data, isError, isSuccess } = useReadSystemByUrlPathQuery(urlPath);
+  const { data, isError, isSuccess } = useReadSystemByUrlPathQuery(urlPath, {
+    skip
+  });
 
   return {
     data,
