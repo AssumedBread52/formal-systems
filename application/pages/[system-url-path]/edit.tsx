3e1d8fb6f048ef425f3dc43797764105696dd849
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-15 17:11:46 -0500

    Use new compornent for edit page

--- application/pages/[system-url-path]/edit.tsx
+++ application/pages/[system-url-path]/edit.tsx
@@ -1,2 +1,1 @@
-export { readFormalSystemByUrlPath as getServerSideProps } from '@/formal-system-back-end/server-side-props';
-export { UpdateFormalSystemPage as default } from '@/formal-system/components/update-formal-system-page/update-formal-system-page';
+export { UpdateSystemForm as default } from '@/system/components';
