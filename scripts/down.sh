830ede0fa9830782bf179106f08cf41fb93d8f3e
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-23 18:55:34 -0500

    Improve usage dump

--- scripts/down.sh
+++ scripts/down.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "--help" ]; then
   echo "Usage: ./scripts/down.sh"
-  echo "  Brings down the development containers. Containers are first stopped and then"
+  echo "  Brings down the development environment. Containers are first stopped and then"
   echo "  removed. The docker network is cleaned up as well."
 
   exit
