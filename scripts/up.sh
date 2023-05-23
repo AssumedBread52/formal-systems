4098fa7b660a761b45fbf10aa548761ec33bcdb2
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-22 19:54:05 -0500

    Slightly better verbiage

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "--help" ]; then
   echo "Usage: ./scripts/up.sh"
-  echo "  Starts the development environment. There are missing files and directories"
+  echo "  Brings up the development environment. There are missing files and directories"
   echo "  necessary for execution but not committed for several reasons: (1) they are"
   echo "  empty directories, (2) it would pose serious security risks, (3) there is"
   echo "  enough data committed to easily generate them. If any needed files and"
