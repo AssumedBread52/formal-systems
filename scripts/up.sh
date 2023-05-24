4bed732e06ace2c9c83d6d5f1afd7f69d7285870
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-23 19:12:06 -0500

    Reset database if initialization scripts change

--- scripts/up.sh
+++ scripts/up.sh
@@ -13,8 +13,23 @@ fi
 
 ./scripts/down.sh
 
+DATABASE_CHECK_SUM_FILE=database/initialization-scripts.cksm
+DATABASE_CURRENT_CHECK_SUM=$(cat database/initialization-scripts/* | sha1sum)
+
+if [ -f "$DATABASE_CHECK_SUM_FILE" ]; then
+  DATABASE_OLD_CHECK_SUM=$(cat $DATABASE_CHECK_SUM_FILE)
+fi
+
 if [ ! -d database/data ]; then
   mkdir database/data
+
+  echo -n "$DATABASE_CURRENT_CHECK_SUM" > $DATABASE_CHECK_SUM_FILE
+elif [ ! "$DATABASE_OLD_CHECK_SUM" = "$DATABASE_CURRENT_CHECK_SUM" ]; then
+  rm -rf database/data
+
+  mkdir database/data
+
+  echo -n "$DATABASE_CURRENT_CHECK_SUM" > $DATABASE_CHECK_SUM_FILE
 fi
 
 GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose up --detach
