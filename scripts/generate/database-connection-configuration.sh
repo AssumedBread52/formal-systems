4e525281882e3be2e340c7cb60e9cafd043d6eaa
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-25 16:42:27 -0500

    Add scheme to database configuration

--- scripts/generate/database-connection-configuration.sh
+++ scripts/generate/database-connection-configuration.sh
@@ -8,6 +8,7 @@ fi
 
 echo -n "" > back-end/database-connection-configuration.env
 echo "DATABASE_TYPE=mongodb" >> back-end/database-connection-configuration.env
+echo "DATABASE_SCHEME=mongodb" >> back-end/database-connection-configuration.env
 echo "DATABASE_HOST=database" >> back-end/database-connection-configuration.env
 echo "DATABASE_PORT=27017" >> back-end/database-connection-configuration.env
 echo "DATABASE_NAME=formal-systems" >> back-end/database-connection-configuration.env
