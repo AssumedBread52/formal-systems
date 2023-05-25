84ae4d678186717681d563bce7cf688f284c02c1
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-25 16:47:22 -0500

    Generate back port number for front and back ends

--- scripts/up.sh
+++ scripts/up.sh
@@ -30,6 +30,7 @@ function generate_environment_variables() {
 
 generate_environment_variables database-credentials
 generate_environment_variables database-connection-configuration
+generate_environment_variables back-end-port-number
 
 DATABASE_CHECK_SUM_FILE=database/initialization-scripts.cksm
 DATABASE_CURRENT_CHECK_SUM=$(cat database/initialization-scripts/* | sha1sum)
