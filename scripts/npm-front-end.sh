b46cb18a8c3fa2ea369b636348531681d73b3e5e
Antonio Sanchez <asanchez16756@gmail.com>
2023-05-25 15:56:09 -0500

    Add utility container for the front end

---
+++ scripts/npm-front-end.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NETWORK=$(docker network ls | awk '/formal-systems/ {print $2}')
+
+GROUP_ID=$(id -g) USER_ID=$(id -u) docker-compose run --rm npm-front-end $@
+
+if [ -z $NETWORK ]; then
+  ./scripts/down.sh
+fi
