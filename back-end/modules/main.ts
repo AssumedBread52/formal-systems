2743f85327da99d46e2c954d650a97387b60dcf4
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:33:13 -0500

    Use config port enable cors and continue on error

--- back-end/modules/main.ts
+++ back-end/modules/main.ts
@@ -1,12 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(MainModule, {
-    abortOnError: true
+    abortOnError: false
   });
 
-  await app.listen(5000);
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 5000;
+
+  await app.listen(port);
 };
 
 bootstrap();
