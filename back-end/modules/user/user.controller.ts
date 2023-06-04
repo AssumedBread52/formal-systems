081c0c43f1689d8d2b9a4a3fc38661bfadd6ef75
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:27:19 -0500

    Added user processes in controller

---
+++ back-end/modules/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { IdPayload } from '@/auth/data-transfer-objects/id.payload';
+import { SessionUser } from '@/auth/decorators/session-user';
+import { JwtGuard } from '@/auth/guards/jwt.guard';
+import { Body, Controller, Get, NotFoundException, Param, Patch, UseGuards, ValidationPipe } from '@nestjs/common';
+import { EditProfilePayload } from './data-transfer-objects/edit-profile.payload';
+import { UserPayload } from './data-transfer-objects/user.payload';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('session-user')
+  getSessionUser(@SessionUser() sessionUser: UserEntity): UserPayload {
+    return new UserPayload(sessionUser);
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<UserPayload> {
+    const user = await this.userService.readById(id);
+
+    if (!user) {
+      throw new NotFoundException('User with given ID not found.');
+    }
+
+    return new UserPayload(user);
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch('session-user')
+  async patchSessionUser(@SessionUser() sessionUser: UserEntity, @Body(ValidationPipe) editProfilePayload: EditProfilePayload): Promise<IdPayload> {
+    await this.userService.update(sessionUser, editProfilePayload);
+
+    const { _id } = sessionUser;
+
+    return {
+      id: _id.toString()
+    };
+  }
+};
