68c1cf3b23e120c29864b891eb6c54ac5dfcf8cb
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:16:23 -0500

    Add auth data types

---
+++ back-end/modules/auth/data-transfer-objects/sign-up.payload.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class SignUpPayload {
+  @IsNotEmpty()
+  firstName: string;
+  @IsNotEmpty()
+  lastName: string;
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+};
