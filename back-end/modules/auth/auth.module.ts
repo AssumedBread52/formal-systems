0b80a433f72681c7081c22dfc298e9ba245972b6
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:31:59 -0500

    Created auth module

---
+++ back-end/modules/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { UserModule } from '@/user/user.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [
+        ConfigModule
+      ],
+      inject: [
+        ConfigService
+      ],
+      useFactory: (configService: ConfigService): JwtModuleOptions => {
+        const secret = configService.get<string>('JSON_WEB_TOKEN_SECRET');
+        const expiresIn = configService.get<string>('JSON_WEB_TOKEN_EXPIRES_IN');
+
+        return {
+          secret,
+          signOptions: {
+            expiresIn
+          }
+        };
+      }
+    }),
+    UserModule
+  ],
+  controllers: [
+    AuthController
+  ],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    LocalStrategy
+  ]
+})
+export class AuthModule {
+};
