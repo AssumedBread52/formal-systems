1c8efe4424b1cad65ea63853103a66df10dcef32
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:30:39 -0500

    Add validation and token generation

---
+++ back-end/modules/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { UserEntity } from '@/user/user.entity';
+import { UserService } from '@/user/user.service';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { TokenPayload } from './data-transfer-objects/token.payload';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService, private userService: UserService) {
+  }
+
+  async validateUserByCredentials(email: string, password: string): Promise<UserEntity> {
+    const user = await this.userService.readByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('User not found.');
+    }
+
+    const { hashedPassword } = user;
+
+    const matched = await compare(password, hashedPassword);
+
+    if (!matched) {
+      throw new UnauthorizedException('Invalid e-mail address or password.');
+    }
+
+    return user;
+  }
+
+  async validateUserById(id: string): Promise<UserEntity> {
+    const user = await this.userService.readById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    return user;
+  }
+
+  async generateTokenPayload(user: UserEntity): Promise<TokenPayload> {
+    const { _id } = user;
+
+    const token = await this.jwtService.signAsync({
+      id: _id.toString()
+    });
+
+    return {
+      token
+    };
+  }
+};
