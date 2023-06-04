78e1edb61bbf5b09f79a39d85593db45b48b22ca
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-04 10:26:58 -0500

    Added user operations for auth processes

---
+++ back-end/modules/user/user.service.ts
@@ -0,0 +1,69 @@
+import { SignUpPayload } from '@/auth/data-transfer-objects/sign-up.payload';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { hash } from 'bcryptjs';
+import { ObjectId } from 'mongodb';
+import { Repository } from 'typeorm';
+import { EditProfilePayload } from './data-transfer-objects/edit-profile.payload';
+import { UserEntity } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectRepository(UserEntity) private userRepository: Repository<UserEntity>) {
+  }
+
+  private async checkForConflict(email: string): Promise<void> {
+    const collision = await this.userRepository.findOneBy({
+      email
+    });
+
+    if (collision) {
+      throw new ConflictException('Users must have a unique e-mail address.');
+    }
+  }
+
+  async create(user: SignUpPayload): Promise<UserEntity> {
+    const { firstName, lastName, email, password } = user;
+
+    await this.checkForConflict(email);
+
+    const hashedPassword = await hash(password, 12);
+
+    return this.userRepository.save({
+      firstName,
+      lastName,
+      email,
+      hashedPassword
+    });
+  }
+
+  readByEmail(email: string): Promise<UserEntity | null> {
+    return this.userRepository.findOneBy({
+      email
+    });
+  }
+
+  readById(id: string): Promise<UserEntity | null> {
+    return this.userRepository.findOneBy({
+      _id: new ObjectId(id)
+    });
+  }
+
+  async update(sessionUser: UserEntity, editProfilePayload: EditProfilePayload): Promise<void> {
+    const { _id, email } = sessionUser;
+    const { newFirstName, newLastName, newEmail, newPassword } = editProfilePayload;
+
+    if (email !== newEmail) {
+      await this.checkForConflict(newEmail);
+    }
+
+    sessionUser.firstName = newFirstName;
+    sessionUser.lastName = newLastName;
+    sessionUser.email = newEmail;
+    if (newPassword) {
+      sessionUser.hashedPassword = await hash(newPassword, 12);
+    }
+
+    await this.userRepository.update(_id, sessionUser);
+  }
+};
