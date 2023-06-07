b94416ed003302e66b322682021254f720cb61f0
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-06 19:39:34 -0500

    Add initial links

--- front-end/modules/app/components/layout/header-menu/header-menu.tsx
+++ front-end/modules/app/components/layout/header-menu/header-menu.tsx
@@ -1,10 +1,20 @@
 'use client';
 
 import { Menu } from 'antd';
+import { ItemType, MenuItemType } from 'antd/es/menu/hooks/useItems';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { ReactElement } from 'react';
 
 export const HeaderMenu = (): ReactElement => {
+  const pathname = usePathname();
+
+  const items = [
+    { label: <Link href='/'>Formal Systems</Link>, key: '/', title: 'Search Formal Systems' },
+    { label: <Link href='/info'>Info</Link>, key: '/info', title: 'What are Formal Systems' },
+  ] as ItemType<MenuItemType>[];
+
   return (
-    <Menu />
+    <Menu defaultSelectedKeys={[pathname]} mode='horizontal' items={items} theme='dark' />
   );
 };
