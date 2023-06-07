d6365cc5c59737489a901e9cee71f16eafdb59b9
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-06 22:13:18 -0500

    Create provider component for app

---
+++ front-end/modules/app/components/app-provider/app-provider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { api } from '@/app/api';
+import { store } from '@/app/store';
+import { Layout } from 'antd';
+import { PropsWithChildren, ReactElement, useEffect } from 'react';
+import { Provider } from 'react-redux';
+
+export const AppProvider = (props: PropsWithChildren): ReactElement => {
+  const { children } = props;
+
+  useEffect((): void => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      store.dispatch(api.endpoints.refreshToken.initiate());
+    }
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        {children}
+      </Layout>
+    </Provider>
+  );
+};
