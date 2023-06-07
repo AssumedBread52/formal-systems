b5cc34967cbe08373cdf038cc3e496880c3179b4
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-06 22:03:48 -0500

    Hook up auth end points via api

---
+++ front-end/modules/app/api.ts
@@ -0,0 +1,39 @@
+import { getSessionUserId } from '@/auth/end-points/get-session-user-id';
+import { refreshToken } from '@/auth/end-points/refresh-token';
+import { signIn } from '@/auth/end-points/sign-in';
+import { signOut } from '@/auth/end-points/sign-out';
+import { signUp } from '@/auth/end-points/sign-up';
+import { EndpointBuilder } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
+import { BaseQueryFn, createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { Tags } from './constants/tags';
+import { TagTypes } from './types/tag-types';
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: `http://${process.env.NEXT_PUBLIC_BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}`,
+    prepareHeaders: (headers: Headers): void => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+    }
+  }),
+  endpoints: (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>): {
+    getSessionUserId: ReturnType<typeof getSessionUserId>;
+    refreshToken: ReturnType<typeof refreshToken>;
+    signIn: ReturnType<typeof signIn>;
+    signOut: ReturnType<typeof signOut>;
+    signUp: ReturnType<typeof signUp>;
+  } => {
+    return {
+      getSessionUserId: getSessionUserId(builder),
+      refreshToken: refreshToken(builder),
+      signIn: signIn(builder),
+      signOut: signOut(builder),
+      signUp: signUp(builder)
+    };
+  },
+  reducerPath: 'api',
+  tagTypes: Object.values(Tags)
+});
