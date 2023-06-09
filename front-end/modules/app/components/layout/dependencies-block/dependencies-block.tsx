d9746f3649589a88c25bc3d59eba83f0f521baf0
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-09 09:31:45 -0500

    Fix href for npm info

--- front-end/modules/app/components/layout/dependencies-block/dependencies-block.tsx
+++ front-end/modules/app/components/layout/dependencies-block/dependencies-block.tsx
@@ -16,7 +16,7 @@ export const DependenciesBlock = (props: DependenciesBlockProps): ReactElement =
 
         return (
           <AntdCol xs={24} sm={24} md={12} lg={8} xl={6} xxl={6}>
-            <AntdCard bodyStyle={{ textAlign: 'center' }} extra={<a target='_blank' href={`https://www.npmjs.com/packages/${packageName}`}>Explore</a>} title={packageName} type='inner'>
+            <AntdCard bodyStyle={{ textAlign: 'center' }} extra={<a target='_blank' href={`https://www.npmjs.com/package/${packageName}`}>Explore</a>} title={packageName} type='inner'>
               version: {version.substring(1)}
             </AntdCard>
           </AntdCol>
