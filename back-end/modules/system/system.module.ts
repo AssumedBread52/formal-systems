fb3952cde77ad390a491310e2c8dbacc5f186e7c
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-10 16:38:16 -0500

    Added system module

---
+++ back-end/modules/system/system.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SystemController } from './system.controller';
+import { SystemEntity } from './system.entity';
+import { SystemService } from './system.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SystemEntity
+    ])
+  ],
+  controllers: [
+    SystemController
+  ],
+  providers: [
+    SystemService
+  ]
+})
+export class SystemModule {
+};
