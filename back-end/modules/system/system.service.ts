a4f155b1bc8774443888eb43f7f718813f37faff
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-10 16:43:57 -0500

    Add ability to perform paginated search

--- back-end/modules/system/system.service.ts
+++ back-end/modules/system/system.service.ts
@@ -1,8 +1,9 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectId } from 'mongodb';
-import { MongoRepository } from 'typeorm';
+import { MongoRepository, RootFilterOperators } from 'typeorm';
 import { NewSystemPayload } from './data-transfer-objects/new-system.payload';
+import { PaginatedResultsPayload } from './data-transfer-objects/paginated-results.payload';
 import { SystemEntity } from './system.entity';
 
 @Injectable()
@@ -42,5 +43,25 @@ export class SystemService {
       description,
       createdByUserId: sessionUserId
     };
+  }
+
+  async readSystems(page: number, take: number, keywords?: string | string[]): Promise<PaginatedResultsPayload> {
+    const skip = (page - 1) * take;
+    const where = {} as RootFilterOperators<SystemEntity>;
+
+    if (keywords && 0 !== keywords.length) {
+      where.$text = {
+        $caseSensitive: false,
+        $search: Array.isArray(keywords) ? keywords.join(',') : keywords
+      };
+    }
+
+    const [results, total] = await this.systemRepository.findAndCount({
+      skip,
+      take,
+      where
+    });
+
+    return new PaginatedResultsPayload(total, results);
   }
 };
