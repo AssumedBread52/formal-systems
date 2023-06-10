0598d98ab4a59c98112fd38caf91476945f861c2
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-10 16:39:37 -0500

    Added result type for paginated search

---
+++ back-end/modules/system/data-transfer-objects/paginated-results.payload.ts
@@ -0,0 +1,23 @@
+import { SystemEntity } from '@/system/system.entity';
+import { Type } from 'class-transformer';
+import { IsArray, IsInt, Min, ValidateNested } from 'class-validator';
+import { SystemPayload } from './system.payload';
+
+export class PaginatedResultsPayload {
+  @IsInt()
+  @Min(0)
+  total: number;
+  @IsArray()
+  @ValidateNested({
+    each: true
+  })
+  @Type(() => SystemPayload)
+  results: SystemPayload[];
+
+  constructor(total: number, results: SystemEntity[]) {
+    this.total = total;
+    this.results = results.map((result: SystemEntity): SystemPayload => {
+      return new SystemPayload(result);
+    });
+  }
+};
