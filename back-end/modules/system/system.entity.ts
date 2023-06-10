ba884dfcdd05f76d1de89ff57957ff9139f50a5e
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-10 15:54:54 -0500

    Added entity for the system collection

---
+++ back-end/modules/system/system.entity.ts
@@ -0,0 +1,16 @@
+import { ObjectId } from 'mongodb';
+import { Column, Entity, ObjectIdColumn } from 'typeorm';
+
+@Entity('system')
+export class SystemEntity {
+  @ObjectIdColumn()
+  _id: ObjectId = new ObjectId();
+  @Column()
+  title: string = '';
+  @Column()
+  urlPath: string = '';
+  @Column()
+  description: string = '';
+  @Column()
+  createdByUserId: ObjectId = new ObjectId();
+};
