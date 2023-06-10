8e3b0f1e09a842fefc76f3d817aae466d5ce81ac
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-10 16:45:03 -0500

    Expose ability to perform paginated system search

--- back-end/modules/system/system.controller.ts
+++ back-end/modules/system/system.controller.ts
@@ -1,13 +1,19 @@
 import { SessionUserId } from '@/auth/decorators/session-user-id';
 import { JwtGuard } from '@/auth/guards/jwt.guard';
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, ParseIntPipe, Post, Query, UseGuards, ValidationPipe } from '@nestjs/common';
 import { ObjectId } from 'mongodb';
 import { NewSystemPayload } from './data-transfer-objects/new-system.payload';
+import { PaginatedResultsPayload } from './data-transfer-objects/paginated-results.payload';
 import { SystemService } from './system.service';
 
 @Controller('system')
 export class SystemController {
   constructor(private systemService: SystemService) {
+  }
+
+  @Get()
+  getSystems(@Query('page', ParseIntPipe) page: number, @Query('count', ParseIntPipe) count: number, @Query('keywords') keywords?: string | string[]): Promise<PaginatedResultsPayload> {
+    return this.systemService.readSystems(page, count, keywords);
   }
 
   @UseGuards(JwtGuard)
