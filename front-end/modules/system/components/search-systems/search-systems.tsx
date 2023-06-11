8d9fd0a731d651294b841afdc61c5e31fb6e1be0
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 09:22:47 -0500

    Adjusted for changes in search controls

--- front-end/modules/system/components/search-systems/search-systems.tsx
+++ front-end/modules/system/components/search-systems/search-systems.tsx
@@ -2,12 +2,15 @@ import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { SearchControls } from '@/common/components/search-controls/search-controls';
 import { ReactElement } from 'react';
 import { CreateButton } from './create-button/create-button';
+import { SystemList } from './system-list/system-list';
 
 export const SearchSystems = (): ReactElement => {
+  const { results, total } = { results: [], total: 0 };
+
   return (
     <AntdCard extra={<CreateButton />} title='Formal Systems'>
-      <SearchControls>
-        Search Systems
+      <SearchControls total={total} resultType='formal systems'>
+        <SystemList systems={results} />
       </SearchControls>
     </AntdCard>
   );
