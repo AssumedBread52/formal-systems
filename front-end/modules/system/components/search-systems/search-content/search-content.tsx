655875992ed5d97506dadad0f59180a0f00d1df1
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 13:55:11 -0500

    Add error state handling

--- front-end/modules/system/components/search-systems/search-content/search-content.tsx
+++ front-end/modules/system/components/search-systems/search-content/search-content.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { AntdButton } from '@/common/components/antd-button/antd-button';
 import { AntdCol } from '@/common/components/antd-col/antd-col';
+import { AntdResult } from '@/common/components/antd-result/antd-result';
 import { AntdRow } from '@/common/components/antd-row/antd-row';
 import { InputPagination } from '@/common/components/input-pagination/input-pagination';
 import { InputSearch } from '@/common/components/input-search/input-search';
 import { useSearchSystems } from '@/system/hooks/use-search-systems';
+import { useRouter } from 'next/navigation';
 import { LoadingResults } from './loading-results/loading-results';
 
 export const SearchContent = () => {
-  const [searchResults, loading] = useSearchSystems();
+  const { back } = useRouter();
+  const [searchResults, loading, errorMessage] = useSearchSystems();
 
   const { total } = searchResults ?? { total: 0 };
 
@@ -23,6 +27,9 @@ export const SearchContent = () => {
       <AntdCol span={24}>
         {loading && (
           <LoadingResults />
+        )}
+        {errorMessage && (
+          <AntdResult status='500' subTitle={errorMessage} extra={[<AntdButton htmlType='button' type='primary' onClick={back}>Back</AntdButton>]} />
         )}
       </AntdCol>
       <AntdCol span={24}>
