dd3557daa85cfad1b79944b69c86f8d83119372d
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 08:46:13 -0500

    Added search input

--- front-end/modules/common/components/search-controls/search-controls.tsx
+++ front-end/modules/common/components/search-controls/search-controls.tsx
@@ -1,11 +1,52 @@
+'use client';
+
+import { AntdCol } from '@/common/components/antd-col/antd-col';
+import { AntdInputSearch } from '@/common/components/antd-input-search/antd-input-search';
+import { AntdRow } from '@/common/components/antd-row/antd-row';
+import { SearchParameters } from '@/common/types/search-parameters';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { stringify } from 'querystring';
 import { PropsWithChildren, ReactElement } from 'react';
 
 export const SearchControls = (props: PropsWithChildren): ReactElement => {
   const { children } = props;
 
+  const pathname = usePathname();
+
+  const { push } = useRouter();
+
+  const searchParams = useSearchParams();
+
+  const count = parseInt(searchParams.get('count') ?? '10');
+  const keywords = searchParams.getAll('keywords');
+  const page = parseInt(searchParams.get('page') ?? '1');
+
+  const searchHandler = (value: string): void => {
+    const newKeywords = value.split(' ').filter((newKeyword: string): boolean => {
+      return 0 !== newKeyword.length;
+    });
+
+    const searchParameters = { count, page } as SearchParameters;
+
+    if (0 < newKeywords.length) {
+      searchParameters.keywords = newKeywords;
+    }
+
+    push(`${pathname}?${stringify(searchParameters)}`);
+  };
+
   return (
-    <div>
-      {children}
-    </div>
+    <AntdRow gutter={[0, 16]}>
+      <AntdCol span={24}>
+        <AntdInputSearch allowClear defaultValue={keywords.join(' ')} enterButton onSearch={searchHandler} />
+      </AntdCol>
+      <AntdCol span={24}>
+      </AntdCol>
+      <AntdCol span={24}>
+        {children}
+      </AntdCol>
+      <AntdCol span={24}>
+      </AntdCol>
+    </AntdRow>
   );
 };
