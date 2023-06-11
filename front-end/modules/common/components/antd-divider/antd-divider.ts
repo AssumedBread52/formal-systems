4ea732df0ab274b72793bd920b20d12350023c28
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 14:06:19 -0500

    Added divider

---
+++ front-end/modules/common/components/antd-divider/antd-divider.ts
@@ -0,0 +1,5 @@
+'use client';
+
+import { Divider } from 'antd';
+
+export const AntdDivider = Divider;
--- front-end/modules/system/components/search-systems/search-content/system-list/system-item/system-item.tsx
+++ front-end/modules/system/components/search-systems/search-content/system-list/system-item/system-item.tsx
@@ -1,4 +1,5 @@
 import { AntdCard } from '@/common/components/antd-card/antd-card';
+import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
 import { System } from '@/system/types/system';
 import { ReactElement } from 'react';
 
@@ -8,6 +9,7 @@ export const SystemItem = (props: Omit<System, 'id'>): ReactElement => {
   return (
     <AntdCard title={title}>
       {description}
+      <AntdDivider />
     </AntdCard>
   );
 };
