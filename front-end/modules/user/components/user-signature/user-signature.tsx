3b9568cf1b81d8645adde9528d57c0bce93e9109
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 14:42:24 -0500

    Finished user signature

--- front-end/modules/user/components/user-signature/user-signature.tsx
+++ front-end/modules/user/components/user-signature/user-signature.tsx
@@ -1,12 +1,29 @@
+'use client';
+
 import { AntdCardMeta } from '@/common/components/antd-card-meta/antd-card-meta';
+import { AntdSkeleton } from '@/common/components/antd-skeleton/antd-skeleton';
 import { AntdTypographyText } from '@/common/components/antd-typography-text/antd-typography-text';
+import { useGetUserById } from '@/user/hooks/use-get-user-by-id';
 import { UserSignatureProps } from '@/user/types/user-signature-props';
 import { ReactElement } from 'react';
 
 export const UserSignature = (props: UserSignatureProps): ReactElement => {
   const { userId } = props;
 
+  const [user, loading] = useGetUserById(userId);
+
+  let title = 'Error';
+  let description = 'Failed to load user data.';
+  if (user) {
+    const { firstName, lastName } = user;
+
+    title = 'Created By';
+    description = `${firstName} ${lastName}`;
+  }
+
   return (
-    <AntdCardMeta title='Created By' description={<AntdTypographyText italic>{userId}</AntdTypographyText>} />
+    <AntdSkeleton active loading={loading}>
+      <AntdCardMeta title={title} description={<AntdTypographyText italic>{description}</AntdTypographyText>} />
+    </AntdSkeleton>
   );
 };
