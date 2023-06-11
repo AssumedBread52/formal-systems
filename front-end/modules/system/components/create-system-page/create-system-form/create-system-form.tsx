1e9b63077ed0ae4b5e7cf25056397abf4871b4a0
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-11 05:19:58 -0500

    Linked create system hook to form

--- front-end/modules/system/components/create-system-page/create-system-form/create-system-form.tsx
+++ front-end/modules/system/components/create-system-page/create-system-form/create-system-form.tsx
@@ -1,10 +1,28 @@
+'use client';
+
+import { AntdAlert } from '@/common/components/antd-alert/antd-alert';
+import { AntdForm } from '@/common/components/antd-form/antd-form';
+import { AntdLoadingOutlined } from '@/common/components/antd-loading-outlined/antd-loading-outlined';
+import { AntdSpin } from '@/common/components/antd-spin/antd-spin';
+import { useCreateSystem } from '@/system/hooks/use-create-system';
+import { NewSystemPayload } from '@/system/types/new-system-payload';
 import { PropsWithChildren, ReactElement } from 'react';
+
+const TypedAntdForm = AntdForm<NewSystemPayload>;
 
 export const CreateSystemForm = (props: PropsWithChildren): ReactElement => {
   const { children } = props;
+
+  const [finishHandler, spinning, description] = useCreateSystem();
+
   return (
-    <h1>
-      {children}
-    </h1>
+    <AntdSpin indicator={<AntdLoadingOutlined />} size='large' spinning={spinning}>
+      <TypedAntdForm labelCol={{ sm: { span: 0 }, md: { span: 8 } }} wrapperCol={{ sm: { span: 24 }, md: { span: 16 } }} onFinish={finishHandler}>
+        {children}
+      </TypedAntdForm>
+      {description && (
+        <AntdAlert closable description={description} message='Error' showIcon type='error' />
+      )}
+    </AntdSpin>
   );
 };
