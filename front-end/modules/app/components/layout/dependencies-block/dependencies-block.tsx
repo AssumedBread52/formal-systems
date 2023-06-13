48ab5aed36ad75df06c0b3127923849839f1e80f
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-13 06:12:45 -0500

    Resize and reorder

--- front-end/modules/app/components/layout/dependencies-block/dependencies-block.tsx
+++ front-end/modules/app/components/layout/dependencies-block/dependencies-block.tsx
@@ -15,8 +15,8 @@ export const DependenciesBlock = (props: DependenciesBlockProps): ReactElement =
         const version = packages[packageName];
 
         return (
-          <AntdCol key={packageName} xs={24} sm={24} md={12} lg={8} xl={6} xxl={6}>
-            <AntdCard bodyStyle={{ textAlign: 'center' }} extra={<a target='_blank' href={`https://www.npmjs.com/package/${packageName}`}>Explore</a>} title={packageName} type='inner'>
+          <AntdCol key={packageName} xs={24} sm={24} md={12} lg={8} xl={8} xxl={6}>
+            <AntdCard bodyStyle={{ textAlign: 'center' }} extra={<a href={`https://www.npmjs.com/package/${packageName}`} target='_blank'>Explore</a>} title={packageName} type='inner'>
               version: {version.substring(1)}
             </AntdCard>
           </AntdCol>
