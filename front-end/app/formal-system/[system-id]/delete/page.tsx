8b41ed623a8668e27cb30b9c37d226e7d5dc82b9
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-18 16:37:32 -0500

    Add redirects for the old pages

--- front-end/app/formal-system/[system-id]/delete/page.tsx
+++ front-end/app/formal-system/[system-id]/delete/page.tsx
@@ -1,1 +1,19 @@
-export { DeleteSystemPage as default, generateMetadata } from '@/system/components/delete-system-page/delete-system-page';
+import { ServerSideProps } from '@/app/types/server-side-props';
+import { System } from '@/system/types/system';
+import { redirect } from 'next/navigation';
+
+const Page = async (props: ServerSideProps): Promise<never> => {
+  const { params } = props;
+
+  const { 'system-id': id } = params;
+
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${id}`);
+
+  const system = await response.json() as System;
+
+  const { title } = system;
+
+  redirect(`/formal-system/${id}/${title}/delete`);
+};
+
+export default Page;
--- front-end/app/formal-system/[system-id]/edit/page.tsx
+++ front-end/app/formal-system/[system-id]/edit/page.tsx
@@ -1,1 +1,19 @@
-export { EditSystemPage as default, generateMetadata } from '@/system/components/edit-system-page/edit-system-page';
+import { ServerSideProps } from '@/app/types/server-side-props';
+import { System } from '@/system/types/system';
+import { redirect } from 'next/navigation';
+
+const Page = async (props: ServerSideProps): Promise<never> => {
+  const { params } = props;
+
+  const { 'system-id': id } = params;
+
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${id}`);
+
+  const system = await response.json() as System;
+
+  const { title } = system;
+
+  redirect(`/formal-system/${id}/${title}/edit`);
+};
+
+export default Page;
