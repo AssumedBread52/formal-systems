7540a05f02d698837d8702ba878e9708a276f749
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-29 06:43:02 -0500

    Move title to SSR

--- front-end/modules/system/components/delete-system-page/delete-system-form/delete-system-form.tsx
+++ front-end/modules/system/components/delete-system-page/delete-system-form/delete-system-form.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { AntdAlert } from '@/common/components/antd-alert/antd-alert';
-import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { AntdForm } from '@/common/components/antd-form/antd-form';
 import { AntdLoadingOutlined } from '@/common/components/antd-loading-outlined/antd-loading-outlined';
 import { AntdSpin } from '@/common/components/antd-spin/antd-spin';
@@ -19,20 +18,18 @@ export const DeleteSystemForm = (props: PropsWithChildren): ReactElement => {
 
   const { 'system-id': id } = params;
 
-  const [system, loading] = useGetSystemById(id);
+  const [system] = useGetSystemById(id);
 
   const [finishHandler, spinning, description] = useDeleteSystem();
 
   return (
-    <AntdCard headStyle={{ textAlign: 'center' }} loading={loading} style={{ marginLeft: 'auto', marginRight: 'auto', minWidth: '180px', width: '50vw' }} title={`Delete ${id}`}>
-      <AntdSpin indicator={<AntdLoadingOutlined />} size='large' spinning={spinning}>
-        <TypedAntdForm onFinish={() => finishHandler(system?.id ?? '')}>
-          {children}
-        </TypedAntdForm>
-        {description && (
-          <AntdAlert closable description={description} message='Error' showIcon type='error' />
-        )}
-      </AntdSpin>
-    </AntdCard>
+    <AntdSpin indicator={<AntdLoadingOutlined />} size='large' spinning={spinning}>
+      <TypedAntdForm onFinish={() => finishHandler(system?.id ?? '')}>
+        {children}
+      </TypedAntdForm>
+      {description && (
+        <AntdAlert closable description={description} message='Error' showIcon type='error' />
+      )}
+    </AntdSpin>
   );
 };
--- front-end/modules/system/components/delete-system-page/delete-system-page.tsx
+++ front-end/modules/system/components/delete-system-page/delete-system-page.tsx
@@ -1,5 +1,6 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdButton } from '@/common/components/antd-button/antd-button';
+import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { AntdFormItem } from '@/common/components/antd-form-item/antd-form-item';
 import { AntdSpace } from '@/common/components/antd-space/antd-space';
 import { CancelButton } from '@/common/components/cancel-button/cancel-button';
@@ -8,18 +9,32 @@ import { Metadata } from 'next';
 import { ReactElement } from 'react';
 import { DeleteSystemForm } from './delete-system-form/delete-system-form';
 
-export const DeleteSystemPage = (): ReactElement => {
+export const DeleteSystemPage = async (props: ServerSideProps): Promise<ReactElement> => {
+  const { params } = props;
+
+  const { 'system-id': systemId } = params;
+
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${systemId}`, {
+    cache: 'no-store'
+  });
+
+  const system = await response.json() as System;
+
+  const { title } = system;
+
   return (
-    <DeleteSystemForm>
-      <AntdFormItem style={{ textAlign: 'center' }}>
-        <AntdSpace direction='vertical'>
-          <AntdButton htmlType='submit' type='primary'>
-            Submit
-          </AntdButton>
-          <CancelButton />
-        </AntdSpace>
-      </AntdFormItem>
-    </DeleteSystemForm>
+    <AntdCard headStyle={{ textAlign: 'center' }} style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '600px' }} title={`Delete ${title}`}>
+      <DeleteSystemForm>
+        <AntdFormItem style={{ textAlign: 'center' }}>
+          <AntdSpace direction='vertical'>
+            <AntdButton htmlType='submit' type='primary'>
+              Submit
+            </AntdButton>
+            <CancelButton />
+          </AntdSpace>
+        </AntdFormItem>
+      </DeleteSystemForm>
+    </AntdCard>
   );
 };
 
