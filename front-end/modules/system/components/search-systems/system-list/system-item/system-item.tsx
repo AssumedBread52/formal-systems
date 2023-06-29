6e2aca0fbaace38d4bbbae968f8804bc8f5b75cd
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-29 09:08:28 -0500

    Cleaner once pull out of component props

--- front-end/modules/system/components/search-systems/system-list/system-item/system-item.tsx
+++ front-end/modules/system/components/search-systems/system-list/system-item/system-item.tsx
@@ -2,7 +2,7 @@ import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
 import { System } from '@/system/types/system';
 import { UserSignature } from '@/user/components/user-signature/user-signature';
-import { ReactElement } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import { DeleteLink } from './delete-link/delete-link';
 import { EditLink } from './edit-link/edit-link';
 import { ExploreLink } from './explore-link/explore-link';
@@ -10,8 +10,13 @@ import { ExploreLink } from './explore-link/explore-link';
 export const SystemItem = (props: System): ReactElement => {
   const { id, title, description, createdByUserId } = props;
 
+  const actions = [
+    <EditLink id={id} title={title} createdByUserId={createdByUserId} />,
+    <DeleteLink id={id} title={title} createdByUserId={createdByUserId} />
+  ] as ReactNode[];
+
   return (
-    <AntdCard actions={[<EditLink id={id} title={title} createdByUserId={createdByUserId} />, <DeleteLink id={id} title={title} createdByUserId={createdByUserId} />]} extra={<ExploreLink id={id} title={title} />} title={title} type='inner'>
+    <AntdCard actions={actions} extra={<ExploreLink id={id} title={title} />} title={title} type='inner'>
       {description}
       <AntdDivider />
       <UserSignature userId={createdByUserId} />
