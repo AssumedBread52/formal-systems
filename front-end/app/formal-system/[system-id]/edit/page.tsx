e825c02a41d7916cc25da9141982c05005d24f9f
Antonio Sanchez <asanchez16756@gmail.com>
2023-06-29 15:03:02 -0500

    Rename variables

--- front-end/app/formal-system/[system-id]/edit/page.tsx
+++ front-end/app/formal-system/[system-id]/edit/page.tsx
@@ -5,15 +5,15 @@ import { redirect } from 'next/navigation';
 const Page = async (props: ServerSideProps): Promise<never> => {
   const { params } = props;
 
-  const { 'system-id': id } = params;
+  const { 'system-id': systemId } = params;
 
-  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${id}`, {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${systemId}`, {
     cache: 'no-store'
   });
 
   const system = await response.json() as System;
 
-  const { title } = system;
+  const { id, title } = system;
 
   redirect(`/formal-system/${id}/${title}/edit`);
 };
