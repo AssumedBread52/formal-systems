8ba7e718cec45d3bc53c0dbcee64007de5001afd
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-08 08:08:10 -0500

    Put components together at least for now

--- front-end/modules/system/components/system-page/system-page.tsx
+++ front-end/modules/system/components/system-page/system-page.tsx
@@ -1,24 +1,38 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
-import { IdPayload } from '@/common/types/id-payload';
+import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
+import { System } from '@/system/types/system';
+import { UserSignatureProps } from '@/user/types/user-signature-props';
 import { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { ComponentType, ReactElement } from 'react';
 
-const SystemDetails = dynamic(async (): Promise<ComponentType<IdPayload>> => {
-  const { SystemDetails } = await import('./system-details/system-details');
+const UserSignature = dynamic(async (): Promise<ComponentType<UserSignatureProps>> => {
+  const { UserSignature } = await import('@/user/components/user-signature/user-signature');
 
-  return SystemDetails;
+  return UserSignature;
 });
 
-export const SystemPage = (props: ServerSideProps): ReactElement => {
+export const SystemPage = async (props: ServerSideProps): Promise<ReactElement> => {
   const { params } = props;
 
   const { 'system-id': systemId = '', 'system-title': systemTitle = '' } = params;
 
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/system/${systemId}`, {
+    cache: 'no-store'
+  });
+
+  const system = await response.json() as System;
+
+  const { description, createdByUserId } = system;
+
   return (
     <AntdCard title={decodeURIComponent(systemTitle)}>
-      <SystemDetails id={systemId} />
+      <AntdCard type='inner'>
+        {description}
+        <AntdDivider />
+        <UserSignature userId={createdByUserId} />
+      </AntdCard>
     </AntdCard>
   );
 };
