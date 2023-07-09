51f8e2f4036e855795cd29bf5118e4cc36937cea
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-09 08:20:34 -0500

    Only collapsible from the icon

--- front-end/modules/app/components/layout/footer-dependencies/footer-dependencies.tsx
+++ front-end/modules/app/components/layout/footer-dependencies/footer-dependencies.tsx
@@ -2,6 +2,7 @@ import packageJson from '#/package.json';
 import { fetchBackEndDependencies } from '@/app/fetch-data/fetch-back-end-dependencies';
 import { fetchBackEndDevDependencies } from '@/app/fetch-data/fetch-back-end-dev-dependencies';
 import { AntdCollapse } from '@/common/components/antd-collapse/antd-collapse';
+import { CollapsibleType } from 'antd/es/collapse/CollapsePanel';
 import { ReactElement } from 'react';
 import { DependenciesBlock } from './dependencies-block/dependencies-block';
 
@@ -17,6 +18,7 @@ export const FooterDependencies = async (): Promise<ReactElement> => {
       children: (
         <DependenciesBlock packages={backEndDependencies} />
       ),
+      collapsible: 'icon' as CollapsibleType,
       key: 'back-end-dependencies',
       label: 'Back End Dependencies'
     },
@@ -24,6 +26,7 @@ export const FooterDependencies = async (): Promise<ReactElement> => {
       children: (
         <DependenciesBlock packages={backEndDevDependencies} />
       ),
+      collapsible: 'icon' as CollapsibleType,
       key: 'back-end-dev-dependencies',
       label: 'Back End Development Dependencies'
     },
@@ -31,6 +34,7 @@ export const FooterDependencies = async (): Promise<ReactElement> => {
       children: (
         <DependenciesBlock packages={dependencies} />
       ),
+      collapsible: 'icon' as CollapsibleType,
       key: 'front-end-dependencies',
       label: 'Front End Dependencies'
     },
@@ -38,6 +42,7 @@ export const FooterDependencies = async (): Promise<ReactElement> => {
       children: (
         <DependenciesBlock packages={devDependencies} />
       ),
+      collapsible: 'icon' as CollapsibleType,
       key: 'front-end-dev-dependencies',
       label: 'Front End Development Dependencies'
     }
