c1a657503ffc1a51ba29e7e0b62dcc9f8f8a110a
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-11 19:15:15 -0500

    Add cookie parsing on the back-end

--- back-end/modules/main.ts
+++ back-end/modules/main.ts
@@ -1,11 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { MainModule } from './main.module';
 
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(MainModule, {
     abortOnError: false
   });
+
+  app.use(cookieParser());
 
   app.enableCors();
 
