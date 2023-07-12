f5d5548eb521505d573872a56228fff1655c3160
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-11 19:43:00 -0500

    Switch fetch user to use reverse proxy

--- front-end/modules/user/fetch-data/fetch-user.ts
+++ front-end/modules/user/fetch-data/fetch-user.ts
@@ -1,7 +1,7 @@
 import { User } from '@/user/types/user';
 
 export const fetchUser = async (userId: string): Promise<User> => {
-  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.NEXT_PUBLIC_BACK_END_PORT}/user/${userId}`, {
+  const response = await fetch(`http://localhost:${process.env.PORT}/api/user/${userId}`, {
     cache: 'no-store'
   });
 
