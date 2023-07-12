eb138f8483319103541bb04555159546d3ce76dc
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-11 19:50:41 -0500

    Remove token payload handling switch to cookies

--- front-end/modules/app/store.ts
+++ front-end/modules/app/store.ts
@@ -11,17 +11,10 @@ export const store = configureStore({
       return (next) => {
         return (action) => {
           if (api.endpoints.refreshToken.matchFulfilled(action) || api.endpoints.signIn.matchFulfilled(action) || api.endpoints.signUp.matchFulfilled(action)) {
-            const { payload } = action;
-            const { token } = payload;
-
-            localStorage.setItem('token', token);
-
             refreshTimeout = setTimeout(() => {
               store.dispatch(api.endpoints.refreshToken.initiate());
             }, 45000);
           } else if (api.endpoints.refreshToken.matchRejected(action) || api.endpoints.signOut.matchFulfilled(action)) {
-            localStorage.removeItem('token');
-
             if (refreshTimeout) {
               clearTimeout(refreshTimeout);
             }
