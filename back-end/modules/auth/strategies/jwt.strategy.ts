db1d9b921730827fe469fb83cd10c0ba3943defb
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-11 19:26:23 -0500

    Extract token from cookie not authorization header

--- back-end/modules/auth/strategies/jwt.strategy.ts
+++ back-end/modules/auth/strategies/jwt.strategy.ts
@@ -4,17 +4,34 @@ import { UserEntity } from '@/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+import { Request } from 'express';
+import { Strategy, StrategyOptions } from 'passport-jwt';
 import { BaseStrategy } from './base.strategy';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) implements BaseStrategy {
   constructor(private authService: AuthService, configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: JwtStrategy.extractJwt,
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JSON_WEB_TOKEN_SECRET')
     } as StrategyOptions);
+  }
+
+  private static extractJwt(request: Request): string | null {
+    const { cookies } = request;
+
+    if (!cookies) {
+      return null;
+    }
+
+    const { token } = cookies;
+
+    if (!token) {
+      return null;
+    }
+
+    return token;
   }
 
   validate(idPayload: IdPayload): Promise<UserEntity> {
