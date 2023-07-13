a3f70428034d9747a11105b68ec14783cfafeb13
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-13 09:32:43 -0500

    Hide on system page

--- front-end/modules/system/components/system-item/explore-link/explore-link.tsx
+++ front-end/modules/system/components/system-item/explore-link/explore-link.tsx
@@ -1,12 +1,23 @@
 import { System } from '@/system/types/system';
+import { headers } from 'next/headers';
 import Link from 'next/link';
-import { ReactElement } from 'react';
+import { Fragment, ReactElement } from 'react';
 
 export const ExploreLink = (props: Omit<System, 'description' | 'createdByUserId'>): ReactElement => {
   const { id, title } = props;
 
+  const pathname = headers().get('x-invoke-path') ?? '';
+
+  const descriptionPath = `/formal-system/${id}/${title}`;
+
+  if (decodeURIComponent(pathname) === descriptionPath) {
+    return (
+      <Fragment />
+    );
+  }
+
   return (
-    <Link href={`/formal-system/${id}/${title}`}>
+    <Link href={descriptionPath}>
       Explore
     </Link>
   );
