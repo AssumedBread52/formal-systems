5a5840e836cbd2506ce96417827db10aaeb5f2d1
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-14 10:17:55 -0500

    Added payload for creating new symbol

---
+++ back-end/modules/symbol/data-transfer-objects/new-symbol.payload.ts
@@ -0,0 +1,13 @@
+import { SymbolType } from '@/symbol/enums/symbol-type.enum';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+
+export class NewSymbolPayload {
+  @IsNotEmpty()
+  title: string = '';
+  @IsNotEmpty()
+  description: string = '';
+  @IsEnum(SymbolType)
+  type: SymbolType = SymbolType.Constant;
+  @IsNotEmpty()
+  content: string = '';
+};
