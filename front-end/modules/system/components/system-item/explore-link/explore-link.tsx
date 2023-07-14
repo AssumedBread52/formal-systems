0c85bcf0c167a557a32c5fa2d12f82a771bc7651
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-14 13:59:36 -0500

    Move conditional up

--- front-end/modules/system/components/system-item/explore-link/explore-link.tsx
+++ front-end/modules/system/components/system-item/explore-link/explore-link.tsx
@@ -1,23 +1,12 @@
 import { System } from '@/system/types/system';
-import { headers } from 'next/headers';
 import Link from 'next/link';
-import { Fragment, ReactElement } from 'react';
+import { ReactElement } from 'react';
 
 export const ExploreLink = (props: Omit<System, 'description' | 'createdByUserId'>): ReactElement => {
   const { id, title } = props;
 
-  const pathname = headers().get('x-invoke-path') ?? '';
-
-  const descriptionPath = `/formal-system/${id}/${title}`;
-
-  if (decodeURIComponent(pathname) === descriptionPath) {
-    return (
-      <Fragment />
-    );
-  }
-
   return (
-    <Link href={descriptionPath}>
+    <Link href={`/formal-system/${id}/${title}`}>
       Explore
     </Link>
   );
--- front-end/modules/system/components/system-item/system-item.tsx
+++ front-end/modules/system/components/system-item/system-item.tsx
@@ -3,7 +3,8 @@ import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
 import { System } from '@/system/types/system';
 import { UserSignatureProps } from '@/user/types/user-signature-props';
 import dynamic from 'next/dynamic';
-import { ComponentType, ReactElement, ReactNode } from 'react';
+import { headers } from 'next/headers';
+import { ComponentType, Fragment, ReactElement, ReactNode } from 'react';
 import { DeleteLink } from './delete-link/delete-link';
 import { EditLink } from './edit-link/edit-link';
 import { ExploreLink } from './explore-link/explore-link';
@@ -22,8 +23,20 @@ export const SystemItem = (props: System): ReactElement => {
     <DeleteLink id={id} title={title} createdByUserId={createdByUserId} />
   ] as ReactNode[];
 
+  const pathname = headers().get('x-invoke-path') ?? '';
+
+  const descriptionPath = `/formal-system/${id}/${title}`;
+
+  const exploreLink = (
+    <Fragment>
+      {decodeURIComponent(pathname) !== descriptionPath && (
+        <ExploreLink id={id} title={title} />
+      )}
+    </Fragment>
+  );
+
   return (
-    <AntdCard actions={actions} extra={<ExploreLink id={id} title={title} />} title={title} type='inner'>
+    <AntdCard actions={actions} extra={exploreLink} title={title} type='inner'>
       {description}
       <AntdDivider />
       <UserSignature userId={createdByUserId} />
