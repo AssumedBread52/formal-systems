f85ad3ee33af13fb688bd31f6204f4abc9b0e032
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-15 17:15:20 -0500

    Add pagination search controls

--- front-end/modules/symbol/components/search-symbols/search-symbols.tsx
+++ front-end/modules/symbol/components/search-symbols/search-symbols.tsx
@@ -1,12 +1,17 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
+import { PaginationSearchControls } from '@/common/components/pagination-search-controls/pagination-search-controls';
 import { Metadata } from 'next';
 import { ReactElement } from 'react';
 
 export const SearchSymbols = (): ReactElement => {
+  const total = 0;
+
   return (
     <AntdCard title='Symbols'>
-      Search Symbols
+      <PaginationSearchControls resultType='Symbols' total={total}>
+        Search Symbols
+      </PaginationSearchControls>
     </AntdCard>
   );
 };
