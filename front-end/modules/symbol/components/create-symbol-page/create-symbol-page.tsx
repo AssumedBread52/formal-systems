bb42b3eab04b46124ecb2d01e7e41dda41cb475a
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-16 12:10:12 -0500

    Added controls

--- front-end/modules/symbol/components/create-symbol-page/create-symbol-page.tsx
+++ front-end/modules/symbol/components/create-symbol-page/create-symbol-page.tsx
@@ -1,4 +1,8 @@
+import { AntdButton } from '@/common/components/antd-button/antd-button';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
+import { AntdFormItem } from '@/common/components/antd-form-item/antd-form-item';
+import { AntdSpace } from '@/common/components/antd-space/antd-space';
+import { CancelButton } from '@/common/components/cancel-button/cancel-button';
 import { Metadata } from 'next';
 import { ReactElement } from 'react';
 import { CreateSymbolForm } from './create-symbol-form/create-symbol-form';
@@ -8,6 +12,14 @@ export const CreateSymbolPage = (): ReactElement => {
     <AntdCard headStyle={{ textAlign: 'center' }} style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '600px' }} title='Create Symbol'>
       <CreateSymbolForm>
         Placeholder for creating new symbols
+        <AntdFormItem wrapperCol={{ xs: { span: 24 }, sm: { offset: 8 } }}>
+          <AntdSpace wrap>
+            <AntdButton htmlType='submit' type='primary'>
+              Submit
+            </AntdButton>
+            <CancelButton />
+          </AntdSpace>
+        </AntdFormItem>
       </CreateSymbolForm>
     </AntdCard>
   );
