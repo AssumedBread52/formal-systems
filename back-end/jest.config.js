9efcd0915180e93c5dc607d61cd0477f73db6025
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-18 17:32:47 -0500

    Add jest configuration

---
+++ back-end/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  coverageDirectory: '../coverage',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/$1'
+  },
+  rootDir: 'modules',
+  transform: {
+    '^.+\\.ts': 'ts-jest'
+  }
+};
