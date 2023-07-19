9db4e38d8b255645f771ce9b43eed86e7a318e4f
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-19 12:45:16 -0500

    Add mocks repositories and service

---
+++ back-end/modules/app/tests/mocks/config-service.mock.ts
@@ -0,0 +1,10 @@
+export class ConfigServiceMock {
+  get = jest.fn((key: 'JSON_WEB_TOKEN_SECRET' | 'JSON_WEB_TOKEN_EXPIRES_IN'): string => {
+    switch (key) {
+      case 'JSON_WEB_TOKEN_SECRET':
+        return 'secret test token';
+      case 'JSON_WEB_TOKEN_EXPIRES_IN':
+        return '1s';
+    }
+  });
+};
