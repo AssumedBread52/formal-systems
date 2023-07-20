3a9ce18e50b92c63dbac8bab64ff492b0c9cd7e0
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-20 05:55:01 -0500

    Test the dev-dependencies route

---
+++ back-end/modules/app/tests/dev-dependencies.test.ts
@@ -0,0 +1,34 @@
+import { AppModule } from '@/app/app.module';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { readFileSync } from 'fs';
+import * as request from 'supertest';
+
+describe('Test the dependencies call', (): void => {
+  let app: INestApplication;
+
+  beforeAll(async (): Promise<void> => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AppModule
+      ]
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    await app.init();
+  });
+
+  it('tests dependencies response', async (): Promise<void> => {
+    const response = await request(app.getHttpServer()).get('/app/dev-dependencies');
+
+    const { devDependencies } = JSON.parse(readFileSync(process.env.npm_package_json!, 'utf-8'));
+
+    expect(response.statusCode).toBe(HttpStatus.OK);
+    expect(response.body).toEqual(devDependencies);
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
