a84ccd6344ae08460cdfbfb19bb69c175e733dd5
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-20 08:13:44 -0500

    Add search by id test

---
+++ back-end/modules/user/tests/search-by-id.test.ts
@@ -0,0 +1,77 @@
+import { ConfigServiceMock } from '@/app/tests/mocks/config-service.mock';
+import { AuthModule } from '@/auth/auth.module';
+import { SystemEntity } from '@/system/system.entity';
+import { SystemRepositoryMock } from '@/system/tests/mocks/system-repository.mock';
+import { UserEntity } from '@/user/user.entity';
+import { UserModule } from '@/user/user.module';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as cookieParser from 'cookie-parser';
+import { ObjectId } from 'mongodb';
+import * as request from 'supertest';
+import { UserRepositoryMock } from './mocks/user-repository.mock';
+
+describe('Search by ID', (): void => {
+  const signUpPayload = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@test.com',
+    password: '123456'
+  };
+  let app: INestApplication;
+
+  beforeAll(async (): Promise<void> => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AuthModule,
+        UserModule
+      ]
+    }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
+
+    app = moduleRef.createNestApplication();
+
+    app.use(cookieParser());
+
+    await app.init();
+
+    await request(app.getHttpServer()).post('/auth/sign-up').send(signUpPayload);
+  });
+
+  it('fails with an invalid ID', async (): Promise<void> => {
+    const response = await request(app.getHttpServer()).get(`/user/${new ObjectId()}`);
+
+    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+    expect(response.body).toEqual({
+      error: 'Not Found',
+      message: 'User with given ID not found.',
+      statusCode: HttpStatus.NOT_FOUND
+    });
+  });
+
+  it('succeeds with valid token', async (): Promise<void> => {
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    expect(userRepositoryMock.users.length).toBeGreaterThan(0);
+
+    const { _id } = userRepositoryMock.users[0];
+
+    const response = await request(app.getHttpServer()).get(`/user/${_id}`);
+
+    expect(response.statusCode).toBe(HttpStatus.OK);
+    expect(response.body).toEqual({
+      id: userRepositoryMock.users[0]._id.toString(),
+      firstName: userRepositoryMock.users[0].firstName,
+      lastName: userRepositoryMock.users[0].lastName,
+      email: userRepositoryMock.users[0].email,
+      systemCount: userRepositoryMock.users[0].systemCount,
+      constantSymbolCount: userRepositoryMock.users[0].constantSymbolCount,
+      variableSymbolCount: userRepositoryMock.users[0].variableSymbolCount
+    });
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
