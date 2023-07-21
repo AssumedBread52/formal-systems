c4d62e4997a890f7359aa06672bc9cfe68014f2e
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-21 11:56:12 -0500

    Take advantage of helper functions

--- back-end/modules/system/tests/create-system.test.ts
+++ back-end/modules/system/tests/create-system.test.ts
@@ -1,6 +1,9 @@
 import { ConfigServiceMock } from '@/app/tests/mocks/config-service.mock';
 import { AuthModule } from '@/auth/auth.module';
-import { AuthService } from '@/auth/auth.service';
+import { generateToken } from '@/auth/tests/helpers/generate-token';
+import { testWithExpiredToken } from '@/auth/tests/helpers/test-with-expired-token';
+import { testWithInvalidToken } from '@/auth/tests/helpers/test-with-invalid-token';
+import { testWithMissingToken } from '@/auth/tests/helpers/test-with-missing-token';
 import { SystemEntity } from '@/system/system.entity';
 import { SystemModule } from '@/system/system.module';
 import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
@@ -10,17 +13,10 @@ import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import * as cookieParser from 'cookie-parser';
-import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 import { SystemRepositoryMock } from './mocks/system-repository.mock';
 
-describe('Sign Up', (): void => {
-  const signUpPayload = {
-    firstName: 'Test',
-    lastName: 'User',
-    email: 'test@test.com',
-    password: '123456'
-  };
+describe('Create System', (): void => {
   let app: INestApplication;
 
   beforeAll(async (): Promise<void> => {
@@ -37,43 +33,28 @@ describe('Sign Up', (): void => {
 
     await app.init();
 
-    await request(app.getHttpServer()).post('/auth/sign-up').send(signUpPayload);
+    await request(app.getHttpServer()).post('/auth/sign-up').send({
+      firstName: 'Test',
+      lastName: 'User',
+      email: 'test@test.com',
+      password: '123456'
+    });
   });
 
   it('fails without a token', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).post('/system');
-
-    expect(response.statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(response.body).toEqual({
-      message: 'Unauthorized',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
+    await testWithMissingToken(app, 'post', '/system');
   });
 
-  it('fails with token that has an invalid user id', async (): Promise<void> => {
-    const authService = app.get(AuthService);
+  it('fails with an invalid token', async (): Promise<void> => {
+    await testWithInvalidToken(app, 'post', '/system');
+  });
 
-    const token = await authService.generateToken(new ObjectId());
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    expect(response.statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(response.body).toEqual({
-      error: 'Unauthorized',
-      message: 'Invalid token.',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
+  it('fails with an expired token', async (): Promise<void> => {
+    await testWithExpiredToken(app, 'post', '/system');
   });
 
   it('fails with an invalid payload', async (): Promise<void> => {
-    const authService = app.get(AuthService);
-    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
-
-    expect(userRepositoryMock.users.length).toBeGreaterThan(0);
-
-    const token = await authService.generateToken(userRepositoryMock.users[0]._id);
+    const token = await generateToken(app);
 
     const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
       `token=${token}`
@@ -91,13 +72,7 @@ describe('Sign Up', (): void => {
   });
 
   it('succeeds if the title is unique for this user', async (): Promise<void> => {
-    const authService = app.get(AuthService);
-
-    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
-
-    expect(userRepositoryMock.users.length).toBeGreaterThan(0);
-
-    const token = await authService.generateToken(userRepositoryMock.users[0]._id);
+    const token = await generateToken(app);
 
     const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
       `token=${token}`
@@ -111,29 +86,23 @@ describe('Sign Up', (): void => {
   });
 
   it('fails if the title is not unique for this user', async (): Promise<void> => {
-    const authService = app.get(AuthService);
-    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+    const newSystemPayload = {
+      title: 'Test',
+      description: 'This is a test.'
+    };
 
-    expect(userRepositoryMock.users.length).toBeGreaterThan(0);
-
-    const token = await authService.generateToken(userRepositoryMock.users[0]._id);
+    const token = await generateToken(app);
 
     const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
       `token=${token}`
-    ]).send({
-      title: 'Test',
-      description: 'System1'
-    });
+    ]).send(newSystemPayload);
 
     expect(response.statusCode).toBe(HttpStatus.NO_CONTENT);
     expect(response.body).toEqual({});
 
     const collision = await request(app.getHttpServer()).post('/system').set('Cookie', [
       `token=${token}`
-    ]).send({
-      title: 'Test',
-      description: 'System2'
-    });
+    ]).send(newSystemPayload);
 
     expect(collision.statusCode).toBe(HttpStatus.CONFLICT);
     expect(collision.body).toEqual({
