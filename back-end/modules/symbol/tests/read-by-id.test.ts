74789517583dd7ca1a865d3bb1e82dbb3489a1fe
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-23 09:31:27 -0500

    Added symbol read by id test cases

---
+++ back-end/modules/symbol/tests/read-by-id.test.ts
@@ -0,0 +1,104 @@
+import { ConfigServiceMock } from '@/app/tests/mocks/config-service.mock';
+import { AuthModule } from '@/auth/auth.module';
+import { generateToken } from '@/auth/tests/helpers/generate-token';
+import { SymbolType } from '@/symbol/enums/symbol-type.enum';
+import { SymbolEntity } from '@/symbol/symbol.entity';
+import { SymbolModule } from '@/symbol/symbol.module';
+import { SystemEntity } from '@/system/system.entity';
+import { SystemRepositoryMock } from '@/system/tests/mocks/system-repository.mock';
+import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
+import { UserEntity } from '@/user/user.entity';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import * as cookieParser from 'cookie-parser';
+import { ObjectId } from 'mongodb';
+import * as request from 'supertest';
+import { SymbolRepositoryMock } from './mocks/symbol-repository.mock';
+
+describe('Read by ID', (): void => {
+  let app: INestApplication;
+
+  beforeAll(async (): Promise<void> => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AuthModule,
+        SymbolModule
+      ]
+    }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(SymbolEntity)).useClass(SymbolRepositoryMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
+
+    app = moduleRef.createNestApplication();
+
+    app.use(cookieParser());
+
+    await app.init();
+
+    await request(app.getHttpServer()).post('/auth/sign-up').send({
+      firstName: 'Test',
+      lastName: 'User',
+      email: 'test@test.com',
+      password: '123456'
+    });
+
+    const token = await generateToken(app);
+  
+    await request(app.getHttpServer()).post('/system').set('Cookie', [
+      `token=${token}`
+    ]).send({
+      title: 'Test',
+      description: 'System'
+    });
+
+    const systemRepositoryMock = app.get(getRepositoryToken(SystemEntity)) as SystemRepositoryMock;
+
+    const { _id } = systemRepositoryMock.systems[0];
+
+    await request(app.getHttpServer()).post(`/system/${_id}/symbol`).set('Cookie', [
+      `token=${token}`
+    ]).send({
+      title: 'Test',
+      description: 'Symbol',
+      type: SymbolType.Constant,
+      content: '\\alpha'
+    });
+  });
+
+  it('fails with an invalid ID', async (): Promise<void> => {
+    const response = await request(app.getHttpServer()).get(`/system/${new ObjectId()}/symbol/${new ObjectId()}`);
+
+    expect(response.statusCode).toBe(HttpStatus.NOT_FOUND);
+    expect(response.body).toEqual({
+      error: 'Not Found',
+      message: 'Symbol not found.',
+      statusCode: HttpStatus.NOT_FOUND
+    });
+  });
+
+  it('succeeds with a valid ID', async (): Promise<void> => {
+    const symbolRepositoryMock = app.get(getRepositoryToken(SymbolEntity)) as SymbolRepositoryMock;
+
+    expect(symbolRepositoryMock.symbols.length).toBeGreaterThan(0);
+
+    const { _id, title, description, type, content, axiomaticStatementAppearances, nonAxiomaticStatementAppearances, systemId, createdByUserId } = symbolRepositoryMock.symbols[0];
+
+    const response = await request(app.getHttpServer()).get(`/system/${new ObjectId()}/symbol/${_id}`);
+
+    expect(response.statusCode).toBe(HttpStatus.OK);
+    expect(response.body).toEqual({
+      id: _id.toString(),
+      title,
+      description,
+      type,
+      content,
+      axiomaticStatementAppearances,
+      nonAxiomaticStatementAppearances,
+      systemId: systemId.toString(),
+      createdByUserId: createdByUserId.toString()
+    });
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
