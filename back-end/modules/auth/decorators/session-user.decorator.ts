f100686cb171e787622f298fe47826acf93c60b6
Antonio Sanchez <asanchez16756@gmail.com>
2023-07-30 13:55:59 -0500

    Change file name to add type

--- back-end/modules/auth/decorators/session-user.decorator.ts
+++ back-end/modules/auth/decorators/session-user.decorator.ts
@@ -2,7 +2,7 @@ import { UserEntity } from '@/user/user.entity';
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { Request } from 'express';
 
-export const SessionUser = createParamDecorator((key: keyof UserEntity, context: ExecutionContext): UserEntity | UserEntity[keyof UserEntity] => {
+export const SessionUserDecorator = createParamDecorator((key: keyof UserEntity, context: ExecutionContext): UserEntity | UserEntity[keyof UserEntity] => {
   const request = context.switchToHttp().getRequest<Request>();
 
   const { user } = request;
--- back-end/modules/symbol/symbol.controller.ts
+++ back-end/modules/symbol/symbol.controller.ts
@@ -1,4 +1,4 @@
-import { SessionUser } from '@/auth/decorators/session-user';
+import { SessionUserDecorator } from '@/auth/decorators/session-user.decorator';
 import { JwtGuard } from '@/auth/guards/jwt.guard';
 import { IdPayload } from '@/common/data-transfer-objects/id.payload';
 import { SystemService } from '@/system/system.service';
@@ -17,7 +17,7 @@ export class SymbolController {
 
   @UseGuards(JwtGuard)
   @Delete(':id')
-  async deleteSymbol(@SessionUser('_id') sessionUserId: ObjectId, @Param('id') id: string): Promise<IdPayload> {
+  async deleteSymbol(@SessionUserDecorator('_id') sessionUserId: ObjectId, @Param('id') id: string): Promise<IdPayload> {
     const symbol = await this.symbolService.readById(id);
 
     if (!symbol) {
@@ -51,7 +51,7 @@ export class SymbolController {
 
   @UseGuards(JwtGuard)
   @Patch(':id')
-  async patchSymbol(@SessionUser('_id') sessionUserId: ObjectId, @Param('id') id: string, @Body(ValidationPipe) editSymbolPayload: EditSymbolPayload): Promise<IdPayload> {
+  async patchSymbol(@SessionUserDecorator('_id') sessionUserId: ObjectId, @Param('id') id: string, @Body(ValidationPipe) editSymbolPayload: EditSymbolPayload): Promise<IdPayload> {
     const symbol = await this.symbolService.readById(id);
 
     if (!symbol) {
@@ -70,7 +70,7 @@ export class SymbolController {
   @UseGuards(JwtGuard)
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
-  async postSymbol(@SessionUser('_id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Body(ValidationPipe) newSymbolPayload: NewSymbolPayload): Promise<void> {
+  async postSymbol(@SessionUserDecorator('_id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Body(ValidationPipe) newSymbolPayload: NewSymbolPayload): Promise<void> {
     const system = await this.systemService.readById(systemId);
 
     if (!system) {
