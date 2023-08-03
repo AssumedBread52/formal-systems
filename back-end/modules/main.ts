01c9ced2e7fb993e27204b8ca9ff8be9f628609b
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-03 07:51:30 -0500

    Remove default application will throw if missing

--- back-end/modules/main.ts
+++ back-end/modules/main.ts
@@ -14,7 +14,7 @@ const bootstrap = async (): Promise<void> => {
 
   const configService = app.get(ConfigService);
 
-  const port = configService.getOrThrow<number>('PORT') || 5000;
+  const port = configService.getOrThrow<number>('PORT');
 
   await app.listen(port);
 };
