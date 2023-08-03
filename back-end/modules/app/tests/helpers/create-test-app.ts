06142f49aa07e94a3a0d8c226d9e0928f775da47
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-03 06:55:42 -0500

    Add symbol module

--- back-end/modules/app/tests/helpers/create-test-app.ts
+++ back-end/modules/app/tests/helpers/create-test-app.ts
@@ -1,5 +1,8 @@
 import { ConfigServiceMock } from '@/app/tests/mocks/config-service.mock';
 import { AuthModule } from '@/auth/auth.module';
+import { SymbolEntity } from '@/symbol/symbol.entity';
+import { SymbolModule } from '@/symbol/symbol.module';
+import { SymbolRepositoryMock } from '@/symbol/tests/mocks/symbol-repository.mock';
 import { SystemEntity } from '@/system/system.entity';
 import { SystemModule } from '@/system/system.module';
 import { SystemRepositoryMock } from '@/system/tests/mocks/system-repository.mock';
@@ -17,10 +20,11 @@ export const createTestApp = async (): Promise<INestApplication> => {
   const testingModule = await Test.createTestingModule({
     imports: [
       AuthModule,
+      SymbolModule,
       SystemModule,
       UserModule
     ]
-  }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
+  }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(SymbolEntity)).useClass(SymbolRepositoryMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
 
   const app = testingModule.createNestApplication();
 
