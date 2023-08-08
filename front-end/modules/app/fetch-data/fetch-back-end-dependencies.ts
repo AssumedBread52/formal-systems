af833b505866167156ed7313e5c77c988756f930
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-08 06:31:44 -0500

    Static data fetching can't use rewrites

--- front-end/modules/app/fetch-data/fetch-back-end-dependencies.ts
+++ front-end/modules/app/fetch-data/fetch-back-end-dependencies.ts
@@ -1,5 +1,5 @@
 export const fetchBackEndDependencies = async (): Promise<Record<string, string>> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/app/dependencies`);
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/app/dependencies`);
 
   return response.json();
 };
--- front-end/modules/app/fetch-data/fetch-back-end-dev-dependencies.ts
+++ front-end/modules/app/fetch-data/fetch-back-end-dev-dependencies.ts
@@ -1,5 +1,5 @@
 export const fetchBackEndDevDependencies = async (): Promise<Record<string, string>> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/app/dev-dependencies`);
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/app/dev-dependencies`);
 
   return response.json();
 };
