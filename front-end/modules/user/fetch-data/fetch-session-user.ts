00b138e9268fcb3b5401bef83cab838658834d34
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-08 12:45:55 -0500

    Server side request to target back-end directly

--- front-end/modules/user/fetch-data/fetch-session-user.ts
+++ front-end/modules/user/fetch-data/fetch-session-user.ts
@@ -2,8 +2,7 @@ import { User } from '@/user/types/user';
 import { cookies } from 'next/headers';
 
 export const fetchSessionUser = async (): Promise<User> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/user/session-user`, {
-    cache: 'no-store',
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/user/session-user`, {
     headers: {
       Cookie: cookies().toString()
     }
