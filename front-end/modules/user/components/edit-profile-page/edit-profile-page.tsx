d5816ec478caf32d00eb35bb76e4e1bbdd4ab7df
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-13 14:17:33 -0500

    Switch to edit profile to SSR

--- front-end/modules/user/components/edit-profile-page/edit-profile-page.tsx
+++ front-end/modules/user/components/edit-profile-page/edit-profile-page.tsx
@@ -1,4 +1,5 @@
 import { AntdButton } from '@/common/components/antd-button/antd-button';
+import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { AntdFormItem } from '@/common/components/antd-form-item/antd-form-item';
 import { AntdSpace } from '@/common/components/antd-space/antd-space';
 import { CancelButton } from '@/common/components/cancel-button/cancel-button';
@@ -8,27 +9,32 @@ import { InputLastName } from '@/common/components/input-last-name/input-last-na
 import { InputPassword } from '@/common/components/input-password/input-password';
 import { Metadata } from 'next';
 import { ReactElement } from 'react';
-import { ProfileCard } from './profile-card/profile-card';
+import { EditProfileForm } from './edit-profile-form/edit-profile-form';
+import { fetchSessionUser } from '@/user/fetch-data/fetch-session-user';
 
 export const EditProfilePage = async (): Promise<ReactElement> => {
+  const { firstName, lastName, email } = await fetchSessionUser();
+
   return (
-    <ProfileCard>
-      <InputFirstName name='newFirstName' />
-      <InputLastName name='newLastName' />
-      <InputEmail name='newEmail' />
-      <InputPassword name='newPassword' optional />
-      <AntdFormItem wrapperCol={{ xs: { span: 24 }, sm: { offset: 8 } }}>
-        <AntdSpace wrap>
-          <AntdButton htmlType='submit' type='primary'>
-            Submit
-          </AntdButton>
-          <CancelButton />
-          <AntdButton htmlType='reset'>
-            Reset
-          </AntdButton>
-        </AntdSpace>
-      </AntdFormItem>
-    </ProfileCard>
+    <AntdCard headStyle={{ textAlign: 'center' }} style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '600px' }} title='Edit Profile'>
+      <EditProfileForm newFirstName={firstName} newLastName={lastName} newEmail={email}>
+        <InputFirstName name='newFirstName' />
+        <InputLastName name='newLastName' />
+        <InputEmail name='newEmail' />
+        <InputPassword name='newPassword' optional />
+        <AntdFormItem wrapperCol={{ xs: { span: 24 }, sm: { offset: 8 } }}>
+          <AntdSpace wrap>
+            <AntdButton htmlType='submit' type='primary'>
+              Submit
+            </AntdButton>
+            <CancelButton />
+            <AntdButton htmlType='reset'>
+              Reset
+            </AntdButton>
+          </AntdSpace>
+        </AntdFormItem>
+      </EditProfileForm>
+    </AntdCard>
   );
 };
 
---
+++ front-end/modules/user/fetch-data/fetch-session-user.ts
@@ -0,0 +1,13 @@
+import { User } from '@/user/types/user';
+import { cookies } from 'next/dist/client/components/headers';
+
+export const fetchSessionUser = async (): Promise<User> => {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/user/session-user`, {
+    cache: 'no-store',
+    headers: {
+      Cookie: cookies().toString()
+    }
+  });
+
+  return response.json();
+};
