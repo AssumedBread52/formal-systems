985efb4c6d9f6ec9f077e034c54e4fffe0ff427d
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-13 14:05:18 -0500

    Clear router cache on success

--- front-end/modules/common/hooks/use-route-back.ts
+++ front-end/modules/common/hooks/use-route-back.ts
@@ -2,10 +2,11 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export const useRouteBack = (goBack: boolean): void => {
-  const { back } = useRouter();
+  const { back, refresh } = useRouter();
 
   useEffect((): void => {
     if (goBack) {
+      refresh();
       back();
     }
   }, [back, goBack]);
