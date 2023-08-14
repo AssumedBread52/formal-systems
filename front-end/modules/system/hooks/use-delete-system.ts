a91a6f68de4c7848256e6d65004411826e142750
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 15:18:20 -0500

    Fix types

--- front-end/modules/system/hooks/use-delete-system.ts
+++ front-end/modules/system/hooks/use-delete-system.ts
@@ -2,12 +2,13 @@ import { api } from '@/app/api';
 import { TagTypes } from '@/app/types/tag-types';
 import { useRouteBack } from '@/common/hooks/use-route-back';
 import { IdPayload } from '@/common/types/id-payload';
+import { System } from '@/system/types/system';
 import { BaseQueryFn, MutationDefinition } from '@reduxjs/toolkit/dist/query';
 import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 
 const { useDeleteSystemMutation } = api;
 
-export const useDeleteSystem = (): [MutationTrigger<MutationDefinition<IdPayload, BaseQueryFn, TagTypes, IdPayload, 'api'>>, boolean, boolean] => {
+export const useDeleteSystem = (): [MutationTrigger<MutationDefinition<Pick<System, 'id'>, BaseQueryFn, TagTypes, IdPayload, 'api'>>, boolean, boolean] => {
   const [deleteSystem, { isError, isLoading, isSuccess }] = useDeleteSystemMutation();
 
   useRouteBack(isSuccess);
