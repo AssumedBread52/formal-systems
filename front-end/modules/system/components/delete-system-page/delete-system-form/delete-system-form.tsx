4308766bebb07b41664458b6cf4809d168fef9eb
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 15:19:35 -0500

    Clean up types

--- front-end/modules/system/components/delete-system-page/delete-system-form/delete-system-form.tsx
+++ front-end/modules/system/components/delete-system-page/delete-system-form/delete-system-form.tsx
@@ -4,13 +4,13 @@ import { AntdAlert } from '@/common/components/antd-alert/antd-alert';
 import { AntdForm } from '@/common/components/antd-form/antd-form';
 import { AntdLoadingOutlined } from '@/common/components/antd-loading-outlined/antd-loading-outlined';
 import { AntdSpin } from '@/common/components/antd-spin/antd-spin';
-import { IdPayload } from '@/common/types/id-payload';
 import { useDeleteSystem } from '@/system/hooks/use-delete-system';
+import { System } from '@/system/types/system';
 import { PropsWithChildren, ReactElement } from 'react';
 
-const TypedAntdForm = AntdForm<IdPayload>;
+const TypedAntdForm = AntdForm<Pick<System, 'id'>>;
 
-export const DeleteSystemForm = (props: PropsWithChildren<IdPayload>): ReactElement => {
+export const DeleteSystemForm = (props: PropsWithChildren<Pick<System, 'id'>>): ReactElement => {
   const { children } = props;
 
   const [deleteSystem, isDeletingSystem, hasFailed] = useDeleteSystem();
