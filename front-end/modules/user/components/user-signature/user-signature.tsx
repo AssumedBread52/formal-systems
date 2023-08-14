464b7fca883a8c4690bd177ff0d1bb76a12e75e2
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 06:50:08 -0500

    Simplify component

--- front-end/modules/user/components/user-signature/user-signature.tsx
+++ front-end/modules/user/components/user-signature/user-signature.tsx
@@ -1,16 +1,32 @@
+import { AntdAvatar } from '@/common/components/antd-avatar/antd-avatar';
+import { AntdBadge } from '@/common/components/antd-badge/antd-badge';
 import { AntdCardMeta } from '@/common/components/antd-card-meta/antd-card-meta';
+import { AntdTypographyText } from '@/common/components/antd-typography-text/antd-typography-text';
+import { AntdUserOutlined } from '@/common/components/antd-user-outlined/antd-user-outlined';
 import { fetchUser } from '@/user/fetch-data/fetch-user';
 import { UserSignatureProps } from '@/user/types/user-signature-props';
 import { ReactElement } from 'react';
-import { Description } from './description/description';
-import { UserAvatar } from './user-avatar/user-avatar';
 
 export const UserSignature = async (props: UserSignatureProps): Promise<ReactElement> => {
   const { userId } = props;
 
   const { firstName, lastName, email, systemCount, constantSymbolCount, variableSymbolCount } = await fetchUser(userId);
 
+  const userAvatar = (
+    <AntdBadge count={systemCount + constantSymbolCount + variableSymbolCount} title='Entities created'>
+      <AntdAvatar icon={<AntdUserOutlined />} size='large' />
+    </AntdBadge>
+  );
+
+  const signature = (
+    <AntdTypographyText italic>
+      {`${firstName} ${lastName}`}
+      <br />
+      {email}
+    </AntdTypographyText>
+  );
+
   return (
-    <AntdCardMeta avatar={<UserAvatar systemCount={systemCount} constantSymbolCount={constantSymbolCount} variableSymbolCount={variableSymbolCount} />} title='Created By' description={<Description firstName={firstName} lastName={lastName} email={email} />} />
+    <AntdCardMeta avatar={userAvatar} description={signature} title='Created By' />
   );
 };
