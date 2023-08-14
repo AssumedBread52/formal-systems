93278c0e5200bfa661c9b16297d08fb78e70fce4
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 07:24:53 -0500

    Rename function and use SSR

--- front-end/modules/system/fetch-data/fetch-systems-search.ts
+++ front-end/modules/system/fetch-data/fetch-systems-search.ts
@@ -1,10 +1,10 @@
 import { AppSearchParams } from '@/app/types/app-search-params';
-import { SearchResults } from '@/common/types/search-results';
+import { PaginatedSearchResults } from '@/common/types/paginated-search-results';
 import { System } from '@/system/types/system';
 import { stringify } from 'querystring';
 
-export const fetchSystemSearch = async (searchParams: AppSearchParams): Promise<SearchResults<System>> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/system?${stringify(searchParams)}`, {
+export const fetchSystemsSearch = async (searchParams: AppSearchParams): Promise<PaginatedSearchResults<System>> => {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system?${stringify(searchParams)}`, {
     cache: 'no-store'
   });
 
