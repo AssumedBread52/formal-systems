47580f9c158cdb9d4a469ec4967bab53f57aea18
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 07:25:54 -0500

    Use reworked common components

--- front-end/modules/system/components/search-systems/search-systems.tsx
+++ front-end/modules/system/components/search-systems/search-systems.tsx
@@ -1,7 +1,7 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
-import { PaginationSearchControls } from '@/common/components/pagination-search-controls/pagination-search-controls';
-import { fetchSystemSearch } from '@/system/fetch-data/fetch-systems-search';
+import { PaginatedSearchControls } from '@/common/components/paginated-search-controls/paginated-search-controls';
+import { fetchSystemsSearch } from '@/system/fetch-data/fetch-systems-search';
 import { ReactElement } from 'react';
 import { CreateLink } from './create-link/create-link';
 import { SystemList } from './system-list/system-list';
@@ -9,21 +9,21 @@ import { SystemList } from './system-list/system-list';
 export const SearchSystems = async (props: ServerSideProps): Promise<ReactElement> => {
   const { searchParams } = props;
 
-  if (!searchParams.count) {
-    searchParams.count = '10';
-  }
-
   if (!searchParams.page) {
     searchParams.page = '1';
   }
 
-  const { results, total } = await fetchSystemSearch(searchParams);
+  if (!searchParams.count) {
+    searchParams.count = '10';
+  }
+
+  const { results, total } = await fetchSystemsSearch(searchParams);
 
   return (
     <AntdCard extra={<CreateLink />} title='Formal Systems'>
-      <PaginationSearchControls resultType='Formal Systems' total={total}>
+      <PaginatedSearchControls resultType='Formal Systems' total={total}>
         <SystemList systems={results} />
-      </PaginationSearchControls>
+      </PaginatedSearchControls>
     </AntdCard>
   );
 };
