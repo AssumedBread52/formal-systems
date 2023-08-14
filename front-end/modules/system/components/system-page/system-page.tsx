4a186c3faf78df44d52a2b1e76630a61c5f2d5cf
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 15:38:18 -0500

    Allow edit and delete navigation from system page

--- front-end/modules/system/components/system-page/system-page.tsx
+++ front-end/modules/system/components/system-page/system-page.tsx
@@ -1,11 +1,15 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
+import { ProtectedContent } from '@/auth/components/protected-content/protected-content';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
+import { AntdDeleteOutlined } from '@/common/components/antd-delete-outlined/antd-delete-outlined';
 import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
+import { AntdEditOutlined } from '@/common/components/antd-edit-outlined/antd-edit-outlined';
 import { fetchSystem } from '@/system/fetch-data/fetch-system';
 import { UserSignatureProps } from '@/user/types/user-signature-props';
 import { Metadata } from 'next';
 import dynamic from 'next/dynamic';
-import { ComponentType, ReactElement } from 'react';
+import Link from 'next/link';
+import { ComponentType, ReactElement, ReactNode } from 'react';
 import { SymbolsDescription } from './symbols-description/symbols-description';
 
 const UserSignature = dynamic(async (): Promise<ComponentType<UserSignatureProps>> => {
@@ -21,9 +25,22 @@ export const SystemPage = async (props: ServerSideProps): Promise<ReactElement> 
 
   const { id, title, description, constantSymbolCount, variableSymbolCount, createdByUserId } = await fetchSystem(systemId);
 
+  const actions = [
+    <ProtectedContent userId={createdByUserId}>
+      <Link href={`/formal-system/${id}/${title}/edit`}>
+        <AntdEditOutlined />
+      </Link>
+    </ProtectedContent>,
+    <ProtectedContent userId={createdByUserId}>
+      <Link href={`/formal-system/${id}/${title}/delete`}>
+        <AntdDeleteOutlined />
+      </Link>
+    </ProtectedContent>
+  ] as ReactNode[];
+
   return (
     <AntdCard title={title}>
-      <AntdCard>
+      <AntdCard actions={actions}>
         {description}
         <AntdDivider />
         <UserSignature userId={createdByUserId} />
