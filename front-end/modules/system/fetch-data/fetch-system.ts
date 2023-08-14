78370b0afbbe7ee0d000082061d1a7a970b8f22e
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 11:35:19 -0500

    Use SSR

--- front-end/modules/system/fetch-data/fetch-system.ts
+++ front-end/modules/system/fetch-data/fetch-system.ts
@@ -1,7 +1,7 @@
 import { System } from '@/system/types/system';
 
 export const fetchSystem = async (systemId: string): Promise<System> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/system/${systemId}`, {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system/${systemId}`, {
     cache: 'no-store'
   });
 
