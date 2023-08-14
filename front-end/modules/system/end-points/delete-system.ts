8b3f8292ffdd2165e433280e5d3934f84f3bb2e5
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-14 15:14:54 -0500

    Clean up types and order tag description

--- front-end/modules/system/end-points/delete-system.ts
+++ front-end/modules/system/end-points/delete-system.ts
@@ -1,11 +1,12 @@
 import { Tags } from '@/app/constants/tags';
 import { TagTypes } from '@/app/types/tag-types';
 import { IdPayload } from '@/common/types/id-payload';
+import { System } from '@/system/types/system';
 import { BaseQueryFn, FetchArgs } from '@reduxjs/toolkit/dist/query';
 import { EndpointBuilder, MutationDefinition, TagDescription } from '@reduxjs/toolkit/dist/query/endpointDefinitions';
 
-export const deleteSystem = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>): MutationDefinition<IdPayload, BaseQueryFn, TagTypes, IdPayload, 'api'> => {
-  return builder.mutation<IdPayload, IdPayload>({
+export const deleteSystem = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>): MutationDefinition<Pick<System, 'id'>, BaseQueryFn, TagTypes, IdPayload, 'api'> => {
+  return builder.mutation<IdPayload, Pick<System, 'id'>>({
     invalidatesTags: (result?: IdPayload): TagDescription<TagTypes>[] => {
       if (!result) {
         return [];
@@ -14,15 +15,15 @@ export const deleteSystem = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'ap
       const { id } = result;
 
       return [
-        Tags.System,
         {
-          type: Tags.System,
-          id
-        }
+          id,
+          type: Tags.System
+        },
+        Tags.System
       ];
     },
-    query: (idPayload: IdPayload): FetchArgs => {
-      const { id } = idPayload;
+    query: (deletePayload: Pick<System, 'id'>): FetchArgs => {
+      const { id } = deletePayload;
 
       return {
         method: 'DELETE',
