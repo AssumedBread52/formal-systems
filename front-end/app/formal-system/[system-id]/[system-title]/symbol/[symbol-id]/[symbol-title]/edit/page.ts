2d034a6fa8e47dcd55ded8ba50caf19a29062c41
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-15 07:18:18 -0500

    Rename file extension and adjust component name

---
+++ front-end/app/formal-system/[system-id]/[system-title]/symbol/[symbol-id]/[symbol-title]/edit/page.ts
@@ -0,0 +1,1 @@
+export { EditSymbolPage as default, generateMetadata } from '@/symbol/components/edit-symbol-page/edit-symbol-page';
--- front-end/app/formal-system/[system-id]/[system-title]/symbol/[symbol-id]/[symbol-title]/edit/page.tsx
+++
@@ -1,1 +0,0 @@
-export { EditSystemPage as default, generateMetadata } from '@/symbol/components/edit-symbol-page/edit-symbol-page';
