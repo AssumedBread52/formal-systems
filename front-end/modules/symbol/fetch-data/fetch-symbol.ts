18fdebbfb9aa2606cfa1f4a545ccf8eb659c6f5c
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-15 07:08:44 -0500

    Switch to use SSR

--- front-end/modules/symbol/fetch-data/fetch-symbol.ts
+++ front-end/modules/symbol/fetch-data/fetch-symbol.ts
@@ -1,7 +1,7 @@
 import { Symbol } from '@/symbol/types/symbol';
 
 export const fetchSymbol = async (systemId: string, symbolId: string): Promise<Symbol> => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/system/${systemId}/symbol/${symbolId}`, {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system/${systemId}/symbol/${symbolId}`, {
     cache: 'no-store'
   });
 
