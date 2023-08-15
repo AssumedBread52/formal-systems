e5be480c8692e807d02afabb321719fe86753644
Antonio Sanchez <asanchez16756@gmail.com>
2023-08-15 15:20:20 -0500

    Clean up search symbols

--- front-end/modules/symbol/components/search-symbols/search-symbols.tsx
+++ front-end/modules/symbol/components/search-symbols/search-symbols.tsx
@@ -1,7 +1,7 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
-import { PaginationSearchControls } from '@/common/components/pagination-search-controls/pagination-search-controls';
-import { fetchSymbolSearch } from '@/symbol/fetch-data/fetch-symbols-search';
+import { PaginatedSearchControls } from '@/common/components/paginated-search-controls/paginated-search-controls';
+import { fetchSymbolsSearch } from '@/symbol/fetch-data/fetch-symbols-search';
 import { Metadata } from 'next';
 import { ReactElement } from 'react';
 import { CreateLink } from './create-link/create-link';
@@ -10,28 +10,28 @@ import { SymbolList } from './symbol-list/symbol-list';
 export const SearchSymbols = async (props: ServerSideProps): Promise<ReactElement> => {
   const { params, searchParams } = props;
 
-  if (!searchParams.count) {
-    searchParams.count = '10';
-  }
-
   if (!searchParams.page) {
     searchParams.page = '1';
   }
 
+  if (!searchParams.count) {
+    searchParams.count = '10';
+  }
+
   const { 'system-id': systemId = '', 'system-title': systemTitle = '' } = params;
 
-  const { results, total } = await fetchSymbolSearch(systemId, searchParams);
+  const { results, total } = await fetchSymbolsSearch(systemId, searchParams);
 
   return (
-    <AntdCard extra={<CreateLink id={systemId} title={systemTitle} />} title='Symbols'>
-      <PaginationSearchControls resultType='Symbols' total={total}>
+    <AntdCard extra={<CreateLink id={systemId} title={systemTitle} />} title={`${decodeURIComponent(systemTitle)} Symbols`}>
+      <PaginatedSearchControls resultType='Symbols' total={total}>
         <SymbolList symbols={results} systemTitle={systemTitle} />
-      </PaginationSearchControls>
+      </PaginatedSearchControls>
     </AntdCard>
   );
 };
 
-export const generateMetadata = async (props: ServerSideProps): Promise<Metadata> => {
+export const generateMetadata = (props: ServerSideProps): Metadata => {
   const { params } = props;
 
   const { 'system-title': systemTitle = '' } = params;
