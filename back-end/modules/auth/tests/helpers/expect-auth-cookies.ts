beec14246c6e99e2b37c4944053ef92bbaf11679
Antonio Sanchez <asanchez16756@gmail.com>
2023-10-10 20:17:23 -0500

    Adjust test for changes to configuration values

--- back-end/modules/auth/tests/helpers/expect-auth-cookies.ts
+++ back-end/modules/auth/tests/helpers/expect-auth-cookies.ts
@@ -4,6 +4,6 @@ export const expectAuthCookies = (response: Response): void => {
   const cookies = response.get('Set-Cookie');
 
   expect(cookies).toHaveLength(2);
-  expect(cookies[0]).toMatch(/^token=.+; Max-Age=60; .+; HttpOnly; Secure$/);
-  expect(cookies[1]).toMatch(/^authStatus=true; Max-Age=60; .+; Secure$/);
+  expect(cookies[0]).toMatch(/^token=.+; Max-Age=1; .+; HttpOnly; Secure$/);
+  expect(cookies[1]).toMatch(/^authStatus=true; Max-Age=1; .+; Secure$/);
 };
