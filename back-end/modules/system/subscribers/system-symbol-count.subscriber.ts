b26d94a9524d58b2bf8dc2350b0fbf7865b26b1d
Antonio Sanchez <asanchez16756@gmail.com>
2023-12-10 09:18:43 -0600

    Adjust symbol counts when a symbol type is updated

--- back-end/modules/system/subscribers/system-symbol-count.subscriber.ts
+++ back-end/modules/system/subscribers/system-symbol-count.subscriber.ts
@@ -2,7 +2,7 @@ import { SymbolType } from '@/symbol/enums/symbol-type.enum';
 import { SymbolEntity } from '@/symbol/symbol.entity';
 import { SystemEntity } from '@/system/system.entity';
 import { NotFoundException } from '@nestjs/common';
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent } from 'typeorm';
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent, UpdateEvent } from 'typeorm';
 
 @EventSubscriber()
 export class SystemSymbolCountSubscriber implements EntitySubscriberInterface<SymbolEntity> {
@@ -31,6 +31,43 @@ export class SystemSymbolCountSubscriber implements EntitySubscriberInterface<Sy
         break;
       case SymbolType.Variable:
         system.variableSymbolCount++;
+        break;
+    }
+
+    systemRepository.save(system);
+  }
+
+  async afterUpdate(event: UpdateEvent<SymbolEntity>): Promise<void> {
+    const { connection, databaseEntity, entity } = event;
+
+    if (!entity) {
+      return;
+    }
+
+    const { type, systemId } = databaseEntity;
+
+    if (type === entity.type) {
+      return;
+    }
+
+    const systemRepository = connection.getMongoRepository(SystemEntity);
+
+    const system = await systemRepository.findOneBy({
+      _id: systemId
+    });
+
+    if (!system) {
+      throw new NotFoundException('System not found.');
+    }
+
+    switch (type) {
+      case SymbolType.Constant:
+        system.constantSymbolCount--;
+        system.variableSymbolCount++;
+        break;
+      case SymbolType.Variable:
+        system.constantSymbolCount++;
+        system.variableSymbolCount--;
         break;
     }
 
