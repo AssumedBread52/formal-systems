a09a81d0344539d51d61274a287beaf9e8938900
Antonio Sanchez <asanchez16756@gmail.com>
2023-12-10 08:07:15 -0600

    Improve order

--- back-end/modules/app/tests/mocks/config-service.mock.ts
+++ back-end/modules/app/tests/mocks/config-service.mock.ts
@@ -1,5 +1,5 @@
 export class ConfigServiceMock {
-  getOrThrow = jest.fn((key: 'AUTH_COOKIE_MAX_AGE_MILLISECONDS' | 'JSON_WEB_TOKEN_SECRET' | 'JSON_WEB_TOKEN_EXPIRES_IN'): string => {
+  getOrThrow = jest.fn((key: 'AUTH_COOKIE_MAX_AGE_MILLISECONDS' | 'JSON_WEB_TOKEN_EXPIRES_IN' | 'JSON_WEB_TOKEN_SECRET'): string => {
     switch (key) {
       case 'AUTH_COOKIE_MAX_AGE_MILLISECONDS':
         return '1000';
