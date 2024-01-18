4ca28f1612041652e09b8ce1d2e2467a6f0e63c3
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-18 13:16:55 -0600

    Rename variable

--- front-end/modules/symbol/fetch-data/fetch-symbols-search.ts
+++ front-end/modules/symbol/fetch-data/fetch-symbols-search.ts
@@ -3,8 +3,8 @@ import { PaginatedSearchResults } from '@/common/types/paginated-search-results'
 import { Symbol } from '@/symbol/types/symbol';
 import { stringify } from 'querystring';
 
-export const fetchSymbolsSearch = async (systemId: string, searchParams: QueryParams): Promise<PaginatedSearchResults<Symbol>> => {
-  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system/${systemId}/symbol?${stringify(searchParams)}`, {
+export const fetchSymbolsSearch = async (systemId: string, queryParams: QueryParams): Promise<PaginatedSearchResults<Symbol>> => {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system/${systemId}/symbol?${stringify(queryParams)}`, {
     cache: 'no-store'
   });
 
--- front-end/modules/system/fetch-data/fetch-systems-search.ts
+++ front-end/modules/system/fetch-data/fetch-systems-search.ts
@@ -3,8 +3,8 @@ import { PaginatedSearchResults } from '@/common/types/paginated-search-results'
 import { System } from '@/system/types/system';
 import { stringify } from 'querystring';
 
-export const fetchSystemsSearch = async (searchParams: QueryParams): Promise<PaginatedSearchResults<System>> => {
-  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system?${stringify(searchParams)}`, {
+export const fetchSystemsSearch = async (queryParams: QueryParams): Promise<PaginatedSearchResults<System>> => {
+  const response = await fetch(`http://${process.env.BACK_END_HOSTNAME}:${process.env.BACK_END_PORT}/system?${stringify(queryParams)}`, {
     cache: 'no-store'
   });
 
