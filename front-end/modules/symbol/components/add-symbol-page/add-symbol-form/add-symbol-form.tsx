29d8a6bcb655b6536a297790595c557503530a3e
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-19 08:53:45 -0600

    Switch add symbol to use route params

--- front-end/modules/symbol/components/add-symbol-page/add-symbol-form/add-symbol-form.tsx
+++ front-end/modules/symbol/components/add-symbol-page/add-symbol-form/add-symbol-form.tsx
@@ -1,24 +1,27 @@
 'use client';
 
+import { RouteParams } from '@/app/types/route-params';
 import { AntdAlert } from '@/common/components/antd-alert/antd-alert';
 import { AntdForm } from '@/common/components/antd-form/antd-form';
 import { AntdLoadingOutlined } from '@/common/components/antd-loading-outlined/antd-loading-outlined';
 import { AntdSpin } from '@/common/components/antd-spin/antd-spin';
 import { useAddSymbol } from '@/symbol/hooks/use-add-symbol';
 import { NewSymbolPayload } from '@/symbol/types/new-symbol-payload';
-import { Symbol } from '@/symbol/types/symbol';
+import { useParams } from 'next/navigation';
 import { PropsWithChildren, ReactElement } from 'react';
 
 const TypedAntdForm = AntdForm<NewSymbolPayload>;
 
-export const AddSymbolForm = (props: PropsWithChildren<Pick<Symbol, 'systemId'>>): ReactElement => {
+export const AddSymbolForm = (props: PropsWithChildren): ReactElement => {
   const { children } = props;
+
+  const { 'system-id': systemId = '' } = useParams<RouteParams>();
 
   const [addSymbol, isAddingSymbol, hasFailed] = useAddSymbol();
 
   return (
     <AntdSpin indicator={<AntdLoadingOutlined />} size='large' spinning={isAddingSymbol}>
-      <TypedAntdForm initialValues={props} labelCol={{ xs: { span: 0 }, sm: { span: 8 } }} wrapperCol={{ xs: { span: 24 }, sm: { span: 16 } }} onFinish={addSymbol}>
+      <TypedAntdForm initialValues={{ systemId }} labelCol={{ xs: { span: 0 }, sm: { span: 8 } }} wrapperCol={{ xs: { span: 24 }, sm: { span: 16 } }} onFinish={addSymbol}>
         {children}
       </TypedAntdForm>
       {hasFailed && (
--- front-end/modules/symbol/components/add-symbol-page/add-symbol-page.tsx
+++ front-end/modules/symbol/components/add-symbol-page/add-symbol-page.tsx
@@ -1,4 +1,3 @@
-import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdButton } from '@/common/components/antd-button/antd-button';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
 import { AntdFormItem } from '@/common/components/antd-form-item/antd-form-item';
@@ -13,14 +12,10 @@ import { Metadata } from 'next';
 import { ReactElement } from 'react';
 import { AddSymbolForm } from './add-symbol-form/add-symbol-form';
 
-export const AddSymbolPage = (props: ServerSideProps): ReactElement => {
-  const { params } = props;
-
-  const { 'system-id': systemId = '' } = params;
-
+export const AddSymbolPage = (): ReactElement => {
   return (
     <AntdCard headStyle={{ textAlign: 'center' }} style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '600px' }} title='Add Symbol'>
-      <AddSymbolForm systemId={systemId}>
+      <AddSymbolForm>
         <InputTitle name='title' />
         <InputDescription name='description' />
         <InputSymbolType name='type' />
