9349ddd25b608c95ba87dfa538e1c7a86dea3a52
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-23 09:15:33 -0600

    Move symbol item component to highest level and reuse

--- front-end/modules/symbol/components/symbol-page/symbol-page.tsx
+++ front-end/modules/symbol/components/symbol-page/symbol-page.tsx
@@ -1,24 +1,12 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
-import { ProtectedContent } from '@/auth/components/protected-content/protected-content';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
-import { AntdDeleteOutlined } from '@/common/components/antd-delete-outlined/antd-delete-outlined';
+import { AntdDescriptionsItem } from '@/common/components/antd-descriptions-item/antd-descriptions-item';
+import { AntdDescriptions } from '@/common/components/antd-descriptions/antd-descriptions';
 import { AntdDivider } from '@/common/components/antd-divider/antd-divider';
-import { AntdEditOutlined } from '@/common/components/antd-edit-outlined/antd-edit-outlined';
-import { RenderMath } from '@/common/components/render-math/render-math';
-import { SymbolType } from '@/symbol/enums/symbol-type';
+import { SymbolItem } from '@/symbol/components/symbol-item/symbol-item';
 import { fetchSymbol } from '@/symbol/fetch-data/fetch-symbol';
-import { UserSignatureProps } from '@/user/types/user-signature-props';
 import { Metadata } from 'next';
-import dynamic from 'next/dynamic';
-import Link from 'next/link';
-import { ComponentType, ReactElement, ReactNode } from 'react';
-import { SymbolMetrics } from './symbol-metrics/symbol-metrics';
-
-const UserSignature = dynamic(async (): Promise<ComponentType<UserSignatureProps>> => {
-  const { UserSignature } = await import('@/user/components/user-signature/user-signature');
-
-  return UserSignature;
-});
+import { ReactElement } from 'react';
 
 export const SymbolPage = async (props: ServerSideProps): Promise<ReactElement> => {
   const { params } = props;
@@ -27,32 +15,26 @@ export const SymbolPage = async (props: ServerSideProps): Promise<ReactElement> 
 
   const { id, title, description, type, content, axiomAppearances, theoremAppearances, deductionAppearances, createdByUserId } = await fetchSymbol(systemId, symbolId);
 
-  const actions = [
-    <ProtectedContent userId={createdByUserId}>
-      <Link href={`/formal-system/${systemId}/symbol/${id}/edit`}>
-        <AntdEditOutlined />
-      </Link>
-    </ProtectedContent>,
-    <ProtectedContent userId={createdByUserId}>
-      <Link href={`/formal-system/${systemId}/symbol/${id}/remove`}>
-        <AntdDeleteOutlined />
-      </Link>
-    </ProtectedContent>
-  ] as ReactNode[];
-
   return (
-    <AntdCard title={title}>
-      <AntdCard actions={actions}>
-        {description}
-        <AntdDivider />
-        {SymbolType[type]}
-        <AntdDivider />
-        <RenderMath content={content} />
-        <AntdDivider />
-        <UserSignature userId={createdByUserId} />
-      </AntdCard>
+    <AntdCard>
+      <SymbolItem id={id} title={title} description={description} type={type} content={content} systemId={systemId} createdByUserId={createdByUserId} />
       <AntdDivider />
-      <SymbolMetrics axiomAppearances={axiomAppearances} theoremAppearances={theoremAppearances} deductionAppearances={deductionAppearances} />
+      <AntdCard title='Metrics'>
+        <AntdDescriptions bordered>
+          <AntdDescriptionsItem label='Appearance Count'>
+            {axiomAppearances + theoremAppearances + deductionAppearances}
+          </AntdDescriptionsItem>
+          <AntdDescriptionsItem label='Appearance in Axioms Count'>
+            {axiomAppearances}
+          </AntdDescriptionsItem>
+          <AntdDescriptionsItem label='Appearance in Theorems Count'>
+            {theoremAppearances}
+          </AntdDescriptionsItem>
+          <AntdDescriptionsItem label='Appearance in Deductions Count'>
+            {deductionAppearances}
+          </AntdDescriptionsItem>
+        </AntdDescriptions>
+      </AntdCard>
     </AntdCard>
   );
 };
