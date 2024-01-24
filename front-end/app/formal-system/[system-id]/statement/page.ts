b47b71fd2be59735b6ab39d430dfe1026118b489
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-23 18:00:12 -0600

    Add placeholder for statements page

---
+++ front-end/app/formal-system/[system-id]/statement/page.ts
@@ -0,0 +1,12 @@
+import { ServerSideProps } from '@/app/types/server-side-props';
+import { redirect } from 'next/navigation';
+
+const Page = (props: ServerSideProps): never => {
+  const { params } = props;
+
+  const { 'system-id': systemId = '' } = params;
+
+  redirect(`/formal-system/${systemId}/statements`);
+};
+
+export default Page;
