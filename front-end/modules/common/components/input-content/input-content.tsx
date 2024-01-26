af29a7eb2f94c8e9ed6c1cd4a3cf87dcf435ec22
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-26 05:02:05 -0600

    Remove input props renaming

--- front-end/modules/common/components/input-content/input-content.tsx
+++ front-end/modules/common/components/input-content/input-content.tsx
@@ -1,11 +1,11 @@
 import { AntdFormItem } from '@/common/components/antd-form-item/antd-form-item';
 import { AntdInput } from '@/common/components/antd-input/antd-input';
-import { InputProps as InputFieldProps } from '@/common/types/input-props';
+import { InputProps } from '@/common/types/input-props';
 import { Rule } from 'antd/es/form';
 import { Fragment, ReactElement } from 'react';
 import { DisplayContent } from './display-content/display-content';
 
-export const InputContent = (props: InputFieldProps): ReactElement => {
+export const InputContent = (props: InputProps): ReactElement => {
   const { name, optional } = props;
 
   const rules = [
