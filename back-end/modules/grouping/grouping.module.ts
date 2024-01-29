16c711f2a4f4f0d11ded408b7405765158e9ff17
Antonio Sanchez <asanchez16756@gmail.com>
2024-01-29 12:00:51 -0600

    Add their respective entity repositories to the grouping and statement modules

--- back-end/modules/grouping/grouping.module.ts
+++ back-end/modules/grouping/grouping.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GroupingEntity } from './grouping.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      GroupingEntity
+    ])
+  ]
 })
 export class GroupingModule {
 };
--- back-end/modules/statement/statement.module.ts
+++ back-end/modules/statement/statement.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StatementEntity } from './statement.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      StatementEntity
+    ])
+  ]
 })
 export class StatementModule {
 };
