4fbd65ae811cec3268d204a95afe544d38430d13
Antonio Sanchez <asanchez16756@gmail.com>
2024-02-01 12:36:28 -0600

    Add global validation pipe and statement classes

--- back-end/modules/app/tests/helpers/create-test-app.ts
+++ back-end/modules/app/tests/helpers/create-test-app.ts
@@ -3,6 +3,9 @@ import { AuthModule } from '@/auth/auth.module';
 import { GroupingEntity } from '@/grouping/grouping.entity';
 import { GroupingModule } from '@/grouping/grouping.module';
 import { GroupingRepositoryMock } from '@/grouping/tests/mocks/grouping-repository.mock';
+import { StatementEntity } from '@/statement/statement.entity';
+import { StatementModule } from '@/statement/statement.module';
+import { StatementRepositoryMock } from '@/statement/tests/mocks/statement-repository.mock';
 import { SymbolEntity } from '@/symbol/symbol.entity';
 import { SymbolModule } from '@/symbol/symbol.module';
 import { SymbolRepositoryMock } from '@/symbol/tests/mocks/symbol-repository.mock';
@@ -12,7 +15,7 @@ import { SystemRepositoryMock } from '@/system/tests/mocks/system-repository.moc
 import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
 import { UserEntity } from '@/user/user.entity';
 import { UserModule } from '@/user/user.module';
-import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
+import { ClassSerializerInterceptor, INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
@@ -24,11 +27,12 @@ export const createTestApp = async (): Promise<INestApplication> => {
     imports: [
       AuthModule,
       GroupingModule,
+      StatementModule,
       SymbolModule,
       SystemModule,
       UserModule
     ]
-  }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(GroupingEntity)).useClass(GroupingRepositoryMock).overrideProvider(getRepositoryToken(SymbolEntity)).useClass(SymbolRepositoryMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
+  }).overrideProvider(ConfigService).useClass(ConfigServiceMock).overrideProvider(getRepositoryToken(GroupingEntity)).useClass(GroupingRepositoryMock).overrideProvider(getRepositoryToken(StatementEntity)).useClass(StatementRepositoryMock).overrideProvider(getRepositoryToken(SymbolEntity)).useClass(SymbolRepositoryMock).overrideProvider(getRepositoryToken(SystemEntity)).useClass(SystemRepositoryMock).overrideProvider(getRepositoryToken(UserEntity)).useClass(UserRepositoryMock).compile();
 
   const app = testingModule.createNestApplication();
 
@@ -37,6 +41,10 @@ export const createTestApp = async (): Promise<INestApplication> => {
   const reflector = app.get(Reflector);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }));
 
   await app.init();
 
