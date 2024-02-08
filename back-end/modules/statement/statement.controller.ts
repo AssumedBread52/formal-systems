ef86060b5b4b5a154a7126ff4fe6a946a3c69ce8
Antonio Sanchez <asanchez16756@gmail.com>
2024-02-08 08:08:42 -0600

    Add ability to add statement to BE

--- back-end/modules/statement/statement.controller.ts
+++ back-end/modules/statement/statement.controller.ts
@@ -1,5 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { SessionUserDecorator } from '@/auth/decorators/session-user.decorator';
+import { JwtGuard } from '@/auth/guards/jwt.guard';
+import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { ObjectId } from 'mongodb';
+import { NewStatementPayload } from './payloads/new-statement.payload';
+import { StatementService } from './statement.service';
+import { ObjectIdDecorator } from '@/common/decorators/object-id.decorator';
 
 @Controller('system/:systemId/statement')
 export class StatementController {
+  constructor(private statementService: StatementService) {
+  }
+
+  @UseGuards(JwtGuard)
+  @Post()
+  async postStatement(@SessionUserDecorator('_id') sessionUserId: ObjectId, @ObjectIdDecorator('systemId') systemId: ObjectId, @Body(ValidationPipe) newStatementPayload: NewStatementPayload): Promise<void> {
+    await this.statementService.create(newStatementPayload, systemId, sessionUserId);
+  }
 };
--- back-end/modules/statement/statement.service.ts
+++ back-end/modules/statement/statement.service.ts
@@ -1,5 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ObjectId } from 'mongodb';
+import { MongoRepository } from 'typeorm';
+import { NewStatementPayload } from './payloads/new-statement.payload';
+import { StatementEntity } from './statement.entity';
 
 @Injectable()
 export class StatementService {
+  constructor(@InjectRepository(StatementEntity) private statementRepository: MongoRepository<StatementEntity>) {
+  }
+
+  async create(newStatementPayload: NewStatementPayload, systemId: ObjectId, sessionUserId: ObjectId): Promise<StatementEntity> {
+    const { title, description } = newStatementPayload;
+
+    const statement = new StatementEntity();
+
+    statement.title = title;
+    statement.description = description;
+    statement.systemId = systemId;
+    statement.createdByUserId = sessionUserId;
+
+    return this.statementRepository.save(statement);
+  }
 };
