a54fafa53631463e548b31c0cb06698f1532a47e
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-09 06:09:35 -0500

    Add mock method

--- back-end/modules/statement/tests/mocks/statement-repository.mock.ts
+++ back-end/modules/statement/tests/mocks/statement-repository.mock.ts
@@ -1,6 +1,8 @@
 import { StatementEntity } from '@/statement/statement.entity';
 
 export class StatementRepositoryMock {
+  findOneBy = jest.fn();
+
   save = jest.fn((statement: StatementEntity): StatementEntity => {
     return statement;
   });
