2439718476b4e273635950e71b7d5fe038168a10
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-11 06:40:27 -0500

    Fix typo

--- back-end/modules/statement/statement.controller.ts
+++ back-end/modules/statement/statement.controller.ts
@@ -104,7 +104,7 @@ export class StatementController {
     const system = await this.systemService.readById(systemId);
 
     if (!system) {
-      throw new NotFoundException('SStatements cannot be added to a formal system that does not exist.');
+      throw new NotFoundException('Statements cannot be added to a formal system that does not exist.');
     }
 
     const { createdByUserId } = system;
