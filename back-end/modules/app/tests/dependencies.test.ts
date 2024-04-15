21954c8d13ceebbffce8cc6ae20ae7660bd228f9
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-15 13:14:26 -0500

    Simplify dependencies tests

--- back-end/modules/app/tests/dependencies.test.ts
+++ back-end/modules/app/tests/dependencies.test.ts
@@ -1,8 +1,8 @@
-import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
 import { HttpStatus, INestApplication } from '@nestjs/common';
-import { readFileSync } from 'fs';
 import * as request from 'supertest';
 import { createTestApp } from './helpers/create-test-app';
+import { readFileSync } from 'fs';
+import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
 
 describe('Dependencies', (): void => {
   let app: INestApplication;
@@ -11,12 +11,17 @@ describe('Dependencies', (): void => {
     app = await createTestApp();
   });
 
-  it('succeeds in returning the dependencies with their respective versions', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).get('/app/dependencies');
+  const cases = [
+    ['dependencies', 'dependencies'],
+    ['dev-dependencies', 'devDependencies']
+  ];
 
-    const { dependencies } = JSON.parse(readFileSync(process.env.npm_package_json!, 'utf-8'));
+  it.each(cases)('succeeds %s', async (type: string, key: string): Promise<void> => {
+    const response = await request(app.getHttpServer()).get(`/app/${type}`);
 
-    expectCorrectResponse(response, HttpStatus.OK, dependencies);
+    const packageJson = JSON.parse(readFileSync(process.env.npm_package_json!, 'utf-8'));
+
+    expectCorrectResponse(response, HttpStatus.OK, packageJson[key]);
   });
 
   afterAll(async (): Promise<void> => {
