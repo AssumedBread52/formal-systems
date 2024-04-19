dc4ec29dab1e80b4691706125f82140058c7d7d3
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-19 09:17:33 -0500

    Added route parameter tests

--- back-end/modules/statement/tests/update-statement.test.ts
+++ back-end/modules/statement/tests/update-statement.test.ts
@@ -1,8 +1,15 @@
 import { createTestApp } from '@/app/tests/helpers/create-test-app';
+import { AuthService } from '@/auth/auth.service';
 import { testExpiredToken } from '@/auth/tests/helpers/test-expired-token';
 import { testInvalidToken } from '@/auth/tests/helpers/test-invalid-token';
 import { testMissingToken } from '@/auth/tests/helpers/test-missing-token';
-import { INestApplication } from '@nestjs/common';
+import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
+import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
+import { UserEntity } from '@/user/user.entity';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ObjectId } from 'mongodb';
+import * as request from 'supertest';
 
 describe('Update Statement', (): void => {
   let app: INestApplication;
@@ -21,6 +28,56 @@ describe('Update Statement', (): void => {
 
   it('fails with an invalid token', async (): Promise<void> => {
     await testInvalidToken(app, 'patch', '/system/1/statement/1');
+  });
+
+  it('fails with an invalid statement ID', async (): Promise<void> => {
+    const user = new UserEntity();
+
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).patch('/system/1/statement/1').set('Cookie', [
+      `token=${token}`
+    ]).send({
+      newDistinctVariableRestrictions: 'invalid',
+      newVariableTypeHypotheses: 'invalid',
+      newLogicalHypotheses: 'invalid',
+      newAssertion: 'invalid'
+    });
+
+    expectCorrectResponse(response, HttpStatus.UNPROCESSABLE_ENTITY, {
+      error: 'Unprocessable Entity',
+      message: 'statementId should be a mongodb id',
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    });
+  });
+
+  it('fails with an invalid system ID', async (): Promise<void> => {
+    const user = new UserEntity();
+
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).patch(`/system/1/statement/${new ObjectId()}`).set('Cookie', [
+      `token=${token}`
+    ]).send({
+      newDistinctVariableRestrictions: 'invalid',
+      newVariableTypeHypotheses: 'invalid',
+      newLogicalHypotheses: 'invalid',
+      newAssertion: 'invalid'
+    });
+
+    expectCorrectResponse(response, HttpStatus.UNPROCESSABLE_ENTITY, {
+      error: 'Unprocessable Entity',
+      message: 'systemId should be a mongodb id',
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    });
   });
 
   afterAll(async (): Promise<void> => {
