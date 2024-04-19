58f5530c1621d13d4f42d1fc934cc8e985a0afae
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-19 10:50:15 -0500

    Added delete statement tests

---
+++ back-end/modules/statement/tests/delete-statement.test.ts
@@ -0,0 +1,146 @@
+import { createTestApp } from '@/app/tests/helpers/create-test-app';
+import { AuthService } from '@/auth/auth.service';
+import { testExpiredToken } from '@/auth/tests/helpers/test-expired-token';
+import { testInvalidToken } from '@/auth/tests/helpers/test-invalid-token';
+import { testMissingToken } from '@/auth/tests/helpers/test-missing-token';
+import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
+import { StatementEntity } from '@/statement/statement.entity';
+import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
+import { UserEntity } from '@/user/user.entity';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ObjectId } from 'mongodb';
+import * as request from 'supertest';
+import { StatementRepositoryMock } from './mocks/statement-repository.mock';
+
+describe('Delete Statement', (): void => {
+  let app: INestApplication;
+
+  beforeAll(async (): Promise<void> => {
+    app = await createTestApp();
+  });
+
+  it('fails without a token', async (): Promise<void> => {
+    await testMissingToken(app, 'delete', '/system/1/statement/1');
+  });
+
+  it('fails with an expired token', async (): Promise<void> => {
+    await testExpiredToken(app, 'delete', '/system/1/statement/1');
+  });
+
+  it('fails with an invalid token', async (): Promise<void> => {
+    await testInvalidToken(app, 'delete', '/system/1/statement/1');
+  });
+
+  it('fails with an invalid statement ID', async (): Promise<void> => {
+    const user = new UserEntity();
+
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).delete('/system/1/statement/1').set('Cookie', [
+      `token=${token}`
+    ]);
+
+    expectCorrectResponse(response, HttpStatus.UNPROCESSABLE_ENTITY, {
+      error: 'Unprocessable Entity',
+      message: 'statementId should be a mongodb id',
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    });
+  });
+
+  it('fails with an invalid system ID', async (): Promise<void> => {
+    const user = new UserEntity();
+
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).delete(`/system/1/statement/${new ObjectId()}`).set('Cookie', [
+      `token=${token}`
+    ]);
+
+    expectCorrectResponse(response, HttpStatus.UNPROCESSABLE_ENTITY, {
+      error: 'Unprocessable Entity',
+      message: 'systemId should be a mongodb id',
+      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    });
+  });
+
+  it('succeeds if the statement does not exist', async (): Promise<void> => {
+    const statementId = new ObjectId();
+
+    const user = new UserEntity();
+
+    const statementRepositoryMock = app.get(getRepositoryToken(StatementEntity)) as StatementRepositoryMock;
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    statementRepositoryMock.findOneBy.mockReturnValueOnce(null);
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).delete(`/system/${new ObjectId()}/statement/${statementId}`).set('Cookie', [
+      `token=${token}`
+    ]);
+
+    expectCorrectResponse(response, HttpStatus.OK, {
+      id: statementId.toString()
+    });
+  });
+
+  it('fails if the user did not create the statement', async (): Promise<void> => {
+    const statement = new StatementEntity();
+    const user = new UserEntity();
+
+    const statementRepositoryMock = app.get(getRepositoryToken(StatementEntity)) as StatementRepositoryMock;
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    statementRepositoryMock.findOneBy.mockReturnValueOnce(statement);
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).delete(`/system/${statement.systemId}/statement/${statement._id}`).set('Cookie', [
+      `token=${token}`
+    ]);
+
+    expectCorrectResponse(response, HttpStatus.FORBIDDEN, {
+      error: 'Forbidden',
+      message: 'You cannot delete a statement unless you created it.',
+      statusCode: HttpStatus.FORBIDDEN
+    });
+  });
+
+  it('succeeds', async (): Promise<void> => {
+    const statement = new StatementEntity();
+    const user = new UserEntity();
+
+    statement.createdByUserId = user._id;
+
+    const statementRepositoryMock = app.get(getRepositoryToken(StatementEntity)) as StatementRepositoryMock;
+    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+
+    statementRepositoryMock.findOneBy.mockReturnValueOnce(statement);
+    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
+
+    const token = await app.get(AuthService).generateToken(user._id);
+
+    const response = await request(app.getHttpServer()).delete(`/system/${statement.systemId}/statement/${statement._id}`).set('Cookie', [
+      `token=${token}`
+    ]);
+
+    expectCorrectResponse(response, HttpStatus.OK, {
+      id: statement._id.toString()
+    });
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
--- back-end/modules/statement/tests/mocks/statement-repository.mock.ts
+++ back-end/modules/statement/tests/mocks/statement-repository.mock.ts
@@ -3,6 +3,10 @@ import { StatementEntity } from '@/statement/statement.entity';
 export class StatementRepositoryMock {
   findOneBy = jest.fn();
 
+  remove = jest.fn((statement: StatementEntity): StatementEntity => {
+    return statement;
+  });
+
   save = jest.fn((statement: StatementEntity): StatementEntity => {
     return statement;
   });
