c172a2dfdb1a2402f8b53eef68a2c6f67d481cd4
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-23 13:36:52 -0500

    Expand system definition

--- front-end/modules/statement/types/statement.ts
+++ front-end/modules/statement/types/statement.ts
@@ -2,6 +2,10 @@ export type Statement = {
   id: string;
   title: string;
   description: string;
+  distinctVariableRestrictions: [string, string][];
+  variableTypeHypotheses: [string, string][];
+  logicalHypotheses: string[][];
+  assertion: string[];
   systemId: string;
   createdByUserId: string;
 };
