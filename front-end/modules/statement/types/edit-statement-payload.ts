20ece31f3b047eb3689650b8b8daf240500d22ec
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-24 05:55:43 -0500

    Expand the edit statement payload type

--- front-end/modules/statement/types/edit-statement-payload.ts
+++ front-end/modules/statement/types/edit-statement-payload.ts
@@ -2,5 +2,9 @@ export type EditStatementPayload = {
   id: string;
   newTitle: string;
   newDescription: string;
+  newDistinctVariableRestrictions: [string, string][];
+  newVariableTypeHypotheses: [string, string][];
+  newLogicalHypotheses: string[][];
+  newAssertion: string[];
   systemId: string;
 };
