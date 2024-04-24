b33155874b9994c5b0ee114ee683aeeda333c87b
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-24 05:41:34 -0500

    Expand new statement payload

--- front-end/modules/statement/types/new-statement-payload.ts
+++ front-end/modules/statement/types/new-statement-payload.ts
@@ -1,5 +1,9 @@
 export type NewStatementPayload = {
   title: string;
   description: string;
+  distinctVariableRestrictions: [string, string][];
+  variableTypeHypotheses: [string, string][];
+  logicalHypotheses: string[][];
+  assertion: string[];
   systemId: string;
 };
