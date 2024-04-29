39d817cf545237556c68aea571975dd91db790c9
Antonio Sanchez <asanchez16756@gmail.com>
2024-04-29 04:44:31 -0500

    Add respective end points for new back end end points

---
+++ front-end/modules/symbol/end-points/fetch-constant-symbols.ts
@@ -0,0 +1,15 @@
+import { Tags } from '@/app/constants/tags';
+import { TagTypes } from '@/app/types/tag-types';
+import { Symbol } from '@/symbol/types/symbol';
+import { BaseQueryFn, EndpointBuilder, QueryDefinition } from '@reduxjs/toolkit/query';
+
+export const fetchConstantSymbols = (builder: EndpointBuilder<BaseQueryFn, TagTypes, 'api'>): QueryDefinition<string, BaseQueryFn, TagTypes, Symbol[], 'api'> => {
+  return builder.query<Symbol[], string>({
+    providesTags: [
+      Tags.Symbol
+    ],
+    query: (systemId: string): string => {
+      return `/system/${systemId}/symbol/constant`;
+    }
+  });
+};
