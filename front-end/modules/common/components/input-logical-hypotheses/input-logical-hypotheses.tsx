e491156484b951ccda3af075467abff6abb36fc5
Antonio Sanchez <asanchez16756@gmail.com>
2024-05-01 06:43:18 -0500

    Initial commit for logical hypotheses input

---
+++ front-end/modules/common/components/input-logical-hypotheses/input-logical-hypotheses.tsx
@@ -0,0 +1,10 @@
+import { InputProps } from '@/common/types/input-props';
+import { Fragment, ReactElement } from 'react';
+
+export const InputLogicalHypotheses = (props: InputProps): ReactElement => {
+  const {} = props;
+
+  return (
+    <Fragment />
+  );
+};
