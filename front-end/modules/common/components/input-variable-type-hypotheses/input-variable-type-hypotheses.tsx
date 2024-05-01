bcc52788231ba7ae9847efccd98b643bc326e71a
Antonio Sanchez <asanchez16756@gmail.com>
2024-05-01 12:55:28 -0500

    Add type requirement

--- front-end/modules/common/components/input-variable-type-hypotheses/input-variable-type-hypotheses.tsx
+++ front-end/modules/common/components/input-variable-type-hypotheses/input-variable-type-hypotheses.tsx
@@ -2,14 +2,17 @@
 
 import { api } from '@/app/api';
 import { RouteParams } from '@/app/types/route-params';
+import { AntdFormErrorList } from '@/common/components/antd-form-error-list/antd-form-error-list';
 import { RenderMath } from '@/common/components/render-math/render-math';
 import { InputProps } from '@/common/types/input-props';
 import { Symbol } from '@/symbol/types/symbol';
 import { MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import { Button, Flex, Form, FormListFieldData, FormListOperation, Select } from 'antd';
+import { RuleObject } from 'antd/es/form';
 import useFormInstance from 'antd/es/form/hooks/useFormInstance';
 import { DefaultOptionType } from 'antd/es/select';
 import { useParams } from 'next/navigation';
+import { ValidatorRule } from 'rc-field-form/lib/interface';
 import { Fragment, ReactElement, ReactNode } from 'react';
 
 const { useFetchConstantSymbolsQuery, useFetchVariableSymbolsQuery } = api;
@@ -40,11 +43,56 @@ export const InputVariableTypeHypotheses = (props: InputProps): ReactElement => 
     };
   });
 
+  const rules = [
+    {
+      message: 'All variables in the assertion or any logical hypothesis must be typed.',
+      validator: (currentRule: RuleObject, variableTypeHypotheses: ([string, string] | [undefined, string] | [string] | [])[] = []): Promise<any | void> => {
+        const { message } = currentRule;
+
+        const assertion = (formInstance.getFieldValue('assertion') ?? formInstance.getFieldValue('newAssertion') ?? []) as string[];
+        const logicalHypotheses = (formInstance.getFieldValue('logicalHypotheses') ?? formInstance.getFieldValue('newLogicalHypotheses') ?? []) as string[][];
+
+        const variables = (variableSymbols ?? []).reduce((dictionary: Record<string, boolean>, symbol: Symbol): Record<string, boolean> => {
+          const { id } = symbol;
+
+          dictionary[id] = true;
+
+          return dictionary;
+        }, {});
+
+        const typedVariables = variableTypeHypotheses.reduce((dictionary: Record<string, boolean>, variableTypeHypothesis: [string, string] | [undefined, string] | [string] | []): Record<string, boolean> => {
+          if (2 !== variableTypeHypothesis.length) {
+            return dictionary;
+          }
+
+          if ('string' !== typeof variableTypeHypothesis[0]) {
+            return dictionary;
+          }
+
+          dictionary[variableTypeHypothesis[1]] = true;
+
+          return dictionary;
+        }, {});
+
+        const symbolIds = assertion.concat(...logicalHypotheses);
+
+        for (const symbolId of symbolIds) {
+          if (variables[symbolId] && !typedVariables[symbolId]) {
+            return Promise.reject(message);
+          }
+        }
+
+        return Promise.resolve();
+      }
+    }
+  ] as ValidatorRule[];
+
   return (
     <Item label={<Fragment>Variable Type<br />Hypotheses</Fragment>} name={name}>
-      <List name={name}>
-        {(fields: FormListFieldData[], operation: FormListOperation): ReactNode => {
+      <List name={name} rules={rules}>
+        {(fields: FormListFieldData[], operation: FormListOperation, meta: { errors: ReactNode[]; warnings: ReactNode[]; }): ReactNode => {
           const { add, remove } = operation;
+          const { errors } = meta;
 
           const addHandler = (): void => {
             add([]);
@@ -111,6 +159,7 @@ export const InputVariableTypeHypotheses = (props: InputProps): ReactElement => 
                 <Button block disabled={variableSymbols?.length === typeHypotheses?.length} icon={<PlusCircleOutlined />} type='dashed' onClick={addHandler}>
                   Add Variable Type Hypothesis
                 </Button>
+                <AntdFormErrorList errors={errors} />
               </Item>
             </Fragment>
           );
