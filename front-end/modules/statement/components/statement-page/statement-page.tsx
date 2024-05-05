e2caa57c8e2fd9cae9ecc18b0215f2dd30e79485
Antonio Sanchez <asanchez16756@gmail.com>
2024-05-05 14:48:43 -0500

    Display metrics and proof space

--- front-end/modules/statement/components/statement-page/statement-page.tsx
+++ front-end/modules/statement/components/statement-page/statement-page.tsx
@@ -1,5 +1,10 @@
 import { ServerSideProps } from '@/app/types/server-side-props';
 import { AntdCard } from '@/common/components/antd-card/antd-card';
+import { AntdCol } from '@/common/components/antd-col/antd-col';
+import { AntdDescriptionsItem } from '@/common/components/antd-descriptions-item/antd-descriptions-item';
+import { AntdDescriptions } from '@/common/components/antd-descriptions/antd-descriptions';
+import { AntdEmpty } from '@/common/components/antd-empty/antd-empty';
+import { AntdRow } from '@/common/components/antd-row/antd-row';
 import { fetchStatement } from '@/statement/fetch-data/fetch-statement';
 import { ReactElement } from 'react';
 
@@ -8,10 +13,27 @@ export const StatementPage = async (props: ServerSideProps): Promise<ReactElemen
 
   const { 'system-id': systemId = '', 'statement-id': statementId = '' } = params;
 
-  const {} = await fetchStatement(systemId, statementId);
+  const { proofAppearances, proofSteps } = await fetchStatement(systemId, statementId);
 
   return (
-    <AntdCard title='Proof'>
-    </AntdCard>
+    <AntdRow gutter={[0, 16]}>
+      <AntdCol span={24}>
+        <AntdCard title='Metrics'>
+          <AntdDescriptions bordered>
+            <AntdDescriptionsItem label='Proof Appearances Count'>
+              {proofAppearances}
+            </AntdDescriptionsItem>
+            <AntdDescriptionsItem label='Proof Steps'>
+              {proofSteps}
+            </AntdDescriptionsItem>
+          </AntdDescriptions>
+        </AntdCard>
+      </AntdCol>
+      <AntdCol span={24}>
+        <AntdCard title='Proof'>
+          <AntdEmpty description='Proof does not exist.' />
+        </AntdCard>
+      </AntdCol>
+    </AntdRow>
   );
 };
