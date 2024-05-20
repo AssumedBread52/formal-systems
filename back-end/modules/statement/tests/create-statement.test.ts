f8e65c82064510f77fd7efdf0f9e4c4e2edd4be8
Antonio Sanchez <asanchez16756@gmail.com>
2024-05-20 17:48:36 -0500

    Converted error response

--- back-end/modules/statement/tests/create-statement.test.ts
+++ back-end/modules/statement/tests/create-statement.test.ts
@@ -396,10 +396,10 @@ describe('Create Statement', (): void => {
       ]
     });
 
-    expectCorrectResponse(response, HttpStatus.UNPROCESSABLE_ENTITY, {
-      error: 'Unprocessable Entity',
-      message: 'All symbols must exist within the formal system.',
-      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    expectCorrectResponse(response, HttpStatus.NOT_FOUND, {
+      error: 'Not Found',
+      message: 'Symbol not found.',
+      statusCode: HttpStatus.NOT_FOUND
     });
   });
 
