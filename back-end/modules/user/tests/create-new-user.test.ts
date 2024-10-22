4f1d74be70591b187f8b4c35a1a702e85ec012d5
Antonio Sanchez <asanchez16756@gmail.com>
2024-10-22 13:46:49 -0500

    Fix success test case

--- back-end/modules/user/tests/create-new-user.test.ts
+++ back-end/modules/user/tests/create-new-user.test.ts
@@ -1,15 +1,19 @@
 import { createTestApp } from '@/app/tests/helpers/create-test-app';
 import { getOrThrowMock } from '@/app/tests/mocks/get-or-throw.mock';
-import { expectAuthCookies } from '@/auth/tests/helpers/expect-auth-cookies';
 import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
-import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
+import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
+import { saveMock } from '@/common/tests/mocks/save.mock';
 import { UserEntity } from '@/user/user.entity';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
+import { UserRepositoryMock } from './mocks/user-repository.mock';
 
 describe('Create New User', (): void => {
+  const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
+  const save = saveMock();
   let app: INestApplication;
 
   beforeAll(async (): Promise<void> => {
@@ -57,20 +61,68 @@ describe('Create New User', (): void => {
   });
 
   it('succeeds', async (): Promise<void> => {
-    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+    const userId = new ObjectId();
+    const firstName = 'Test';
+    const lastName = 'User';
+    const email = 'test@example.com';
+    const password = '123456';
+    const user = new UserEntity();
 
-    userRepositoryMock.findOneBy.mockReturnValueOnce(null);
-    getOrThrow.mockReturnValueOnce('1000');
+    user._id = userId;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+
+    findOneBy.mockResolvedValueOnce(null);
+    save.mockResolvedValueOnce(user);
 
     const response = await request(app.getHttpServer()).post('/user').send({
-      firstName: 'Test',
-      lastName: 'User',
-      email: 'test@example.com',
-      password: '123456'
+      firstName,
+      lastName,
+      email,
+      password
     });
 
-    expectCorrectResponse(response, HttpStatus.CREATED, {});
-    expectAuthCookies(response);
+    const { statusCode, body } = response;
+    const cookies = response.get('Set-Cookie');
+
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      email
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(1);
+    expect(getOrThrow).toHaveBeenNthCalledWith(1, 'AUTH_COOKIE_MAX_AGE_MILLISECONDS');
+    expect(save).toHaveBeenCalledTimes(1);
+    expect(save).toHaveBeenNthCalledWith(1, {
+      _id: expect.objectContaining(/[0-9a-f]{24}/),
+      firstName,
+      lastName,
+      email,
+      hashedPassword: expect.stringMatching(/\$2a\$12\$.+/),
+      systemCount: 0,
+      constantSymbolCount: 0,
+      variableSymbolCount: 0,
+      axiomCount: 0,
+      theoremCount: 0,
+      deductionCount: 0
+    });
+    expect(statusCode).toBe(HttpStatus.CREATED);
+    expect(body).toEqual({
+      id: userId.toString(),
+      firstName,
+      lastName,
+      email,
+      systemCount: 0,
+      constantSymbolCount: 0,
+      variableSymbolCount: 0,
+      axiomCount: 0,
+      theoremCount: 0,
+      deductionCount: 0
+    });
+    expect(cookies).toBeDefined();
+    expect(cookies).toHaveLength(2);
+    expect(cookies![0]).toMatch(/^token=.+; Max-Age=1; .+; HttpOnly; Secure$/);
+    expect(cookies![1]).toMatch(/^authStatus=true; Max-Age=1; .+; Secure$/);
   });
 
   afterAll(async (): Promise<void> => {
