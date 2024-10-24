1f9d11bdad8dc7086fea38a2583cecee3da5f033
Antonio Sanchez <asanchez16756@gmail.com>
2024-10-24 07:48:57 -0500

    Establish base validation class which enforces conflict check method

---
+++ back-end/modules/common/services/base-validation.service.ts
@@ -0,0 +1,32 @@
+import { InvalidObjectIdException } from '@/common/exceptions/invalid-object-id.exception';
+import { Injectable, ValidationPipe } from '@nestjs/common';
+import { ClassConstructor, plainToClass } from 'class-transformer';
+import { isMongoId, validateSync } from 'class-validator';
+import { ObjectId } from 'mongodb';
+
+@Injectable()
+export abstract class BaseValidationService {
+  abstract conflictCheck(...params: any[]): Promise<void>;
+
+  idCheck(id: any): ObjectId {
+    if (!isMongoId(id)) {
+      throw new InvalidObjectIdException();
+    }
+
+    return new ObjectId(id);
+  }
+
+  payloadCheck<Payload extends object>(payload: any, payloadConstructor: ClassConstructor<Payload>): Payload {
+    const newPayload = plainToClass(payloadConstructor, payload);
+
+    const errors = validateSync(newPayload);
+
+    if (0 < errors.length) {
+      const validationPipe = new ValidationPipe();
+
+      throw validationPipe.createExceptionFactory()(errors);
+    }
+
+    return newPayload;
+  }
+};
--- back-end/modules/user/services/validate.service.ts
+++ back-end/modules/user/services/validate.service.ts
@@ -1,16 +1,14 @@
-import { InvalidObjectIdException } from '@/common/exceptions/invalid-object-id.exception';
+import { BaseValidationService } from '@/common/services/base-validation.service';
 import { UserUniqueEmailAddressException } from '@/user/exceptions/user-unique-email-address.exception';
 import { UserEntity } from '@/user/user.entity';
-import { Injectable, ValidationPipe } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ClassConstructor, plainToClass } from 'class-transformer';
-import { isMongoId, validateSync } from 'class-validator';
-import { ObjectId } from 'mongodb';
 import { MongoRepository } from 'typeorm';
 
 @Injectable()
-export class ValidateService {
+export class ValidateService extends BaseValidationService {
   constructor(@InjectRepository(UserEntity) private userRepository: MongoRepository<UserEntity>) {
+    super();
   }
 
   async conflictCheck(email: string): Promise<void> {
@@ -21,27 +19,5 @@ export class ValidateService {
     if (collision) {
       throw new UserUniqueEmailAddressException();
     }
-  }
-
-  idCheck(id: any): ObjectId {
-    if (!isMongoId(id)) {
-      throw new InvalidObjectIdException();
-    }
-
-    return new ObjectId(id);
-  }
-
-  payloadCheck<Payload extends object>(payload: any, payloadConstructor: ClassConstructor<Payload>): Payload {
-    const newPayload = plainToClass(payloadConstructor, payload);
-
-    const errors = validateSync(newPayload);
-
-    if (0 < errors.length) {
-      const validationPipe = new ValidationPipe();
-
-      throw validationPipe.createExceptionFactory()(errors);
-    }
-
-    return newPayload;
   }
 };
