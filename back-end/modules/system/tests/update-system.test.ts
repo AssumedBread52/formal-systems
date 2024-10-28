b9d53afa161d11e2f0292f29c2537d6587adafe3
Antonio Sanchez <asanchez16756@gmail.com>
2024-10-28 10:06:56 -0500

    Fixed success test case

--- back-end/modules/system/tests/update-system.test.ts
+++ back-end/modules/system/tests/update-system.test.ts
@@ -5,17 +5,22 @@ import { testExpiredToken } from '@/auth/tests/helpers/test-expired-token';
 import { testInvalidToken } from '@/auth/tests/helpers/test-invalid-token';
 import { testMissingToken } from '@/auth/tests/helpers/test-missing-token';
 import { expectCorrectResponse } from '@/common/tests/helpers/expect-correct-response';
+import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
+import { saveMock } from '@/common/tests/mocks/save.mock';
 import { SystemEntity } from '@/system/system.entity';
 import { UserRepositoryMock } from '@/user/tests/mocks/user-repository.mock';
 import { UserEntity } from '@/user/user.entity';
 import { HttpStatus, INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 import { SystemRepositoryMock } from './mocks/system-repository.mock';
 
 describe('Update System', (): void => {
-  getOrThrowMock();
+  const findOneBy = findOneByMock();
+  const getOrThrow = getOrThrowMock();
+  const save = saveMock();
   let app: INestApplication;
 
   beforeAll(async (): Promise<void> => {
@@ -163,29 +168,80 @@ describe('Update System', (): void => {
   });
 
   it('succeeds', async (): Promise<void> => {
-    const system = new SystemEntity();
+    const systemId = new ObjectId();
+    const newTitle = 'New Test System';
+    const newDescription = 'This is a new test.';
+    const constantSymbolCount = 6;
+    const variableSymbolCount = 3;
+    const axiomCount = 6;
+    const theoremCount = 1;
+    const deductionCount = 1;
+    const createdByUserId = new ObjectId();
     const user = new UserEntity();
+    const system = new SystemEntity();
+    const updatedSystem = new SystemEntity();
 
-    system.createdByUserId = user._id;
+    user._id = createdByUserId;
+    system._id = systemId;
+    system.constantSymbolCount = constantSymbolCount;
+    system.variableSymbolCount = variableSymbolCount;
+    system.axiomCount = axiomCount;
+    system.theoremCount = theoremCount;
+    system.deductionCount = deductionCount;
+    system.createdByUserId = createdByUserId;
+    updatedSystem._id = systemId;
+    updatedSystem.title = newTitle;
+    updatedSystem.description = newDescription;
+    updatedSystem.constantSymbolCount = constantSymbolCount;
+    updatedSystem.variableSymbolCount = variableSymbolCount;
+    updatedSystem.axiomCount = axiomCount;
+    updatedSystem.theoremCount = theoremCount;
+    updatedSystem.deductionCount = deductionCount;
+    updatedSystem.createdByUserId = createdByUserId;
 
-    const systemRepositoryMock = app.get(getRepositoryToken(SystemEntity)) as SystemRepositoryMock;
-    const userRepositoryMock = app.get(getRepositoryToken(UserEntity)) as UserRepositoryMock;
+    findOneBy.mockResolvedValueOnce(user);
+    findOneBy.mockResolvedValueOnce(system);
+    findOneBy.mockResolvedValueOnce(null);
+    save.mockResolvedValueOnce(updatedSystem);
 
-    systemRepositoryMock.findOneBy.mockReturnValueOnce(system);
-    systemRepositoryMock.findOneBy.mockReturnValueOnce(null);
-    userRepositoryMock.findOneBy.mockReturnValueOnce(user);
-
-    const token = app.get(TokenService).generateToken(user._id);
-
-    const response = await request(app.getHttpServer()).patch(`/system/${system._id}`).set('Cookie', [
-      `token=${token}`
-    ]).send({
-      newTitle: 'New Title',
-      newDescription: 'This is a new test.'
+    const token = app.get(JwtService).sign({
+      id: createdByUserId
     });
 
-    expectCorrectResponse(response, HttpStatus.OK, {
-      id: system._id.toString()
+    const response = await request(app.getHttpServer()).patch(`/system/${systemId}`).set('Cookie', [
+      `token=${token}`
+    ]).send({
+      newTitle,
+      newDescription
+    });
+
+    const { statusCode, body } = response;
+
+    expect(findOneBy).toHaveBeenCalledTimes(3);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: createdByUserId
+    });
+    expect(findOneBy).toHaveBeenNthCalledWith(2, {
+      _id: systemId
+    });
+    expect(findOneBy).toHaveBeenNthCalledWith(3, {
+      title: newTitle,
+      createdByUserId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(save).toHaveBeenCalledTimes(1);
+    expect(save).toHaveBeenNthCalledWith(1, updatedSystem);
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toEqual({
+      id: systemId.toString(),
+      title: newTitle,
+      description: newDescription,
+      constantSymbolCount,
+      variableSymbolCount,
+      axiomCount,
+      theoremCount,
+      deductionCount,
+      createdByUserId: createdByUserId.toString()
     });
   });
 
