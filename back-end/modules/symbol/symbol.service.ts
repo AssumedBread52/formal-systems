d6c2a545f02e2ba0a1b18447c55c720566540c9c
Antonio Sanchez <asanchez16756@gmail.com>
2024-11-04 13:56:19 -0600

    Cleaning up symbol service

--- back-end/modules/symbol/symbol.service.ts
+++ back-end/modules/symbol/symbol.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectId } from 'mongodb';
-import { MongoRepository, RootFilterOperators } from 'typeorm';
+import { MongoRepository } from 'typeorm';
 import { SymbolType } from './enums/symbol-type.enum';
 import { SymbolEntity } from './symbol.entity';
 
 @Injectable()
 export class SymbolService {
   constructor(@InjectRepository(SymbolEntity) private symbolRepository: MongoRepository<SymbolEntity>) {
-  }
-
-  readById(systemId: ObjectId, symbolId: ObjectId): Promise<SymbolEntity | null> {
-    return this.symbolRepository.findOneBy({
-      _id: symbolId,
-      systemId
-    });
   }
 
   readByIds(systemId: ObjectId, symbolIds: ObjectId[]): Promise<SymbolEntity[]> {
@@ -30,31 +23,6 @@ export class SymbolService {
     return this.symbolRepository.find({
       type,
       systemId
-    });
-  }
-
-  readSymbols(page: number, count: number, keywords: string[], types: SymbolType[], systemId: ObjectId): Promise<[SymbolEntity[], number]> {
-    const where = {
-      systemId
-    } as RootFilterOperators<SymbolEntity>;
-
-    if (0 !== keywords.length) {
-      where.$text = {
-        $caseSensitive: false,
-        $search: keywords.join(',')
-      };
-    }
-
-    if (0 !== types.length) {
-      where.type = {
-        $in: types
-      };
-    }
-
-    return this.symbolRepository.findAndCount({
-      skip: (page - 1) * count,
-      take: count,
-      where
     });
   }
 };
