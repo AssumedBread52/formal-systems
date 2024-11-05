461ea21973a9a98641ecb4a1cf203b622cfa57bf
Antonio Sanchez <asanchez16756@gmail.com>
2024-11-05 14:11:27 -0600

    Fix system counting of symbols

--- back-end/modules/system/subscribers/system-symbol-count.subscriber.ts
+++ back-end/modules/system/subscribers/system-symbol-count.subscriber.ts
@@ -2,7 +2,7 @@ import { SymbolType } from '@/symbol/enums/symbol-type.enum';
 import { SymbolEntity } from '@/symbol/symbol.entity';
 import { SystemNotFoundException } from '@/system/exceptions/system-not-found.exception';
 import { SystemEntity } from '@/system/system.entity';
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent, UpdateEvent } from 'typeorm';
+import { DataSource, EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent, UpdateEvent } from 'typeorm';
 
 @EventSubscriber()
 export class SystemSymbolCountSubscriber implements EntitySubscriberInterface<SymbolEntity> {
@@ -10,73 +10,38 @@ export class SystemSymbolCountSubscriber implements EntitySubscriberInterface<Sy
     return SymbolEntity;
   }
 
-  async beforeInsert(event: InsertEvent<SymbolEntity>): Promise<void> {
+  async afterInsert(event: InsertEvent<SymbolEntity>): Promise<void> {
     const { connection, entity } = event;
 
-    const { type, systemId } = entity;
-
-    const systemRepository = connection.getMongoRepository(SystemEntity);
-
-    const system = await systemRepository.findOneBy({
-      _id: systemId
-    });
-
-    if (!system) {
-      throw new SystemNotFoundException();
-    }
-
-    switch (type) {
-      case SymbolType.Constant:
-        system.constantSymbolCount++;
-        break;
-      case SymbolType.Variable:
-        system.variableSymbolCount++;
-        break;
-    }
-
-    await systemRepository.save(system);
+    this.adjustSymbolCounts(connection, entity);
   }
 
-  async beforeRemove(event: RemoveEvent<SymbolEntity>): Promise<void> {
+  async afterRemove(event: RemoveEvent<SymbolEntity>): Promise<void> {
     const { connection, databaseEntity } = event;
 
-    const { type, systemId } = databaseEntity;
-
-    const systemRepository = connection.getMongoRepository(SystemEntity);
-
-    const system = await systemRepository.findOneBy({
-      _id: systemId
-    });
-
-    if (!system) {
-      throw new SystemNotFoundException();
-    }
-
-    switch (type) {
-      case SymbolType.Constant:
-        system.constantSymbolCount--;
-        break;
-      case SymbolType.Variable:
-        system.variableSymbolCount--;
-        break;
-    }
-
-    await systemRepository.save(system);
+    this.adjustSymbolCounts(connection, databaseEntity);
   }
 
-  async beforeUpdate(event: UpdateEvent<SymbolEntity>): Promise<void> {
+  async afterUpdate(event: UpdateEvent<SymbolEntity>): Promise<void> {
     const { connection, databaseEntity, entity } = event;
 
     if (!entity) {
       return;
     }
 
-    const { type, systemId } = databaseEntity;
+    const { type } = databaseEntity;
 
     if (type === entity.type) {
       return;
     }
 
+    this.adjustSymbolCounts(connection, databaseEntity);
+  }
+
+  private async adjustSymbolCounts(connection: DataSource, symbol: SymbolEntity): Promise<void> {
+    const { systemId } = symbol;
+
+    const symbolRepository = connection.getMongoRepository(SymbolEntity);
     const systemRepository = connection.getMongoRepository(SystemEntity);
 
     const system = await systemRepository.findOneBy({
@@ -87,17 +52,15 @@ export class SystemSymbolCountSubscriber implements EntitySubscriberInterface<Sy
       throw new SystemNotFoundException();
     }
 
-    switch (type) {
-      case SymbolType.Constant:
-        system.constantSymbolCount--;
-        system.variableSymbolCount++;
-        break;
-      case SymbolType.Variable:
-        system.constantSymbolCount++;
-        system.variableSymbolCount--;
-        break;
-    }
+    system.constantSymbolCount = await symbolRepository.count({
+      type: SymbolType.Constant,
+      systemId
+    });
+    system.variableSymbolCount = await symbolRepository.count({
+      type: SymbolType.Variable,
+      systemId
+    });
 
-    await systemRepository.save(system);
+    systemRepository.save(system);
   }
 };
