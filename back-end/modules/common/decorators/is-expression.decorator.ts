69b45ba4469d514716cedd0aa07dae75206ebe08
Antonio Sanchez <asanchez16756@gmail.com>
2024-11-11 06:42:32 -0600

    Clean up decorator

--- back-end/modules/common/decorators/is-expression.decorator.ts
+++ back-end/modules/common/decorators/is-expression.decorator.ts
@@ -1,25 +1,25 @@
-import { ValidateBy, ValidationOptions, buildMessage, isArray, isMongoId } from 'class-validator';
+import { buildMessage, isArray, isMongoId, ValidateBy, ValidationOptions } from 'class-validator';
 
 export const IsExpressionDecorator = (validationOptions?: ValidationOptions): PropertyDecorator => {
   return ValidateBy({
     name: 'is-expression',
     validator: {
+      defaultMessage: buildMessage((eachPrefix: string): string => {
+        return `${eachPrefix}each value in $property must be a mongodb id`;
+      }, validationOptions),
       validate: (value: any): boolean => {
         if (!isArray(value)) {
           return false;
         }
 
         for (let item of value) {
-          if (!isMongoId(`${item}`)) {
+          if (!isMongoId(item)) {
             return false;
           }
         }
 
         return true;
-      },
-      defaultMessage: buildMessage((eachPrefix: string): string => {
-        return `${eachPrefix}each value in $property must be a mongodb id`;
-      }, validationOptions)
+      }
     }
   }, validationOptions);
 };
