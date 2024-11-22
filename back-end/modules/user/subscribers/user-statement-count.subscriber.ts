39365b4e4039313100ba55861133491fd6c75b62
Antonio Sanchez <asanchez16756@gmail.com>
2024-11-22 05:21:10 -0600

    Clean up user statement counting

--- back-end/modules/user/subscribers/user-statement-count.subscriber.ts
+++ back-end/modules/user/subscribers/user-statement-count.subscriber.ts
@@ -1,7 +1,7 @@
 import { StatementEntity } from '@/statement/statement.entity';
 import { UserNotFoundException } from '@/user/exceptions/user-not-found.exception';
 import { UserEntity } from '@/user/user.entity';
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent } from 'typeorm';
+import { DataSource, EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent } from 'typeorm';
 
 @EventSubscriber()
 export class UserStatementCountSubscriber implements EntitySubscriberInterface<StatementEntity> {
@@ -12,28 +12,19 @@ export class UserStatementCountSubscriber implements EntitySubscriberInterface<S
   async afterInsert(event: InsertEvent<StatementEntity>): Promise<void> {
     const { connection, entity } = event;
 
-    const { createdByUserId } = entity;
-
-    const userRepository = connection.getMongoRepository(UserEntity);
-
-    const user = await userRepository.findOneBy({
-      _id: createdByUserId
-    });
-
-    if (!user) {
-      throw new UserNotFoundException();
-    }
-
-    user.axiomCount++;
-
-    userRepository.save(user);
+    this.adjustStatementCounts(connection, entity);
   }
 
   async afterRemove(event: RemoveEvent<StatementEntity>): Promise<void> {
     const { connection, databaseEntity } = event;
 
-    const { createdByUserId } = databaseEntity;
+    this.adjustStatementCounts(connection, databaseEntity);
+  }
 
+  private async adjustStatementCounts(connection: DataSource, statement: StatementEntity): Promise<void> {
+    const { createdByUserId } = statement;
+
+    const statementRepository = connection.getMongoRepository(StatementEntity);
     const userRepository = connection.getMongoRepository(UserEntity);
 
     const user = await userRepository.findOneBy({
@@ -44,7 +35,9 @@ export class UserStatementCountSubscriber implements EntitySubscriberInterface<S
       throw new UserNotFoundException();
     }
 
-    user.axiomCount--;
+    user.axiomCount = await statementRepository.count({
+      createdByUserId
+    });
 
     userRepository.save(user);
   }
