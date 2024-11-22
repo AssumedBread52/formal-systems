09f4dd9d8fc43a89a1d9a852514b8d3bb35c7214
Antonio Sanchez <asanchez16756@gmail.com>
2024-11-22 05:34:42 -0600

    Fix statement counting

--- back-end/modules/system/subscribers/system-statement-count.subscriber.ts
+++ back-end/modules/system/subscribers/system-statement-count.subscriber.ts
@@ -1,19 +1,30 @@
 import { StatementEntity } from '@/statement/statement.entity';
 import { SystemNotFoundException } from '@/system/exceptions/system-not-found.exception';
 import { SystemEntity } from '@/system/system.entity';
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent } from 'typeorm';
+import { DataSource, EntitySubscriberInterface, EventSubscriber, InsertEvent, RemoveEvent } from 'typeorm';
 
 @EventSubscriber()
 export class SystemStatementCountSubscriber implements EntitySubscriberInterface<StatementEntity> {
-  listenTo(): string | Function {
+  listenTo(): Function | string {
     return StatementEntity;
   }
 
   async afterInsert(event: InsertEvent<StatementEntity>): Promise<void> {
     const { connection, entity } = event;
 
-    const { systemId } = entity;
+    this.adjustStatementCounts(connection, entity);
+  }
 
+  async afterRemove(event: RemoveEvent<StatementEntity>): Promise<void> {
+    const { connection, databaseEntity } = event;
+
+    this.adjustStatementCounts(connection, databaseEntity);
+  }
+
+  private async adjustStatementCounts(connection: DataSource, statement: StatementEntity): Promise<void> {
+    const { systemId } = statement;
+
+    const statementRepository = connection.getMongoRepository(StatementEntity);
     const systemRepository = connection.getMongoRepository(SystemEntity);
 
     const system = await systemRepository.findOneBy({
@@ -24,27 +35,9 @@ export class SystemStatementCountSubscriber implements EntitySubscriberInterface
       throw new SystemNotFoundException();
     }
 
-    system.axiomCount++;
-
-    systemRepository.save(system);
-  }
-
-  async afterRemove(event: RemoveEvent<StatementEntity>): Promise<void> {
-    const { connection, databaseEntity } = event;
-
-    const { systemId } = databaseEntity;
-
-    const systemRepository = connection.getMongoRepository(SystemEntity);
-
-    const system = await systemRepository.findOneBy({
-      _id: systemId
+    system.axiomCount = await statementRepository.count({
+      systemId
     });
-
-    if (!system) {
-      return;
-    }
-
-    system.axiomCount--;
 
     systemRepository.save(system);
   }
