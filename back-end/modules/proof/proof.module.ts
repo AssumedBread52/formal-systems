4cc3c8389b75622934910a26aec7dfdccddc4d7d
Antonio Sanchez <asanchez16756@gmail.com>
2024-12-10 12:48:00 -0600

    Added services to the module

--- back-end/modules/proof/proof.module.ts
+++ back-end/modules/proof/proof.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProofController } from './proof.controller';
+import { ProofEntity } from './proof.entity';
+import { ProofReadService } from './services/proof-read.service';
+import { ValidateService } from './services/validate.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ProofEntity
+    ])
+  ],
   controllers: [
     ProofController
+  ],
+  providers: [
+    ProofReadService,
+    ValidateService
   ]
 })
 export class ProofModule {
