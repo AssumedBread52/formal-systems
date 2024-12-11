ad23b821bf706c3ba07682b85a8f50d666597c45
Antonio Sanchez <asanchez16756@gmail.com>
2024-12-10 18:04:25 -0600

    Add most of the create proof checks

--- back-end/modules/proof/services/proof-create.service.ts
+++ back-end/modules/proof/services/proof-create.service.ts
@@ -1,12 +1,39 @@
+import { OwnershipException } from '@/auth/exceptions/ownership.exception';
+import { NewProofPayload } from '@/proof/payloads/new-proof.payload';
 import { ProofEntity } from '@/proof/proof.entity';
+import { StatementReadService } from '@/statement/services/statement-read.service';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ObjectId } from 'mongodb';
+import { MongoRepository } from 'typeorm';
+import { ValidateService } from './validate.service';
 
 @Injectable()
 export class ProofCreateService {
-  async create(sessionUserId: ObjectId, systemId: any, statementId: any, payload: any): Promise<ProofEntity> {
-    console.log(sessionUserId, systemId, statementId, payload);
+  constructor(@InjectRepository(ProofEntity) private proofRepository: MongoRepository<ProofEntity>, private statementReadService: StatementReadService, private validateService: ValidateService) {
+  }
 
-    return new ProofEntity();
+  async create(sessionUserId: ObjectId, containingSystemId: any, statementId: any, payload: any): Promise<ProofEntity> {
+    const { _id, systemId, createdByUserId } = await this.statementReadService.readById(containingSystemId, statementId);
+
+    if (createdByUserId.toString() !== sessionUserId.toString()) {
+      throw new OwnershipException();
+    }
+
+    const newProofPayload = this.validateService.payloadCheck(payload, NewProofPayload);
+
+    const { title, description } = newProofPayload;
+
+    await this.validateService.conflictCheck(title, systemId, _id);
+
+    const proof = new ProofEntity();
+
+    proof.title = title;
+    proof.description = description;
+    proof.statementId = _id;
+    proof.systemId = systemId;
+    proof.createdByUserId = sessionUserId;
+
+    return this.proofRepository.save(proof);
   }
 };
