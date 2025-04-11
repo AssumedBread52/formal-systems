fa7fb1dd6f4c17330ca5e8ab9b39fe6e900221b9
Antonio Sanchez <asanchez16756@gmail.com>
2025-04-11 07:44:17 -0500

    Validate additional token payload fields

--- back-end/modules/auth/payloads/token.payload.ts
+++ back-end/modules/auth/payloads/token.payload.ts
@@ -1,6 +1,12 @@
-import { IsMongoId } from 'class-validator';
+import { IsInt, IsMongoId, IsPositive } from 'class-validator';
 
 export class TokenPayload {
   @IsMongoId()
   id: string = '';
+  @IsInt()
+  @IsPositive()
+  iat: number = 0;
+  @IsInt()
+  @IsPositive()
+  exp: number = 0;
 };
