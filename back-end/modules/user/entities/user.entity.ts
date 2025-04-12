a1670b7f80746401f3e8ba120cab69cd004c8375
Antonio Sanchez <asanchez16756@gmail.com>
2025-04-12 11:30:27 -0500

    Counts can be 0

--- back-end/modules/user/entities/user.entity.ts
+++ back-end/modules/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { AdapterType } from '@/user/enums/adapter-type.enum';
 import { Exclude } from 'class-transformer';
-import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsPositive, Matches } from 'class-validator';
+import { IsEmail, IsEnum, IsInt, IsNotEmpty, Matches, Min } from 'class-validator';
 
 export class UserEntity {
   @Exclude()
@@ -18,24 +18,24 @@ export class UserEntity {
   @Matches(/\$2b\$12\$.+/)
   hashedPassword: string = '';
   @IsInt()
-  @IsPositive()
+  @Min(0)
   systemCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   constantSymbolCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   variableSymbolCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   axiomCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   theoremCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   deductionCount: number = 0;
   @IsInt()
-  @IsPositive()
+  @Min(0)
   proofCount: number = 0;
 };
