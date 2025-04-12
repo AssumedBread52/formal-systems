71390cf6b52e08512564c5601a9304d0b5e836cf
Antonio Sanchez <asanchez16756@gmail.com>
2025-04-12 15:34:43 -0500

    Simplify to success test case only

--- back-end/modules/auth/tests/sign-out.test.ts
+++ back-end/modules/auth/tests/sign-out.test.ts
@@ -1,7 +1,7 @@
-import { createTestApp } from '@/app/tests/helpers/create-test-app';
-import { getOrThrowMock } from '@/app/tests/mocks/get-or-throw.mock';
+import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
-import { UserEntity } from '@/user/user.entity';
+import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
+import { MongoUserEntity } from '@/user/entities/mongo-user.entity';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ObjectId } from 'mongodb';
@@ -16,107 +16,16 @@ describe('Sign Out', (): void => {
     app = await createTestApp();
   });
 
-  it('fails without a token', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).post('/auth/sign-out');
-
-    const { statusCode, body } = response;
-    const cookies = response.get('Set-Cookie');
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      message: 'Unauthorized',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-    expect(cookies).toBeUndefined();
-  });
-
-  it('fails with an expired token', async (): Promise<void> => {
-    const token = app.get(JwtService).sign({});
-
-    await new Promise((resolve: (value: unknown) => void): void => {
-      setTimeout(resolve, 1000);
-    });
-
-    const response = await request(app.getHttpServer()).post('/auth/sign-out').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-    const cookies = response.get('Set-Cookie');
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      message: 'Unauthorized',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-    expect(cookies).toBeUndefined();
-  });
-
-  it('fails with an invalid token', async (): Promise<void> => {
-    const token = app.get(JwtService).sign({});
-
-    const response = await request(app.getHttpServer()).post('/auth/sign-out').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-    const cookies = response.get('Set-Cookie');
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      error: 'Unauthorized',
-      message: 'Invalid token.',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-    expect(cookies).toBeUndefined();
-  });
-
-  it('fails if the user ID in the token payload does not match a user', async (): Promise<void> => {
-    const userId = new ObjectId();
-
-    findOneBy.mockResolvedValueOnce(null);
-
-    const token = app.get(JwtService).sign({
-      id: userId
-    });
-
-    const response = await request(app.getHttpServer()).post('/auth/sign-out').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-    const cookies = response.get('Set-Cookie');
-
-    expect(findOneBy).toHaveBeenCalledTimes(1);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: userId
-    });
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      error: 'Unauthorized',
-      message: 'Invalid token.',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-    expect(cookies).toBeUndefined();
-  });
-
   it('succeeds', async (): Promise<void> => {
     const userId = new ObjectId();
-    const user = new UserEntity();
+    const user = new MongoUserEntity();
 
     user._id = userId;
 
     findOneBy.mockResolvedValueOnce(user);
 
     const token = app.get(JwtService).sign({
-      id: userId
+      userId
     });
 
     const response = await request(app.getHttpServer()).post('/auth/sign-out').set('Cookie', [
