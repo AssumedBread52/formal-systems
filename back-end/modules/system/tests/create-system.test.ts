793447a2fa710cc1e3ac6d83c1bb7c4ddc45a836
Antonio Sanchez <asanchez16756@gmail.com>
2025-05-02 09:43:00 -0500

    Fix create system test

--- back-end/modules/system/tests/create-system.test.ts
+++ back-end/modules/system/tests/create-system.test.ts
@@ -1,15 +1,17 @@
-import { createTestApp } from '@/app/tests/helpers/create-test-app';
-import { getOrThrowMock } from '@/app/tests/mocks/get-or-throw.mock';
+import { createTestApp } from '@/common/tests/helpers/create-test-app';
+import { existsByMock } from '@/common/tests/mocks/exists-by.mock';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
+import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
 import { saveMock } from '@/common/tests/mocks/save.mock';
-import { SystemEntity } from '@/system/system.entity';
-import { UserEntity } from '@/user/user.entity';
+import { MongoSystemEntity } from '@/system/entities/mongo-system.entity';
+import { MongoUserEntity } from '@/user/entities/mongo-user.entity';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 
 describe('Create System', (): void => {
+  const existsBy = existsByMock();
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
   const save = saveMock();
@@ -19,179 +21,13 @@ describe('Create System', (): void => {
     app = await createTestApp();
   });
 
-  it('fails without a token', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).post('/system');
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      message: 'Unauthorized',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-  });
-
-  it('fails with an expired token', async (): Promise<void> => {
-    const token = app.get(JwtService).sign({});
-
-    await new Promise((resolve: (value: unknown) => void): void => {
-      setTimeout(resolve, 1000);
-    });
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      message: 'Unauthorized',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-  });
-
-  it('fails with an invalid token', async (): Promise<void> => {
-    const token = app.get(JwtService).sign({});
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      error: 'Unauthorized',
-      message: 'Invalid token.',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-  });
-
-  it('fails if the user ID in the token payload does not match a user', async (): Promise<void> => {
-    const createdByUserId = new ObjectId();
-
-    findOneBy.mockResolvedValueOnce(null);
-
-    const token = app.get(JwtService).sign({
-      id: createdByUserId
-    });
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(1);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: createdByUserId
-    });
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNAUTHORIZED);
-    expect(body).toEqual({
-      error: 'Unauthorized',
-      message: 'Invalid token.',
-      statusCode: HttpStatus.UNAUTHORIZED
-    });
-  });
-
-  it('fails with an invalid payload', async (): Promise<void> => {
-    const createdByUserId = new ObjectId();
-    const user = new UserEntity();
-
-    user._id = createdByUserId;
-
-    findOneBy.mockResolvedValueOnce(user);
-
-    const token = app.get(JwtService).sign({
-      id: createdByUserId
-    });
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(1);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: createdByUserId
-    });
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.BAD_REQUEST);
-    expect(body).toEqual({
-      error: 'Bad Request',
-      message: [
-        'title should not be empty',
-        'description should not be empty'
-      ],
-      statusCode: HttpStatus.BAD_REQUEST
-    });
-  });
-
-  it('fails with a title collision', async (): Promise<void> => {
-    const title = 'Test System';
-    const createdByUserId = new ObjectId();
-    const user = new UserEntity();
-    const conflictSystem = new SystemEntity();
-
-    user._id = createdByUserId;
-    conflictSystem.title = title;
-    conflictSystem.createdByUserId = createdByUserId;
-
-    findOneBy.mockResolvedValueOnce(user);
-    findOneBy.mockResolvedValueOnce(conflictSystem);
-
-    const token = app.get(JwtService).sign({
-      id: createdByUserId
-    });
-
-    const response = await request(app.getHttpServer()).post('/system').set('Cookie', [
-      `token=${token}`
-    ]).send({
-      title,
-      description: 'This is a test.'
-    });
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(2);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: createdByUserId
-    });
-    expect(findOneBy).toHaveBeenNthCalledWith(2, {
-      title,
-      createdByUserId
-    });
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(save).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.CONFLICT);
-    expect(body).toEqual({
-      error: 'Conflict',
-      message: 'Systems created by the same user must have a unique title.',
-      statusCode: HttpStatus.CONFLICT
-    });
-  });
-
   it('succeeds', async (): Promise<void> => {
     const systemId = new ObjectId();
     const title = 'Test System';
     const description = 'This is a test.';
     const createdByUserId = new ObjectId();
-    const user = new UserEntity();
-    const system = new SystemEntity();
+    const user = new MongoUserEntity();
+    const system = new MongoSystemEntity();
 
     user._id = createdByUserId;
     system._id = systemId;
@@ -199,8 +35,8 @@ describe('Create System', (): void => {
     system.description = description;
     system.createdByUserId = createdByUserId;
 
+    existsBy.mockResolvedValueOnce(false);
     findOneBy.mockResolvedValueOnce(user);
-    findOneBy.mockResolvedValueOnce(null);
     save.mockResolvedValueOnce(system);
 
     const token = app.get(JwtService).sign({
@@ -216,13 +52,14 @@ describe('Create System', (): void => {
 
     const { statusCode, body } = response;
 
-    expect(findOneBy).toHaveBeenCalledTimes(2);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: createdByUserId
-    });
-    expect(findOneBy).toHaveBeenNthCalledWith(2, {
+    expect(existsBy).toHaveBeenCalledTimes(1);
+    expect(existsBy).toHaveBeenNthCalledWith(1, {
       title,
       createdByUserId
+    });
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: createdByUserId
     });
     expect(getOrThrow).toHaveBeenCalledTimes(0);
     expect(save).toHaveBeenCalledTimes(1);
@@ -235,6 +72,7 @@ describe('Create System', (): void => {
       axiomCount: 0,
       theoremCount: 0,
       deductionCount: 0,
+      proofCount: 0,
       createdByUserId
     });
     expect(statusCode).toBe(HttpStatus.CREATED);
@@ -247,6 +85,7 @@ describe('Create System', (): void => {
       axiomCount: 0,
       theoremCount: 0,
       deductionCount: 0,
+      proofCount: 0,
       createdByUserId: createdByUserId.toString()
     });
   });
