8b22e8a91f374d56f3cea9d2758192f64407ff33
Antonio Sanchez <asanchez16756@gmail.com>
2025-10-20 20:26:11 -0500

    Improve typing

--- back-end/modules/health/tests/mocks/ping-check.mock.ts
+++ back-end/modules/health/tests/mocks/ping-check.mock.ts
@@ -1,6 +1,6 @@
 import { HealthIndicatorResult, TypeOrmHealthIndicator, TypeOrmPingCheckSettings } from '@nestjs/terminus';
 
-export const pingCheckMock = (): jest.SpyInstance<Promise<HealthIndicatorResult<string>>, [key: string, options?: TypeOrmPingCheckSettings], any> => {
+export const pingCheckMock = (): jest.SpyInstance<Promise<HealthIndicatorResult<string>>, [key: string, options?: TypeOrmPingCheckSettings | undefined], any> => {
   const pingCheck = jest.spyOn(TypeOrmHealthIndicator.prototype, 'pingCheck');
 
   beforeEach((): void => {
