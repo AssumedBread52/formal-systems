9feb049848743e9ed48401b64ebba6ed43022bdc
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-13 04:59:11 -0600

    Added GraphQL input decorators

--- back-end/modules/user/payloads/new-user.payload.ts
+++ back-end/modules/user/payloads/new-user.payload.ts
@@ -1,12 +1,18 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
+@InputType()
 export class NewUserPayload {
+  @Field()
   @IsNotEmpty()
   public readonly firstName: string = '';
+  @Field()
   @IsNotEmpty()
   public readonly lastName: string = '';
+  @Field()
   @IsEmail()
   public readonly email: string = '';
+  @Field()
   @IsStrongPassword()
   public readonly password: string = '';
 };
