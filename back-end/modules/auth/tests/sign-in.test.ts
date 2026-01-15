92f2c3b20729ebdfa0a03949b21e03f25994593d
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-15 15:41:12 -0600

    Fix sign in and sign out to include GraphQL and for now only should cover successful case

--- back-end/modules/auth/tests/sign-in.test.ts
+++ back-end/modules/auth/tests/sign-in.test.ts
@@ -2,26 +2,29 @@ import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
 import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
 import { MongoUserEntity } from '@/user/entities/mongo-user.entity';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { hashSync } from 'bcryptjs';
 import * as request from 'supertest';
 
 describe('Sign In', (): void => {
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
-  let app: INestApplication;
+  let app: NestExpressApplication
 
   beforeAll(async (): Promise<void> => {
     app = await createTestApp();
   });
 
-  it('succeeds', async (): Promise<void> => {
-    const email = 'test@example.com';
-    const password = 'TestUser1!';
+  it('POST /auth/sign-in (configuration read: successful, user search: successful, payload: valid)', async (): Promise<void> => {
+    const email = 'test1.user1@example.com';
+    const password = 'Test1User1!';
     const user = new MongoUserEntity();
 
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
     user.email = email;
-    user.hashedPassword = hashSync(password, 12);
+    user.hashedPassword = hashSync(password);
 
     findOneBy.mockResolvedValueOnce(user);
     getOrThrow.mockReturnValueOnce(1000);
@@ -41,11 +44,66 @@ describe('Sign In', (): void => {
     expect(getOrThrow).toHaveBeenCalledTimes(1);
     expect(getOrThrow).toHaveBeenNthCalledWith(1, 'AUTH_COOKIE_MAX_AGE_MILLISECONDS');
     expect(statusCode).toBe(HttpStatus.NO_CONTENT);
-    expect(body).toEqual({});
+    expect(body).toStrictEqual({
+    });
     expect(cookies).toBeDefined();
     expect(cookies).toHaveLength(2);
-    expect(cookies![0]).toMatch(/^token=.+; Max-Age=1; .+; HttpOnly; Secure$/);
-    expect(cookies![1]).toMatch(/^authStatus=true; Max-Age=1; .+; Secure$/);
+    expect(cookies![0]).toMatch(/^token=.+; Max-Age=1; Path=\/; Expires=(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12]\d|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d) GMT; HttpOnly; Secure$/);
+    expect(cookies![1]).toMatch(/^authStatus=true; Max-Age=1; Path=\/; Expires=(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12]\d|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d) GMT; Secure$/);
+  });
+
+  it('POST /graphql mutation signIn (configuration read: successful, user search: successful, payload: valid)', async (): Promise<void> => {
+    const email = 'test1.user1@example.com';
+    const password = 'Test1User1!';
+    const user = new MongoUserEntity();
+
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
+    user.email = email;
+    user.hashedPassword = hashSync(password);
+
+    findOneBy.mockResolvedValueOnce(user);
+    getOrThrow.mockReturnValueOnce(1000);
+
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: 'mutation signIn($email: String!, $password: String!) { signIn(email: $email, password: $password) { id, firstName, lastName, email, systemCount, constantSymbolCount, variableSymbolCount, axiomCount, theoremCount, deductionCount, proofCount } }',
+      variables: {
+        email,
+        password
+      }
+    });
+
+    const { statusCode, body } = response;
+    const cookies = response.get('Set-Cookie');
+
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      email
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(1);
+    expect(getOrThrow).toHaveBeenNthCalledWith(1, 'AUTH_COOKIE_MAX_AGE_MILLISECONDS');
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toStrictEqual({
+      data: {
+        signIn: {
+          id: user._id.toString(),
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email,
+          systemCount: 0,
+          constantSymbolCount: 0,
+          variableSymbolCount: 0,
+          axiomCount: 0,
+          theoremCount: 0,
+          deductionCount: 0,
+          proofCount: 0
+        }
+      }
+    });
+    expect(cookies).toBeDefined();
+    expect(cookies).toHaveLength(2);
+    expect(cookies![0]).toMatch(/^token=.+; Max-Age=1; Path=\/; Expires=(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12]\d|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d) GMT; HttpOnly; Secure$/);
+    expect(cookies![1]).toMatch(/^authStatus=true; Max-Age=1; Path=\/; Expires=(Mon|Tue|Wed|Thu|Fri|Sat|Sun), (0[1-9]|[12]\d|3[01]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d) GMT; Secure$/);
   });
 
   afterAll(async (): Promise<void> => {
--- back-end/modules/auth/tests/sign-out.test.ts
+++ back-end/modules/auth/tests/sign-out.test.ts
@@ -2,25 +2,31 @@ import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
 import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
 import { MongoUserEntity } from '@/user/entities/mongo-user.entity';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { hashSync } from 'bcryptjs';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 
 describe('Sign Out', (): void => {
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
-  let app: INestApplication;
+  let app: NestExpressApplication
 
   beforeAll(async (): Promise<void> => {
     app = await createTestApp();
   });
 
-  it('succeeds', async (): Promise<void> => {
+  it('POST /auth/sign-out (user search: successful)', async (): Promise<void> => {
     const userId = new ObjectId();
     const user = new MongoUserEntity();
 
     user._id = userId;
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
+    user.email = 'test1.user1@example.com';
+    user.hashedPassword = hashSync('Test1User1!');
 
     findOneBy.mockResolvedValueOnce(user);
 
@@ -41,7 +47,62 @@ describe('Sign Out', (): void => {
     });
     expect(getOrThrow).toHaveBeenCalledTimes(0);
     expect(statusCode).toBe(HttpStatus.NO_CONTENT);
-    expect(body).toEqual({});
+    expect(body).toStrictEqual({
+    });
+    expect(cookies).toBeDefined();
+    expect(cookies).toHaveLength(2);
+    expect(cookies![0]).toBe('token=; Path=\/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+    expect(cookies![1]).toBe('authStatus=; Path=\/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+  });
+
+  it('POST /graphql mutation signOut (user search: successful)', async (): Promise<void> => {
+    const userId = new ObjectId();
+    const user = new MongoUserEntity();
+
+    user._id = userId;
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
+    user.email = 'test1.user1@example.com';
+    user.hashedPassword = hashSync('Test1User1!');
+
+    findOneBy.mockResolvedValueOnce(user);
+
+    const token = app.get(JwtService).sign({
+      userId
+    });
+
+    const response = await request(app.getHttpServer()).post('/graphql').set('Cookie', [
+      `token=${token}`
+    ]).send({
+      query: 'mutation { signOut { id firstName lastName email systemCount constantSymbolCount variableSymbolCount axiomCount theoremCount deductionCount proofCount } }'
+    });
+
+    const { statusCode, body } = response;
+    const cookies = response.get('Set-Cookie');
+
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: userId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toStrictEqual({
+      data: {
+        signOut: {
+          id: user._id.toString(),
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          systemCount: 0,
+          constantSymbolCount: 0,
+          variableSymbolCount: 0,
+          axiomCount: 0,
+          theoremCount: 0,
+          deductionCount: 0,
+          proofCount: 0
+        }
+      }
+    });
     expect(cookies).toBeDefined();
     expect(cookies).toHaveLength(2);
     expect(cookies![0]).toBe('token=; Path=\/; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
