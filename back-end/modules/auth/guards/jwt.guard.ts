dad133df8b69eccc390b69744a6d101f484ed40a
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-15 16:00:21 -0600

    Improve guards to cover GraphQL operations

--- back-end/modules/auth/guards/jwt.guard.ts
+++ back-end/modules/auth/guards/jwt.guard.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
+  public override getRequest(context: ExecutionContext): Request {
+    switch (context.getType()) {
+      case 'http':
+        const httpArgumentHost = context.switchToHttp();
+
+        return httpArgumentHost.getRequest<Request>();
+      default:
+        const gqlExecutionContext = GqlExecutionContext.create(context);
+
+        const gqlContext = gqlExecutionContext.getContext<{ req: Request; res: Response }>();
+
+        const { req } = gqlContext;
+
+        return req;
+    }
+  }
 };
--- back-end/modules/auth/guards/local.guard.ts
+++ back-end/modules/auth/guards/local.guard.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LocalGuard extends AuthGuard('local') {
+  public override getRequest(context: ExecutionContext): Request {
+    switch (context.getType()) {
+      case 'http':
+        const httpArgumentHost = context.switchToHttp();
+
+        return httpArgumentHost.getRequest<Request>();
+      default:
+        const gqlExecutionContext = GqlExecutionContext.create(context);
+
+        const gqlArgs = gqlExecutionContext.getArgs<{ email: string; password: string; }>();
+
+        const gqlContext = gqlExecutionContext.getContext<{ req: Request; res: Response }>();
+
+        const { req } = gqlContext;
+
+        req.body = { ...req.body, ...gqlArgs };
+
+        return req;
+    }
+  }
 };
