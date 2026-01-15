7f091ccf171bbfa1304c574ed25911c3ddf80d67
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-15 15:56:43 -0600

    Improved exceptions

--- back-end/modules/auth/exceptions/invalid-credentials.exception.ts
+++ back-end/modules/auth/exceptions/invalid-credentials.exception.ts
@@ -1,7 +1,7 @@
 import { UnauthorizedException } from '@nestjs/common';
 
 export class InvalidCredentialsException extends UnauthorizedException {
-  constructor() {
-    super('Invalid e-mail address or password.');
+  public constructor() {
+    super('Invalid e-mail address or password');
   }
 };
