8ae89ea35b7e37ce8ade018c427d00706082a8c2
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-15 16:00:41 -0600

    Improve session user decorator to work with either REST or GraphQL

--- back-end/modules/auth/decorators/session-user.decorator.ts
+++ back-end/modules/auth/decorators/session-user.decorator.ts
@@ -1,9 +1,27 @@
 import { UserEntity } from '@/user/entities/user.entity';
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-import { Request } from 'express';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Request, Response } from 'express';
 
-export const SessionUserDecorator = createParamDecorator((key: keyof UserEntity | undefined, context: ExecutionContext): UserEntity | UserEntity[keyof UserEntity] => {
-  const request = context.switchToHttp().getRequest<Request>();
+const getRequest = (context: ExecutionContext): Request => {
+  switch (context.getType()) {
+    case 'http':
+      const httpArgumentHost = context.switchToHttp();
+
+      return httpArgumentHost.getRequest<Request>();
+    default:
+      const gqlExecutionContext = GqlExecutionContext.create(context);
+
+      const gqlContext = gqlExecutionContext.getContext<{ req: Request; res: Response; }>();
+
+      const { req } = gqlContext;
+
+      return req;
+  }
+};
+
+export const SessionUser = createParamDecorator((key: keyof UserEntity | undefined, context: ExecutionContext): UserEntity | UserEntity[keyof UserEntity] => {
+  const request = getRequest(context);
 
   const { user } = request;
 
