0c04ef22c2e7d8d2f596ef840b5fac5fcd897735
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-15 16:11:37 -0600

    Update auth module configuration

--- back-end/modules/auth/auth.module.ts
+++ back-end/modules/auth/auth.module.ts
@@ -2,7 +2,9 @@ import { UserModule } from '@/user/user.module';
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
+import { AuthController } from './controllers/auth.controller';
+import { AuthResolver } from './resolvers/auth.resolver';
+import { AuthService } from './services/auth.service';
 import { CookieService } from './services/cookie.service';
 import { TokenService } from './services/token.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -38,6 +40,8 @@ import { LocalStrategy } from './strategies/local.strategy';
     AuthController
   ],
   providers: [
+    AuthResolver,
+    AuthService,
     CookieService,
     JwtStrategy,
     LocalStrategy,
