699a4842bb176b881d4523f426455ece65df599d
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-20 17:58:00 -0600

    Improve new exception class

--- back-end/modules/system/exceptions/unique-title.exception.ts
+++ back-end/modules/system/exceptions/unique-title.exception.ts
@@ -1,7 +1,7 @@
 import { ConflictException } from '@nestjs/common';
 
 export class UniqueTitleException extends ConflictException {
-  constructor() {
-    super('Systems created by the same user must have a unique title.');
+  public constructor() {
+    super('Systems created by the same user must have a unique title');
   }
 };
