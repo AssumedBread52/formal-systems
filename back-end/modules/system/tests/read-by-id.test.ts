b138f4997fa8a87ca5e9aaa9b7f013406da04790
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-27 20:35:17 -0600

    Fixed two more tests

--- back-end/modules/system/tests/read-by-id.test.ts
+++ back-end/modules/system/tests/read-by-id.test.ts
@@ -2,30 +2,31 @@ import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
 import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
 import { MongoSystemEntity } from '@/system/entities/mongo-system.entity';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 
 describe('Read System by ID', (): void => {
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeAll(async (): Promise<void> => {
     app = await createTestApp();
   });
 
-  it('succeeds', async (): Promise<void> => {
+  it('GET /system/:systemId', async (): Promise<void> => {
+    const userId = new ObjectId();
     const systemId = new ObjectId();
-    const title = 'Test System';
-    const description = 'This is a test.';
+    const title = 'TestSystem1';
+    const description = 'Test System 1';
     const constantSymbolCount = 6;
     const variableSymbolCount = 3;
     const axiomCount = 6;
     const theoremCount = 1;
     const deductionCount = 3;
     const proofCount = 6;
-    const createdByUserId = new ObjectId();
     const system = new MongoSystemEntity();
 
     system._id = systemId;
@@ -37,7 +38,7 @@ describe('Read System by ID', (): void => {
     system.theoremCount = theoremCount;
     system.deductionCount = deductionCount;
     system.proofCount = proofCount;
-    system.createdByUserId = createdByUserId;
+    system.createdByUserId = userId;
 
     findOneBy.mockResolvedValueOnce(system);
 
@@ -51,7 +52,7 @@ describe('Read System by ID', (): void => {
     });
     expect(getOrThrow).toHaveBeenCalledTimes(0);
     expect(statusCode).toBe(HttpStatus.OK);
-    expect(body).toEqual({
+    expect(body).toStrictEqual({
       id: systemId.toString(),
       title,
       description,
@@ -61,7 +62,66 @@ describe('Read System by ID', (): void => {
       theoremCount,
       deductionCount,
       proofCount,
-      createdByUserId: createdByUserId.toString()
+      createdByUserId: userId.toString()
+    });
+  });
+
+  it('POST /graphql query system', async (): Promise<void> => {
+    const userId = new ObjectId();
+    const systemId = new ObjectId();
+    const title = 'TestSystem1';
+    const description = 'Test System 1';
+    const constantSymbolCount = 6;
+    const variableSymbolCount = 3;
+    const axiomCount = 6;
+    const theoremCount = 1;
+    const deductionCount = 3;
+    const proofCount = 6;
+    const system = new MongoSystemEntity();
+
+    system._id = systemId;
+    system.title = title;
+    system.description = description;
+    system.constantSymbolCount = constantSymbolCount;
+    system.variableSymbolCount = variableSymbolCount;
+    system.axiomCount = axiomCount;
+    system.theoremCount = theoremCount;
+    system.deductionCount = deductionCount;
+    system.proofCount = proofCount;
+    system.createdByUserId = userId;
+
+    findOneBy.mockResolvedValueOnce(system);
+
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: 'mutation system($systemId: String!) { system(systemId: $systemId) { id title description constantSymbolCount variableSymbolCount axiomCount theoremCount deductionCount proofCount createdByUserId } }',
+      variables: {
+        systemId
+      }
+    });
+
+    const { statusCode, body } = response;
+
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: systemId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toStrictEqual({
+      data: {
+        system: {
+          id: systemId.toString(),
+          title,
+          description,
+          constantSymbolCount,
+          variableSymbolCount,
+          axiomCount,
+          theoremCount,
+          deductionCount,
+          proofCount,
+          createdByUserId: userId.toString()
+        }
+      }
     });
   });
 
--- back-end/modules/system/tests/update-system.test.ts
+++ back-end/modules/system/tests/update-system.test.ts
@@ -1,52 +1,56 @@
 import { createTestApp } from '@/common/tests/helpers/create-test-app';
-import { existsByMock } from '@/common/tests/mocks/exists-by.mock';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
 import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
 import { saveMock } from '@/common/tests/mocks/save.mock';
 import { MongoSystemEntity } from '@/system/entities/mongo-system.entity';
 import { MongoUserEntity } from '@/user/entities/mongo-user.entity';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { hashSync } from 'bcryptjs';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 
 describe('Update System', (): void => {
-  const existsBy = existsByMock();
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
   const save = saveMock();
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeAll(async (): Promise<void> => {
     app = await createTestApp();
   });
 
-  it('succeeds', async (): Promise<void> => {
+  it('PATCH /system/:systemId', async (): Promise<void> => {
+    const userId = new ObjectId();
     const systemId = new ObjectId();
-    const newTitle = 'New Test System';
-    const newDescription = 'This is a new test.';
+    const newTitle = 'TestSystem2';
+    const newDescription = 'Test System 2';
     const constantSymbolCount = 6;
     const variableSymbolCount = 3;
     const axiomCount = 6;
     const theoremCount = 1;
     const deductionCount = 3;
     const proofCount = 6;
-    const createdByUserId = new ObjectId();
     const user = new MongoUserEntity();
     const system = new MongoSystemEntity();
     const updatedSystem = new MongoSystemEntity();
 
-    user._id = createdByUserId;
+    user._id = userId;
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
+    user.email = 'test1.user1@example.com';
+    user.hashedPassword = hashSync('TestUser1!');
     system._id = systemId;
-    system.title = 'Test System';
-    system.description = 'This is a test.';
+    system.title = 'TestSystem1';
+    system.description = 'Test System 1';
     system.constantSymbolCount = constantSymbolCount;
     system.variableSymbolCount = variableSymbolCount;
     system.axiomCount = axiomCount;
     system.theoremCount = theoremCount;
     system.deductionCount = deductionCount;
     system.proofCount = proofCount;
-    system.createdByUserId = createdByUserId;
+    system.createdByUserId = userId;
     updatedSystem._id = systemId;
     updatedSystem.title = newTitle;
     updatedSystem.description = newDescription;
@@ -56,15 +60,15 @@ describe('Update System', (): void => {
     updatedSystem.theoremCount = theoremCount;
     updatedSystem.deductionCount = deductionCount;
     updatedSystem.proofCount = proofCount;
-    updatedSystem.createdByUserId = createdByUserId;
+    updatedSystem.createdByUserId = userId;
 
-    existsBy.mockResolvedValueOnce(false);
     findOneBy.mockResolvedValueOnce(user);
     findOneBy.mockResolvedValueOnce(system);
+    findOneBy.mockResolvedValueOnce(null);
     save.mockResolvedValueOnce(updatedSystem);
 
     const token = app.get(JwtService).sign({
-      userId: createdByUserId
+      userId
     });
 
     const response = await request(app.getHttpServer()).patch(`/system/${systemId}`).set('Cookie', [
@@ -76,23 +80,22 @@ describe('Update System', (): void => {
 
     const { statusCode, body } = response;
 
-    expect(existsBy).toHaveBeenCalledTimes(1);
-    expect(existsBy).toHaveBeenNthCalledWith(1, {
-      title: newTitle,
-      createdByUserId
-    });
-    expect(findOneBy).toHaveBeenCalledTimes(2);
+    expect(findOneBy).toHaveBeenCalledTimes(3);
     expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: createdByUserId
+      _id: userId
     });
     expect(findOneBy).toHaveBeenNthCalledWith(2, {
       _id: systemId
+    });
+    expect(findOneBy).toHaveBeenNthCalledWith(3, {
+      title: newTitle,
+      createdByUserId: userId
     });
     expect(getOrThrow).toHaveBeenCalledTimes(0);
     expect(save).toHaveBeenCalledTimes(1);
     expect(save).toHaveBeenNthCalledWith(1, updatedSystem);
     expect(statusCode).toBe(HttpStatus.OK);
-    expect(body).toEqual({
+    expect(body).toStrictEqual({
       id: systemId.toString(),
       title: newTitle,
       description: newDescription,
@@ -102,7 +105,105 @@ describe('Update System', (): void => {
       theoremCount,
       deductionCount,
       proofCount,
-      createdByUserId: createdByUserId.toString()
+      createdByUserId: userId.toString()
+    });
+  });
+
+  it('POST /graphql mutation updateSystem', async (): Promise<void> => {
+    const userId = new ObjectId();
+    const systemId = new ObjectId();
+    const newTitle = 'TestSystem2';
+    const newDescription = 'Test System 2';
+    const constantSymbolCount = 6;
+    const variableSymbolCount = 3;
+    const axiomCount = 6;
+    const theoremCount = 1;
+    const deductionCount = 3;
+    const proofCount = 6;
+    const user = new MongoUserEntity();
+    const system = new MongoSystemEntity();
+    const updatedSystem = new MongoSystemEntity();
+
+    user._id = userId;
+    user.firstName = 'Test1';
+    user.lastName = 'User1';
+    user.email = 'test1.user1@example.com';
+    user.hashedPassword = hashSync('TestUser1!');
+    system._id = systemId;
+    system.title = 'TestSystem1';
+    system.description = 'Test System 1';
+    system.constantSymbolCount = constantSymbolCount;
+    system.variableSymbolCount = variableSymbolCount;
+    system.axiomCount = axiomCount;
+    system.theoremCount = theoremCount;
+    system.deductionCount = deductionCount;
+    system.proofCount = proofCount;
+    system.createdByUserId = userId;
+    updatedSystem._id = systemId;
+    updatedSystem.title = newTitle;
+    updatedSystem.description = newDescription;
+    updatedSystem.constantSymbolCount = constantSymbolCount;
+    updatedSystem.variableSymbolCount = variableSymbolCount;
+    updatedSystem.axiomCount = axiomCount;
+    updatedSystem.theoremCount = theoremCount;
+    updatedSystem.deductionCount = deductionCount;
+    updatedSystem.proofCount = proofCount;
+    updatedSystem.createdByUserId = userId;
+
+    findOneBy.mockResolvedValueOnce(user);
+    findOneBy.mockResolvedValueOnce(system);
+    findOneBy.mockResolvedValueOnce(null);
+    save.mockResolvedValueOnce(updatedSystem);
+
+    const token = app.get(JwtService).sign({
+      userId
+    });
+
+    const response = await request(app.getHttpServer()).post('/graphql').set('Cookie', [
+      `token=${token}`
+    ]).send({
+      query: 'mutation updateSystem($systemId: String!, $editSystemPayload: EditSystemPayload!) { updateSystem(systemId: $systemId, systemPayload: $editSystemPayload) { id title description constantSymbolCount variableSymbolCount axiomCount theoremCount deductionCount proofCount createdByUserId } }',
+      variables: {
+        systemId,
+        editSystemPayload: {
+          newTitle,
+          newDescription
+        }
+      }
+    });
+
+    const { statusCode, body } = response;
+
+    expect(findOneBy).toHaveBeenCalledTimes(3);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: userId
+    });
+    expect(findOneBy).toHaveBeenNthCalledWith(2, {
+      _id: systemId
+    });
+    expect(findOneBy).toHaveBeenNthCalledWith(3, {
+      title: newTitle,
+      createdByUserId: userId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(save).toHaveBeenCalledTimes(1);
+    expect(save).toHaveBeenNthCalledWith(1, updatedSystem);
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toStrictEqual({
+      data: {
+        updateSystem: {
+          id: systemId.toString(),
+          title: newTitle,
+          description: newDescription,
+          constantSymbolCount,
+          variableSymbolCount,
+          axiomCount,
+          theoremCount,
+          deductionCount,
+          proofCount,
+          createdByUserId: userId.toString()
+        }
+      }
     });
   });
 
