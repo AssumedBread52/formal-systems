7d5a50710bc062f62ef5bf27fe014adf7898334d
Antonio Sanchez <asanchez16756@gmail.com>
2026-01-30 18:34:28 -0600

    Fix typescript error

--- back-end/modules/common/tests/mocks/find-and-count.mock.ts
+++ back-end/modules/common/tests/mocks/find-and-count.mock.ts
@@ -1,7 +1,7 @@
 import { MongoRepository } from 'typeorm';
 import { MongoFindManyOptions } from 'typeorm/find-options/mongodb/MongoFindManyOptions';
 
-export const findAndCountMock = (): jest.SpyInstance<Promise<[any[], number]>, [options?: MongoFindManyOptions<any>], any> => {
+export const findAndCountMock = (): jest.SpyInstance<Promise<[any[], number]>, [options?: MongoFindManyOptions<any> | undefined], any> => {
   const findAndCount = jest.spyOn(MongoRepository.prototype, 'findAndCount');
 
   beforeEach((): void => {
