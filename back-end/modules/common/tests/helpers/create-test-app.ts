1fee05bfb8dd888ce8eae95750ec766bec57c96d
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-03 17:12:36 -0600

    Fix import path

--- back-end/modules/common/tests/helpers/create-test-app.ts
+++ back-end/modules/common/tests/helpers/create-test-app.ts
@@ -3,7 +3,7 @@ import { DependencyModule } from '@/dependency/dependency.module';
 import { HealthModule } from '@/health/health.module';
 import { StatementEntity } from '@/statement/statement.entity';
 import { StatementModule } from '@/statement/statement.module';
-import { SymbolEntity } from '@/symbol/symbol.entity';
+import { SymbolEntity } from '@/symbol/entities/symbol.entity';
 import { SymbolModule } from '@/symbol/symbol.module';
 import { MongoSystemEntity } from '@/system/entities/mongo-system.entity';
 import { SystemModule } from '@/system/system.module';
