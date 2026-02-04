613e1a8cdd2c0aedcad131b6b05eef5d511287ac
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-04 16:53:33 -0600

    Fix symbol read by ID test for future symbol module changes

--- back-end/modules/symbol/tests/read-by-id.test.ts
+++ back-end/modules/symbol/tests/read-by-id.test.ts
@@ -1,87 +1,35 @@
-import { createTestApp } from '@/app/tests/helpers/create-test-app';
-import { getOrThrowMock } from '@/app/tests/mocks/get-or-throw.mock';
+import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
+import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
+import { MongoSymbolEntity } from '@/symbol/entities/mongo-symbol.entity';
 import { SymbolType } from '@/symbol/enums/symbol-type.enum';
-import { SymbolEntity } from '@/symbol/symbol.entity';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { ObjectId } from 'mongodb';
 import * as request from 'supertest';
 
 describe('Read Symbol by ID', (): void => {
   const findOneBy = findOneByMock();
   const getOrThrow = getOrThrowMock();
-  let app: INestApplication;
+  let app: NestExpressApplication;
 
   beforeAll(async (): Promise<void> => {
     app = await createTestApp();
   });
 
-  it('fails with an invalid symbol ID', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).get('/system/1/symbol/1');
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
-    expect(body).toEqual({
-      error: 'Unprocessable Entity',
-      message: 'Invalid Object ID.',
-      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
-    });
-  });
-
-  it('fails with an invalid system ID', async (): Promise<void> => {
-    const response = await request(app.getHttpServer()).get(`/system/1/symbol/${new ObjectId()}`);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(0);
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
-    expect(body).toEqual({
-      error: 'Unprocessable Entity',
-      message: 'Invalid Object ID.',
-      statusCode: HttpStatus.UNPROCESSABLE_ENTITY
-    });
-  });
-
-  it('fails if the symbol does not exist', async (): Promise<void> => {
-    const symbolId = new ObjectId();
+  it('GET /system/:systemId/symbol/:symbolId', async (): Promise<void> => {
+    const userId = new ObjectId();
     const systemId = new ObjectId();
-
-    findOneBy.mockResolvedValueOnce(null);
-
-    const response = await request(app.getHttpServer()).get(`/system/${systemId}/symbol/${symbolId}`);
-
-    const { statusCode, body } = response;
-
-    expect(findOneBy).toHaveBeenCalledTimes(1);
-    expect(findOneBy).toHaveBeenNthCalledWith(1, {
-      _id: symbolId,
-      systemId
-    });
-    expect(getOrThrow).toHaveBeenCalledTimes(0);
-    expect(statusCode).toBe(HttpStatus.NOT_FOUND);
-    expect(body).toEqual({
-      error: 'Not Found',
-      message: 'Symbol not found.',
-      statusCode: HttpStatus.NOT_FOUND
-    });
-  });
-
-  it('succeeds', async (): Promise<void> => {
     const symbolId = new ObjectId();
-    const title = 'Test Symbol';
-    const description = 'This is a test.';
-    const type = SymbolType.Variable;
+    const title = 'TestSymbol1';
+    const description = 'Test Symbol 1';
+    const type = SymbolType.variable;
     const content = '\\alpha';
     const axiomAppearanceCount = 6;
     const theoremAppearanceCount = 1;
     const deductionAppearanceCount = 2;
-    const systemId = new ObjectId();
-    const createdByUserId = new ObjectId();
-    const symbol = new SymbolEntity();
+    const proofAppearanceCount = 4;
+    const symbol = new MongoSymbolEntity();
 
     symbol._id = symbolId;
     symbol.title = title;
@@ -91,8 +39,9 @@ describe('Read Symbol by ID', (): void => {
     symbol.axiomAppearanceCount = axiomAppearanceCount;
     symbol.theoremAppearanceCount = theoremAppearanceCount;
     symbol.deductionAppearanceCount = deductionAppearanceCount;
+    symbol.proofAppearanceCount = proofAppearanceCount;
     symbol.systemId = systemId;
-    symbol.createdByUserId = createdByUserId;
+    symbol.createdByUserId = userId;
 
     findOneBy.mockResolvedValueOnce(symbol);
 
@@ -107,7 +56,7 @@ describe('Read Symbol by ID', (): void => {
     });
     expect(getOrThrow).toHaveBeenCalledTimes(0);
     expect(statusCode).toBe(HttpStatus.OK);
-    expect(body).toEqual({
+    expect(body).toStrictEqual({
       id: symbolId.toString(),
       title,
       description,
@@ -116,8 +65,73 @@ describe('Read Symbol by ID', (): void => {
       axiomAppearanceCount,
       theoremAppearanceCount,
       deductionAppearanceCount,
+      proofAppearanceCount,
       systemId: systemId.toString(),
-      createdByUserId: createdByUserId.toString()
+      createdByUserId: userId.toString()
+    });
+  });
+
+  it('POST /graphql query symbol', async (): Promise<void> => {
+    const userId = new ObjectId();
+    const systemId = new ObjectId();
+    const symbolId = new ObjectId();
+    const title = 'TestSymbol1';
+    const description = 'Test Symbol 1';
+    const type = SymbolType.variable;
+    const content = '\\alpha';
+    const axiomAppearanceCount = 6;
+    const theoremAppearanceCount = 1;
+    const deductionAppearanceCount = 2;
+    const proofAppearanceCount = 4;
+    const symbol = new MongoSymbolEntity();
+
+    symbol._id = symbolId;
+    symbol.title = title;
+    symbol.description = description;
+    symbol.type = type;
+    symbol.content = content;
+    symbol.axiomAppearanceCount = axiomAppearanceCount;
+    symbol.theoremAppearanceCount = theoremAppearanceCount;
+    symbol.deductionAppearanceCount = deductionAppearanceCount;
+    symbol.proofAppearanceCount = proofAppearanceCount;
+    symbol.systemId = systemId;
+    symbol.createdByUserId = userId;
+
+    findOneBy.mockResolvedValueOnce(symbol);
+
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: 'query symbol($systemId: String!, $symbolId: String!) { symbol(systemId: $systemId, symbolId: $symbolId) { id title description type content axiomAppearanceCount theoremAppearanceCount deductionAppearanceCount proofAppearanceCount systemId createdByUserId } }',
+      variables: {
+        systemId,
+        symbolId
+      }
+    });
+
+    const { statusCode, body } = response;
+
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      _id: symbolId,
+      systemId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(statusCode).toBe(HttpStatus.OK);
+    expect(body).toStrictEqual({
+      data: {
+        symbol: {
+          id: symbolId.toString(),
+          title,
+          description,
+          type,
+          content,
+          axiomAppearanceCount,
+          theoremAppearanceCount,
+          deductionAppearanceCount,
+          proofAppearanceCount,
+          systemId: systemId.toString(),
+          createdByUserId: userId.toString()
+        }
+      }
     });
   });
 
