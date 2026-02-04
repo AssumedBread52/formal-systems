cc89a86fb55a48ec5058a9bf34b37605e7c28e5e
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-04 17:27:41 -0600

    Remove symbol payload

--- back-end/modules/symbol/controllers/symbol.controller.ts
+++ back-end/modules/symbol/controllers/symbol.controller.ts
@@ -2,7 +2,6 @@ import { SessionUser } from '@/auth/decorators/session-user.decorator';
 import { JwtGuard } from '@/auth/guards/jwt.guard';
 import { PaginatedResultsPayload } from '@/common/payloads/paginated-results.payload';
 import { SymbolEntity } from '@/symbol/entities/symbol.entity';
-import { SymbolPayload } from '@/symbol/payloads/symbol.payload';
 import { SymbolService } from '@/symbol/services/symbol.service';
 import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ObjectId } from 'mongodb';
@@ -15,10 +14,8 @@ export class SymbolController {
   @Delete(':symbolId')
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public async deleteSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolPayload> {
-    const deletedSymbol = await this.symbolService.delete(sessionUserId, systemId, symbolId);
-
-    return new SymbolPayload(deletedSymbol);
+  public async deleteSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolEntity> {
+    return this.symbolService.delete(sessionUserId, systemId, symbolId);
   }
 
   @Get()
@@ -31,27 +28,21 @@ export class SymbolController {
 
   @Get(':symbolId')
   @UseInterceptors(ClassSerializerInterceptor)
-  public async getById(@Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolPayload> {
-    const symbol = await this.symbolService.readById(systemId, symbolId);
-
-    return new SymbolPayload(symbol);
+  public getById(@Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolEntity> {
+    return this.symbolService.readById(systemId, symbolId);
   }
 
   @Patch(':symbolId')
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public async patchSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string, @Body() payload: any): Promise<SymbolPayload> {
-    const updatedSymbol = await this.symbolService.update(sessionUserId, systemId, symbolId, payload);
-
-    return new SymbolPayload(updatedSymbol);
+  public patchSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string, @Body() payload: any): Promise<SymbolEntity> {
+    return this.symbolService.update(sessionUserId, systemId, symbolId, payload);
   }
 
   @Post()
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public async postSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Body() payload: any): Promise<SymbolPayload> {
-    const createdSymbol = await this.symbolService.create(sessionUserId, systemId, payload);
-
-    return new SymbolPayload(createdSymbol);
+  public postSymbol(@SessionUser('id') sessionUserId: ObjectId, @Param('systemId') systemId: string, @Body() payload: any): Promise<SymbolEntity> {
+    return this.symbolService.create(sessionUserId, systemId, payload);
   }
 };
