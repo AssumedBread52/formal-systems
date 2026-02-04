a0c3bc7fe2224f7e2c23d12048441eddd6b878dc
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-04 17:35:51 -0600

    Let nest validation input payloads

--- back-end/modules/symbol/controllers/symbol.controller.ts
+++ back-end/modules/symbol/controllers/symbol.controller.ts
@@ -2,8 +2,10 @@ import { SessionUser } from '@/auth/decorators/session-user.decorator';
 import { JwtGuard } from '@/auth/guards/jwt.guard';
 import { PaginatedResultsPayload } from '@/common/payloads/paginated-results.payload';
 import { SymbolEntity } from '@/symbol/entities/symbol.entity';
+import { EditSymbolPayload } from '@/symbol/payloads/edit-symbol.payload';
+import { NewSymbolPayload } from '@/symbol/payloads/new-symbol.payload';
 import { SymbolService } from '@/symbol/services/symbol.service';
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
 
 @Controller('system/:systemId/symbol')
 export class SymbolController {
@@ -34,14 +36,14 @@ export class SymbolController {
   @Patch(':symbolId')
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public patchSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string, @Body() payload: any): Promise<SymbolEntity> {
-    return this.symbolService.update(sessionUserId, systemId, symbolId, payload);
+  public patchSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string, @Body(new ValidationPipe({ transform: true })) editSymbolPayload: EditSymbolPayload): Promise<SymbolEntity> {
+    return this.symbolService.update(sessionUserId, systemId, symbolId, editSymbolPayload);
   }
 
   @Post()
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public postSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Body() payload: any): Promise<SymbolEntity> {
-    return this.symbolService.create(sessionUserId, systemId, payload);
+  public postSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Body(new ValidationPipe({ transform: true })) newSymbolPayload: NewSymbolPayload): Promise<SymbolEntity> {
+    return this.symbolService.create(sessionUserId, systemId, newSymbolPayload);
   }
 };
