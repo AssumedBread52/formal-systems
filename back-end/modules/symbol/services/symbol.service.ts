9c4ab2ee422740a9788bbf46ec86bec4295e30ab
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-04 18:36:33 -0600

    Made create use case more robust

--- back-end/modules/symbol/services/symbol.service.ts
+++ back-end/modules/symbol/services/symbol.service.ts
@@ -1,5 +1,6 @@
 import { OwnershipException } from '@/auth/exceptions/ownership.exception';
 import { InvalidObjectIdException } from '@/common/exceptions/invalid-object-id.exception';
+import { validatePayload } from '@/common/helpers/validate-payload';
 import { SymbolEntity } from '@/symbol/entities/symbol.entity';
 import { InUseException } from '@/symbol/exceptions/in-use.exception';
 import { SymbolNotFoundException } from '@/symbol/exceptions/symbol-not-found.exception';
@@ -11,47 +12,71 @@ import { SearchSymbolsPayload } from '@/symbol/payloads/search-symbols.payload';
 import { SymbolRepository } from '@/symbol/repositories/symbol.repository';
 import { SystemNotFoundException } from '@/system/exceptions/system-not-found.exception';
 import { SystemRepository } from '@/system/repositories/system.repository';
-import { Injectable, ValidationPipe } from '@nestjs/common';
+import { HttpException, Injectable, InternalServerErrorException, ValidationPipe } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { ClassConstructor, plainToClass } from 'class-transformer';
 import { isMongoId, validateSync } from 'class-validator';
 import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class SymbolService {
-  public constructor(private readonly symbolRepository: SymbolRepository, private readonly systemRepository: SystemRepository) {
+  public constructor(private readonly eventEmitter2: EventEmitter2, private readonly symbolRepository: SymbolRepository, private readonly systemRepository: SystemRepository) {
   }
 
-  async create(sessionUserId: string, systemId: any, payload: any): Promise<SymbolEntity> {
-    const system = await this.systemRepository.findOneBy({
-      id: systemId
-    });
+  public async create(sessionUserId: string, systemId: string, newSymbolPayload: NewSymbolPayload): Promise<SymbolEntity> {
+    try {
+      if (!isMongoId(sessionUserId)) {
+        throw new Error('Invalid session user ID');
+      }
 
-    if (!system) {
-      throw new SystemNotFoundException();
+      if (!isMongoId(systemId)) {
+        throw new Error('Invalid system ID');
+      }
+
+      const validatedNewSymbolPayload = validatePayload(newSymbolPayload, NewSymbolPayload);
+
+      const system = await this.systemRepository.findOneBy({
+        id: systemId
+      });
+
+      if (!system) {
+        throw new SystemNotFoundException();
+      }
+
+      if (sessionUserId !== system.createdByUserId) {
+        throw new OwnershipException();
+      }
+
+      const conflict = await this.symbolRepository.findOneBy({
+        title: validatedNewSymbolPayload.title,
+        systemId
+      });
+
+      if (conflict) {
+        throw new UniqueTitleException();
+      }
+
+      const symbol = new SymbolEntity();
+
+      symbol.title = validatedNewSymbolPayload.title;
+      symbol.description = validatedNewSymbolPayload.description;
+      symbol.type = validatedNewSymbolPayload.type;
+      symbol.content = validatedNewSymbolPayload.content;
+      symbol.systemId = systemId;
+      symbol.createdByUserId = sessionUserId;
+
+      const savedSymbol = await this.symbolRepository.save(symbol);
+
+      this.eventEmitter2.emit('symbol.create.completed', savedSymbol);
+
+      return savedSymbol;
+    } catch (error: unknown) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException('Creating symbol failed');
     }
-
-    const { id, createdByUserId } = system;
-
-    if (createdByUserId.toString() !== sessionUserId) {
-      throw new OwnershipException();
-    }
-
-    const newSymbolPayload = this.payloadCheck(payload, NewSymbolPayload);
-
-    const { title, description, type, content } = newSymbolPayload;
-
-    await this.conflictCheck(title, id);
-
-    const symbol = new SymbolEntity();
-
-    symbol.title = title;
-    symbol.description = description;
-    symbol.type = type;
-    symbol.content = content;
-    symbol.systemId = id;
-    symbol.createdByUserId = sessionUserId;
-
-    return this.symbolRepository.save(symbol);
   }
 
   async delete(sessionUserId: string, systemId: any, symbolId: any): Promise<SymbolEntity> {
@@ -168,7 +193,7 @@ export class SymbolService {
     return new PaginatedSymbolsPayload(results, total);
   }
 
-  async conflictCheck(title: string, systemId: string): Promise<void> {
+  private async conflictCheck(title: string, systemId: string): Promise<void> {
     const collision = await this.symbolRepository.findOneBy({
       title,
       systemId
@@ -179,7 +204,7 @@ export class SymbolService {
     }
   }
 
-  idCheck(id: any): ObjectId {
+  private idCheck(id: any): ObjectId {
     if (!isMongoId(id)) {
       throw new InvalidObjectIdException();
     }
@@ -187,7 +212,7 @@ export class SymbolService {
     return new ObjectId(id);
   }
 
-  payloadCheck<Payload extends object>(payload: any, payloadConstructor: ClassConstructor<Payload>): Payload {
+  private payloadCheck<Payload extends object>(payload: any, payloadConstructor: ClassConstructor<Payload>): Payload {
     const newPayload = plainToClass(payloadConstructor, payload);
 
     const errors = validateSync(newPayload);
