9d319782be43699b719fe70a1008c7ecc092e2be
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-09 18:12:35 -0600

    Move statement controller and entity into relevant directories

--- back-end/modules/statement/statement.module.ts
+++ back-end/modules/statement/statement.module.ts
@@ -2,13 +2,13 @@ import { SymbolModule } from '@/symbol/symbol.module';
 import { SystemModule } from '@/system/system.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StatementController } from './controllers/statement.controller';
+import { StatementEntity } from './entities/statement.entity';
 import { StatementCreateService } from './services/statement-create.service';
 import { StatementDeleteService } from './services/statement-delete.service';
 import { StatementReadService } from './services/statement-read.service';
 import { StatementUpdateService } from './services/statement-update.service';
 import { ValidateService } from './services/validate.service';
-import { StatementController } from './statement.controller';
-import { StatementEntity } from './statement.entity';
 
 @Module({
   imports: [
