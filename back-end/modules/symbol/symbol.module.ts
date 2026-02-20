334d2cac6553fc9a85d24aec37e47f6446f02bda
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-20 17:49:16 -0600

    Fix import order

--- back-end/modules/symbol/symbol.module.ts
+++ back-end/modules/symbol/symbol.module.ts
@@ -3,11 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SymbolController } from './controllers/symbol.controller';
 import { MongoSymbolEntity } from './entities/mongo-symbol.entity';
+import { CountListener } from './listeners/count.listener';
 import { SymbolRepository } from './repositories/symbol.repository';
 import { SymbolResolver } from './resolvers/symbol.resolver';
 import { SymbolReadService } from './services/symbol-read.service';
 import { SymbolWriteService } from './services/symbol-write.service';
-import { CountListener } from './listeners/count.listener';
 
 @Module({
   imports: [
