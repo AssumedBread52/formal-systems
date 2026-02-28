fb176e822351c76c7db539a64d4d235f54570dd4
Antonio Sanchez <asanchez16756@gmail.com>
2026-02-28 15:51:37 -0600

    Added expression controller

---
+++ back-end/modules/expression/controllers/expression.controller.ts
@@ -0,0 +1,41 @@
+import { SessionUser } from '@/auth/decorators/session-user.decorator';
+import { JwtGuard } from '@/auth/guards/jwt.guard';
+import { ExpressionEntity } from '@/expression/entities/expression.entity';
+import { NewExpressionPayload } from '@/expression/payloads/new-expression.payload';
+import { PaginatedExpressionsPayload } from '@/expression/payloads/paginated-expressions.payload';
+import { SearchExpressionsPayload } from '@/expression/payloads/search-expressions.payload';
+import { ExpressionReadService } from '@/expression/services/expression-read.service';
+import { ExpressionWriteService } from '@/expression/services/expression-write.service';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Query, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
+
+@Controller('system/:systemId/expression')
+export class ExpressionController {
+  public constructor(private readonly expressionReadService: ExpressionReadService, private readonly expressionWriteService: ExpressionWriteService) {
+  }
+
+  @Delete(':expressionId')
+  @UseGuards(JwtGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  public deleteExpression(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Param('expressionId') expressionId: string): Promise<ExpressionEntity> {
+    return this.expressionWriteService.delete(sessionUserId, systemId, expressionId);
+  }
+
+  @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
+  public getExpressions(@Param('systemId') systemId: string, @Query(new ValidationPipe({ transform: true })) searchExpressionsPayload: SearchExpressionsPayload): Promise<PaginatedExpressionsPayload> {
+    return this.expressionReadService.searchExpressions(systemId, searchExpressionsPayload);
+  }
+
+  @Get(':expressionId')
+  @UseInterceptors(ClassSerializerInterceptor)
+  public getById(@Param('systemId') systemId: string, @Param('expressionId') expressionId: string): Promise<ExpressionEntity> {
+    return this.expressionReadService.selectById(systemId, expressionId);
+  }
+
+  @Post()
+  @UseGuards(JwtGuard)
+  @UseInterceptors(ClassSerializerInterceptor)
+  public postExpression(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Body(new ValidationPipe({ transform: true })) newExpressionPayload: NewExpressionPayload): Promise<ExpressionEntity> {
+    return this.expressionWriteService.create(sessionUserId, systemId, newExpressionPayload);
+  }
+};
