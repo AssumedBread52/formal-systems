fcc86f1b0ba799ac6981949dcbf0dc49c992f3bb
Antonio Sanchez <asanchez16756@gmail.com>
2026-03-25 16:24:46 -0500

    Update types in find one by

--- back-end/modules/common/tests/mocks/find-one-by.mock.ts
+++ back-end/modules/common/tests/mocks/find-one-by.mock.ts
@@ -1,7 +1,7 @@
-import { MongoRepository } from 'typeorm';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
-export const findOneByMock = (): jest.SpyInstance<Promise<any>, [where: any], any> => {
-  const findOneBy = jest.spyOn(MongoRepository.prototype, 'findOneBy');
+export const findOneByMock = (): jest.SpyInstance<Promise<any>, [where: FindOptionsWhere<any> | FindOptionsWhere<any>[]], any> => {
+  const findOneBy = jest.spyOn(Repository.prototype, 'findOneBy');
 
   beforeEach((): void => {
     findOneBy.mockReset();
