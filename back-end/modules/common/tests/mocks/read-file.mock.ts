305f4d5388df46d03a2d704e8378ce26783ffe6f
Antonio Sanchez <asanchez16756@gmail.com>
2026-03-27 17:52:05 -0500

    Adjusted for new types

--- back-end/modules/common/tests/mocks/read-file.mock.ts
+++ back-end/modules/common/tests/mocks/read-file.mock.ts
@@ -2,7 +2,7 @@ import { Abortable } from 'events';
 import { ObjectEncodingOptions, OpenMode, PathLike } from 'fs';
 import * as fsPromises from 'fs/promises';
 
-export const readFileMock = (): jest.SpyInstance<Promise<string | Buffer<ArrayBufferLike>>, [path: PathLike | fsPromises.FileHandle, options?: (ObjectEncodingOptions & Abortable & { flag?: OpenMode | undefined; }) | BufferEncoding | null | undefined], any> => {
+export const readFileMock = (): jest.SpyInstance<Promise<string | Buffer<ArrayBuffer>>, [path: PathLike | fsPromises.FileHandle, options?: (ObjectEncodingOptions & Abortable & { flag?: OpenMode | undefined; }) | BufferEncoding | null | undefined], any> => {
   const readFile = jest.spyOn(fsPromises, 'readFile');
 
   beforeEach((): void => {
