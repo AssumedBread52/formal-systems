a88dea2071e8f5249c0f303a76dccaffb8a64d53
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-01 04:17:51 -0500

    Add systems table migration

---
+++ back-end/modules/migration/migrations/2-add-systems-table.migration.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddSystemsTable0000000000002 implements MigrationInterface {
+  public up(queryRunner: QueryRunner): Promise<any> {
+    return queryRunner.query(`
+      CREATE TABLE systems (
+        id            UUID          NOT NULL  DEFAULT gen_random_uuid(),
+        owner_user_id UUID          NOT NULL,
+        name          VARCHAR(200)  NOT NULL,
+        description   VARCHAR(5000) NOT NULL,
+
+        CONSTRAINT systems_primary_key          PRIMARY KEY (id),
+        CONSTRAINT systems_owner_foreign_key    FOREIGN KEY (owner_user_id) REFERENCES users(id),
+        CONSTRAINT systems_name_nonempty        CHECK(LENGTH(name) > 0),
+        CONSTRAINT systems_description_nonempty CHECK(LENGTH(description) > 0),
+        CONSTRAINT systems_owner_name_unique    UNIQUE(owner_user_id, name)
+      );
+    `);
+  }
+
+  public down(queryRunner: QueryRunner): Promise<any> {
+    return queryRunner.query('DROP TABLE systems;');
+  }
+};
--- back-end/modules/migration/migrations/index.ts
+++ back-end/modules/migration/migrations/index.ts
@@ -1,5 +1,7 @@
 import { AddUsersTable0000000000001 } from './1-add-users-table.migration';
+import { AddSystemsTable0000000000002 } from './2-add-systems-table.migration';
 
 export default {
-  AddUsersTable0000000000001
+  AddUsersTable0000000000001,
+  AddSystemsTable0000000000002
 };
