9769cbbd0fd4f8861cff7ff5c345110496ec2b60
Antonio Sanchez <asanchez16756@gmail.com>
2026-03-31 20:15:13 -0500

    Added missing two user tests

---
+++ back-end/modules/user/tests/read-users.test.ts
@@ -0,0 +1,74 @@
+import { validatePayload } from '@/common/helpers/validate-payload';
+import { createTestApp } from '@/common/tests/helpers/create-test-app';
+import { findAndCountMock } from '@/common/tests/mocks/find-and-count.mock';
+import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
+import { UserEntity } from '@/user/entities/user.entity';
+import { HttpStatus } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { hashSync } from 'bcryptjs';
+import { instanceToPlain } from 'class-transformer';
+import * as request from 'supertest';
+import { ILike } from 'typeorm';
+
+describe('Read Users', (): void => {
+  const findAndCount = findAndCountMock();
+  const getOrThrow = getOrThrowMock();
+  let app: NestExpressApplication;
+
+  beforeAll(async (): Promise<void> => {
+    app = await createTestApp();
+  });
+
+  it('GET /user', async (): Promise<void> => {
+    const page = 2;
+    const pageSize = 20;
+    const searchText = '1 User';
+    const total = 1;
+    const user = validatePayload({
+      id: 'f9c7d036-e7e1-4775-b33c-43138e506e82',
+      handle: 'Test1 User1',
+      email: 'test1.user1@example.com',
+      passwordHash: hashSync('Test1User1!')
+    }, UserEntity);
+
+    const urlSearchParams = new URLSearchParams();
+    urlSearchParams.set('page', page.toString());
+    urlSearchParams.set('pageSize', pageSize.toString());
+    urlSearchParams.set('searchText', searchText);
+
+    findAndCount.mockResolvedValueOnce([
+      [
+        user
+      ],
+      total
+    ]);
+
+    const response = await request(app.getHttpServer()).get(`/user?${urlSearchParams}`);
+
+    expect(findAndCount).toHaveBeenCalledTimes(1);
+    expect(findAndCount).toHaveBeenNthCalledWith(1, {
+      skip: 20,
+      take: 20,
+      where: [
+        {
+          handle: ILike(`%${searchText}%`)
+        },
+        {
+          email: ILike(`%${searchText}%`)
+        }
+      ]
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(response.body).toStrictEqual({
+      results: [
+        instanceToPlain(user)
+      ],
+      total
+    });
+    expect(response.statusCode).toBe(HttpStatus.OK);
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
---
+++ back-end/modules/user/tests/relations.test.ts
@@ -0,0 +1,83 @@
+import { validatePayload } from '@/common/helpers/validate-payload';
+import { createTestApp } from '@/common/tests/helpers/create-test-app';
+import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
+import { findMock } from '@/common/tests/mocks/find.mock';
+import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
+import { SystemEntity } from '@/system/entities/system.entity';
+import { UserEntity } from '@/user/entities/user.entity';
+import { HttpStatus } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { hashSync } from 'bcryptjs';
+import { instanceToPlain } from 'class-transformer';
+import * as request from 'supertest';
+import { In } from 'typeorm';
+
+describe('Relations', (): void => {
+  const find = findMock();
+  const findOneBy = findOneByMock();
+  const getOrThrow = getOrThrowMock();
+  let app: NestExpressApplication;
+
+  beforeAll(async (): Promise<void> => {
+    app = await createTestApp();
+  });
+
+  it('POST /graphql query user', async (): Promise<void> => {
+    const userId = 'f9c7d036-e7e1-4775-b33c-43138e506e82';
+    const handle = 'Test1 User1';
+    const email = 'test1.user1@example.com';
+    const user = validatePayload({
+      id: userId,
+      handle,
+      email,
+      passwordHash: hashSync('Test1User1!')
+    }, UserEntity);
+    const system = validatePayload({
+      id: 'ebe1615e-8c75-461a-b6f4-29db73a14ee7',
+      ownerUserId: userId,
+      name: 'TestSystem1',
+      description: 'Test System 1'
+    }, SystemEntity);
+
+    find.mockResolvedValueOnce([system]);
+    findOneBy.mockResolvedValueOnce(user);
+
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: 'query ($userId: String!) { user(userId: $userId) { id handle email systems { id ownerUserId name description } } }',
+      variables: {
+        userId
+      }
+    });
+
+    expect(find).toHaveBeenCalledTimes(1);
+    expect(find).toHaveBeenNthCalledWith(1, {
+      where: {
+        ownerUserId: In([
+          userId
+        ])
+      }
+    });
+    expect(findOneBy).toHaveBeenCalledTimes(1);
+    expect(findOneBy).toHaveBeenNthCalledWith(1, {
+      id: userId
+    });
+    expect(getOrThrow).toHaveBeenCalledTimes(0);
+    expect(response.body).toStrictEqual({
+      data: {
+        user: {
+          id: userId,
+          handle,
+          email,
+          systems: [
+            instanceToPlain(system)
+          ]
+        }
+      }
+    });
+    expect(response.statusCode).toBe(HttpStatus.OK);
+  });
+
+  afterAll(async (): Promise<void> => {
+    await app.close();
+  });
+});
