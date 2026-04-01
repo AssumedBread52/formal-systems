67f1810d21871350c612add5ab93531674e9ebba
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-01 04:01:14 -0500

    Simplify table constraints

--- back-end/modules/migration/migrations/1-add-users-table.migration.ts
+++ back-end/modules/migration/migrations/1-add-users-table.migration.ts
@@ -9,14 +9,11 @@ export class AddUsersTable0000000000001 implements MigrationInterface {
         email         VARCHAR(254)  NOT NULL,
         password_hash CHAR(60)      NOT NULL,
 
-        CONSTRAINT users_primary_key            PRIMARY KEY (id),
-        CONSTRAINT users_handle_nonempty        CHECK(LENGTH(BTRIM(handle)) > 0),
-        CONSTRAINT users_handle_no_outer_spaces CHECK(handle = BTRIM(handle)),
-        CONSTRAINT users_email_nonempty         CHECK(LENGTH(BTRIM(email)) > 0),
-        CONSTRAINT users_email_no_outer_spaces  CHECK(email = BTRIM(email)),
-        CONSTRAINT users_password_hash_nonempty CHECK(LENGTH(BTRIM(password_hash)) > 0),
-        CONSTRAINT users_handle_unique          UNIQUE(handle),
-        CONSTRAINT users_email_unique           UNIQUE(email)
+        CONSTRAINT users_primary_key      PRIMARY KEY (id),
+        CONSTRAINT users_handle_nonempty  CHECK(LENGTH(handle) > 0),
+        CONSTRAINT users_email_nonempty   CHECK(LENGTH(email) > 0),
+        CONSTRAINT users_handle_unique    UNIQUE(handle),
+        CONSTRAINT users_email_unique     UNIQUE(email)
       );
     `);
   }
