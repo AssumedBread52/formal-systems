ecb56c04239674951f4c91317346101b7357a9f5
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-01 04:19:10 -0500

    Don't transform instances just validate them

--- back-end/modules/common/helpers/validate-payload.ts
+++ back-end/modules/common/helpers/validate-payload.ts
@@ -3,20 +3,24 @@ import { validateSync } from 'class-validator';
 
 export const validatePayload = <Payload extends object>(payload: any, payloadConstructor: ClassConstructor<Payload>): Payload => {
   try {
-    const newPayload = plainToInstance(payloadConstructor, payload, {
-      ignoreDecorators: true
-    });
+    const isInstance = payload instanceof payloadConstructor;
 
-    const errors = validateSync(newPayload, {
-      forbidNonWhitelisted: true,
-      whitelist: true
+    if (!isInstance) {
+      payload = plainToInstance(payloadConstructor, payload, {
+        ignoreDecorators: true
+      });
+    }
+
+    const errors = validateSync(payload, {
+      forbidNonWhitelisted: !isInstance,
+      whitelist: !isInstance
     });
 
     if (0 < errors.length) {
       throw new Error('Payload validation errors detected');
     }
 
-    return newPayload;
+    return payload;
   } catch {
     throw new Error('Payload validation failed');
   }
