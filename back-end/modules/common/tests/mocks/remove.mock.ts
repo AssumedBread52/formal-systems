cb1bffc05eefc8b18b3b4375b34a09a6bb0d9fb0
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-01 20:39:38 -0500

    Switch to non mongo repository type

--- back-end/modules/common/tests/mocks/remove.mock.ts
+++ back-end/modules/common/tests/mocks/remove.mock.ts
@@ -1,7 +1,7 @@
-import { MongoRepository, RemoveOptions } from 'typeorm';
+import { RemoveOptions, Repository } from 'typeorm';
 
 export const removeMock = (): jest.SpyInstance<Promise<any>, [entity: any, options?: RemoveOptions | undefined], any> => {
-  const remove = jest.spyOn(MongoRepository.prototype, 'remove');
+  const remove = jest.spyOn(Repository.prototype, 'remove');
 
   beforeEach((): void => {
     remove.mockReset();
