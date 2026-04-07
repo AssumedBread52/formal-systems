45e5a1519e21848c97bfab6c79d5491513ddecab
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-06 20:57:14 -0500

    Update search symbols payload for postgres

--- back-end/modules/symbol/payloads/search-symbols.payload.ts
+++ back-end/modules/symbol/payloads/search-symbols.payload.ts
@@ -1,7 +1,7 @@
 import { SymbolType } from '@/symbol/enums/symbol-type.enum';
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import { ArrayMaxSize, ArrayUnique, IsArray, IsEnum, IsInt, IsNotEmpty, Min } from 'class-validator';
+import { ArrayMaxSize, ArrayUnique, IsArray, IsEnum, IsInt, IsString, Min } from 'class-validator';
 
 @InputType()
 export class SearchSymbolsPayload {
@@ -23,15 +23,11 @@ export class SearchSymbolsPayload {
     return Number;
   })
   public readonly pageSize: number = 10;
-  @ArrayUnique()
-  @Field((): [typeof String] => {
-    return [String];
+  @Field((): typeof String => {
+    return String;
   })
-  @IsArray()
-  @IsNotEmpty({
-    each: true
-  })
-  public readonly keywords: string[] = [];
+  @IsString()
+  public readonly searchText: string = '';
   @ArrayMaxSize(Object.keys(SymbolType).length)
   @ArrayUnique()
   @Field((): [typeof SymbolType] => {
