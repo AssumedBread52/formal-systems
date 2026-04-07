718584e4c16cb56f432bc865991853b4ef6851df
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-06 20:51:10 -0500

    Add additional parsing and validating enforcement

--- back-end/modules/symbol/controllers/symbol.controller.ts
+++ back-end/modules/symbol/controllers/symbol.controller.ts
@@ -7,7 +7,7 @@ import { PaginatedSymbolsPayload } from '@/symbol/payloads/paginated-symbols.pay
 import { SearchSymbolsPayload } from '@/symbol/payloads/search-symbols.payload';
 import { SymbolReadService } from '@/symbol/services/symbol-read.service';
 import { SymbolWriteService } from '@/symbol/services/symbol-write.service';
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Query, UseGuards, UseInterceptors, ValidationPipe } from '@nestjs/common';
 
 @Controller('system/:systemId/symbol')
 export class SymbolController {
@@ -17,33 +17,33 @@ export class SymbolController {
   @Delete(':symbolId')
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public deleteSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolEntity> {
+  public deleteSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId', new ParseUUIDPipe()) systemId: string, @Param('symbolId', new ParseUUIDPipe()) symbolId: string): Promise<SymbolEntity> {
     return this.symbolWriteService.delete(sessionUserId, systemId, symbolId);
   }
 
   @Get()
   @UseInterceptors(ClassSerializerInterceptor)
-  public getSymbols(@Param('systemId') systemId: string, @Query(new ValidationPipe({ transform: true })) searchSymbolsPayload: SearchSymbolsPayload): Promise<PaginatedSymbolsPayload> {
+  public getSymbols(@Param('systemId', new ParseUUIDPipe()) systemId: string, @Query(new ValidationPipe({ forbidNonWhitelisted: true, transform: true, whitelist: true })) searchSymbolsPayload: SearchSymbolsPayload): Promise<PaginatedSymbolsPayload> {
     return this.symbolReadService.searchSymbols(systemId, searchSymbolsPayload);
   }
 
   @Get(':symbolId')
   @UseInterceptors(ClassSerializerInterceptor)
-  public getById(@Param('systemId') systemId: string, @Param('symbolId') symbolId: string): Promise<SymbolEntity> {
+  public getById(@Param('systemId', new ParseUUIDPipe()) systemId: string, @Param('symbolId', new ParseUUIDPipe()) symbolId: string): Promise<SymbolEntity> {
     return this.symbolReadService.selectById(systemId, symbolId);
   }
 
   @Patch(':symbolId')
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public patchSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Param('symbolId') symbolId: string, @Body(new ValidationPipe({ transform: true })) editSymbolPayload: EditSymbolPayload): Promise<SymbolEntity> {
+  public patchSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId', new ParseUUIDPipe()) systemId: string, @Param('symbolId', new ParseUUIDPipe()) symbolId: string, @Body(new ValidationPipe({ forbidNonWhitelisted: true, transform: true, whitelist: true })) editSymbolPayload: EditSymbolPayload): Promise<SymbolEntity> {
     return this.symbolWriteService.update(sessionUserId, systemId, symbolId, editSymbolPayload);
   }
 
   @Post()
   @UseGuards(JwtGuard)
   @UseInterceptors(ClassSerializerInterceptor)
-  public postSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId') systemId: string, @Body(new ValidationPipe({ transform: true })) newSymbolPayload: NewSymbolPayload): Promise<SymbolEntity> {
+  public postSymbol(@SessionUser('id') sessionUserId: string, @Param('systemId', new ParseUUIDPipe()) systemId: string, @Body(new ValidationPipe({ forbidNonWhitelisted: true, transform: true, whitelist: true })) newSymbolPayload: NewSymbolPayload): Promise<SymbolEntity> {
     return this.symbolWriteService.create(sessionUserId, systemId, newSymbolPayload);
   }
 };
