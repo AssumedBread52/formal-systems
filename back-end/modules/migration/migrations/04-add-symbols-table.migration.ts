644e59190002e3e100035479b0d29eefa782d4e6
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-15 19:21:03 -0500

    Update the name of the fourth migration script

--- back-end/modules/migration/migrations/04-add-symbols-table.migration.ts
+++ back-end/modules/migration/migrations/04-add-symbols-table.migration.ts
@@ -11,13 +11,13 @@ export class AddSymbolsTable0000000000004 implements MigrationInterface {
         type        symbol_type   NOT NULL,
         content     VARCHAR(250)  NOT NULL,
 
-        CONSTRAINT symbols_primary_key            PRIMARY KEY (id),
-        CONSTRAINT symbols_system_foreign_key     FOREIGN KEY (system_id) REFERENCES systems(id),
-        CONSTRAINT symbols_name_nonempty          CHECK(LENGTH(name) > 0),
-        CONSTRAINT symbols_description_nonempty   CHECK(LENGTH(description) > 0),
-        CONSTRAINT symbols_content_nonempty       CHECK(LENGTH(content) > 0),
-        CONSTRAINT symbols_system_id_name_unique  UNIQUE(system_id, name),
-        CONSTRAINT symbols_id_system_id_unique    UNIQUE(id, system_id)
+        CONSTRAINT symbols_primary_key          PRIMARY KEY (id),
+        CONSTRAINT symbols_system_foreign_key   FOREIGN KEY (system_id) REFERENCES systems(id),
+        CONSTRAINT symbols_name_nonempty        CHECK(LENGTH(name) > 0),
+        CONSTRAINT symbols_description_nonempty CHECK(LENGTH(description) > 0),
+        CONSTRAINT symbols_content_nonempty     CHECK(LENGTH(content) > 0),
+        CONSTRAINT symbols_system_name_unique   UNIQUE(system_id, name),
+        CONSTRAINT symbols_id_system_unique     UNIQUE(system_id, id)
       );
     `);
   }
