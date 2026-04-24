d0d5edc07c2911461b2e66fba678357b06906081
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-24 06:32:33 -0500

    Expose expression searching via GraphQL

--- back-end/modules/expression/resolvers/expression.resolver.ts
+++ back-end/modules/expression/resolvers/expression.resolver.ts
@@ -1,6 +1,8 @@
 import { ExpressionEntity } from '@/expression/entities/expression.entity';
+import { PaginatedExpressionsPayload } from '@/expression/payloads/paginated-expressions.payload';
+import { SearchExpressionsPayload } from '@/expression/payloads/search-expressions.payload';
 import { ExpressionReadService } from '@/expression/services/expression-read.service';
-import { ParseUUIDPipe } from '@nestjs/common';
+import { ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
@@ -13,5 +15,12 @@ export class ExpressionResolver {
   })
   public expression(@Args('systemId', new ParseUUIDPipe()) systemId: string, @Args('expressionId', new ParseUUIDPipe()) expressionId: string): Promise<ExpressionEntity> {
     return this.expressionReadService.selectById(systemId, expressionId);
+  }
+
+  @Query((): typeof PaginatedExpressionsPayload => {
+    return PaginatedExpressionsPayload;
+  })
+  public expressions(@Args('systemId', new ParseUUIDPipe()) systemId: string, @Args('filters', new ValidationPipe({ forbidNonWhitelisted: true, transform: true, whitelist: true })) searchExpressionsPayload: SearchExpressionsPayload): Promise<PaginatedExpressionsPayload> {
+    return this.expressionReadService.searchExpressions(systemId, searchExpressionsPayload);
   }
 };
