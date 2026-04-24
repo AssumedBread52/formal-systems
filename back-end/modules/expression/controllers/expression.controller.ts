60812dead7d4d4aeef4a1203dd22e559c3da6985
Antonio Sanchez <asanchez16756@gmail.com>
2026-04-24 06:29:20 -0500

    Expose expression searching via REST

--- back-end/modules/expression/controllers/expression.controller.ts
+++ back-end/modules/expression/controllers/expression.controller.ts
@@ -1,6 +1,8 @@
 import { ExpressionEntity } from '@/expression/entities/expression.entity';
+import { PaginatedExpressionsPayload } from '@/expression/payloads/paginated-expressions.payload';
+import { SearchExpressionsPayload } from '@/expression/payloads/search-expressions.payload';
 import { ExpressionReadService } from '@/expression/services/expression-read.service';
-import { ClassSerializerInterceptor, Controller, Get, Param, ParseUUIDPipe, SerializeOptions, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Param, ParseUUIDPipe, Query, SerializeOptions, UseInterceptors, ValidationPipe } from '@nestjs/common';
 
 @Controller('system/:systemId/expression')
 @SerializeOptions({
@@ -10,6 +12,12 @@ import { ClassSerializerInterceptor, Controller, Get, Param, ParseUUIDPipe, Seri
 })
 export class ExpressionController {
   public constructor(private readonly expressionReadService: ExpressionReadService) {
+  }
+
+  @Get()
+  @UseInterceptors(ClassSerializerInterceptor)
+  public getExpressions(@Param('systemId', new ParseUUIDPipe()) systemId: string, @Query(new ValidationPipe({ forbidNonWhitelisted: true, transform: true, whitelist: true })) searchExpressionsPayload: SearchExpressionsPayload): Promise<PaginatedExpressionsPayload> {
+    return this.expressionReadService.searchExpressions(systemId, searchExpressionsPayload);
   }
 
   @Get(':expressionId')
