982e2bb172953733c7cea2316506dc5290a0c338
AssumedBread52 <asanchez16756@gmail.com>
2026-04-30 21:08:47 -0500

    Should match table name

--- back-end/modules/migration/migrations/10-add-statement-distinct-variable-pairs-table.migration.ts
+++ back-end/modules/migration/migrations/10-add-statement-distinct-variable-pairs-table.migration.ts
@@ -9,17 +9,17 @@ export class AddStatementDistinctVariablePairsTable0000000000010 implements Migr
         variable_symbol_1_id  UUID  NOT NULL,
         variable_symbol_2_id  UUID  NOT NULL,
 
-        CONSTRAINT statement_distinct_variables_primary_key                   PRIMARY KEY (statement_id, variable_symbol_1_id, variable_symbol_2_id),
-        CONSTRAINT statement_distinct_variables_system_foreign_key            FOREIGN KEY (system_id) REFERENCES systems(id),
-        CONSTRAINT statement_distinct_variables_statement_foreign_key         FOREIGN KEY (system_id, statement_id) REFERENCES statements(system_id, id),
-        CONSTRAINT statement_distinct_variables_variable_symbol_1_foreign_key FOREIGN KEY (system_id, variable_symbol_1_id) REFERENCES symbols(system_id, id),
-        CONSTRAINT statement_distinct_variables_variable_symbol_2_foreign_key FOREIGN KEY (system_id, variable_symbol_2_id) REFERENCES symbols(system_id, id),
-        CONSTRAINT statement_distinct_variables_order                         CHECK(variable_symbol_1_id < variable_symbol_2_id)
+        CONSTRAINT statement_distinct_variable_pairs_primary_key                   PRIMARY KEY (statement_id, variable_symbol_1_id, variable_symbol_2_id),
+        CONSTRAINT statement_distinct_variable_pairs_system_foreign_key            FOREIGN KEY (system_id) REFERENCES systems(id),
+        CONSTRAINT statement_distinct_variable_pairs_statement_foreign_key         FOREIGN KEY (system_id, statement_id) REFERENCES statements(system_id, id),
+        CONSTRAINT statement_distinct_variable_pairs_variable_symbol_1_foreign_key FOREIGN KEY (system_id, variable_symbol_1_id) REFERENCES symbols(system_id, id),
+        CONSTRAINT statement_distinct_variable_pairs_variable_symbol_2_foreign_key FOREIGN KEY (system_id, variable_symbol_2_id) REFERENCES symbols(system_id, id),
+        CONSTRAINT statement_distinct_variable_pairs_order                         CHECK(variable_symbol_1_id < variable_symbol_2_id)
       );
     `);
   }
 
   public down(queryRunner: QueryRunner): Promise<any> {
-    return queryRunner.query('DROP TABLE statement_distinct_variables;');
+    return queryRunner.query('DROP TABLE statement_distinct_variable_pairs;');
   }
 };
