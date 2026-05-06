c7e3593284b27f47408e3c0a7f9494a8226195a5
AssumedBread52 <asanchez16756@gmail.com>
2026-05-05 20:27:48 -0500

    Test that new relationship resolves correctly

--- back-end/modules/statement/tests/statement-relations.test.ts
+++ back-end/modules/statement/tests/statement-relations.test.ts
@@ -3,6 +3,7 @@ import { createTestApp } from '@/common/tests/helpers/create-test-app';
 import { findByMock } from '@/common/tests/mocks/find-by.mock';
 import { findOneByMock } from '@/common/tests/mocks/find-one-by.mock';
 import { getOrThrowMock } from '@/common/tests/mocks/get-or-throw.mock';
+import { ExpressionEntity } from '@/expression/entities/expression.entity';
 import { StatementEntity } from '@/statement/entities/statement.entity';
 import { SystemEntity } from '@/system/entities/system.entity';
 import { HttpStatus } from '@nestjs/common';
@@ -33,6 +34,13 @@ describe('Statement Relations', (): void => {
       name: 'TestSystem1',
       description: 'Test System 1'
     }, SystemEntity);
+    const expression = validatePayload({
+      id: expressionId,
+      systemId,
+      canonical: [
+        '7bde3313-f751-42f0-8d89-88c4ab394282'
+      ]
+    }, ExpressionEntity);
     const statement = validatePayload({
       id: statementId,
       systemId,
@@ -42,20 +50,28 @@ describe('Statement Relations', (): void => {
     }, StatementEntity);
 
     findBy.mockResolvedValueOnce([
+      expression
+    ]);
+    findBy.mockResolvedValueOnce([
       system
     ]);
     findOneBy.mockResolvedValueOnce(statement);
 
     const response = await request(app.getHttpServer()).post('/graphql').send({
-      query: 'query ($systemId: String!, $statementId: String!) { statement(systemId: $systemId, statementId: $statementId) { id systemId assertionExpressionId name description system { id ownerUserId name description } } }',
+      query: 'query ($systemId: String!, $statementId: String!) { statement(systemId: $systemId, statementId: $statementId) { id systemId assertionExpressionId name description system { id ownerUserId name description } assertion { id systemId canonical } } }',
       variables: {
         systemId,
         statementId
       }
     });
 
-    expect(findBy).toHaveBeenCalledTimes(1);
+    expect(findBy).toHaveBeenCalledTimes(2);
     expect(findBy).toHaveBeenNthCalledWith(1, {
+      id: In([
+        expressionId
+      ])
+    });
+    expect(findBy).toHaveBeenNthCalledWith(2, {
       id: In([
         systemId
       ])
@@ -74,7 +90,8 @@ describe('Statement Relations', (): void => {
           assertionExpressionId: expressionId,
           name,
           description,
-          system: instanceToPlain(system)
+          system: instanceToPlain(system),
+          assertion: instanceToPlain(expression)
         }
       }
     });
