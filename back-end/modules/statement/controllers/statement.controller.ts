c38d8754f55fff84334aa3ef5edaee1a9ca580d0
AssumedBread52 <asanchez16756@gmail.com>
2026-05-11 17:45:39 -0500

    Add serialization decorator

--- back-end/modules/statement/controllers/statement.controller.ts
+++ back-end/modules/statement/controllers/statement.controller.ts
@@ -1,6 +1,6 @@
 import { StatementEntity } from '@/statement/entities/statement.entity';
 import { StatementReadService } from '@/statement/services/statement-read.service';
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Param, ParseUUIDPipe, UseInterceptors } from '@nestjs/common';
 
 @Controller('system/:systemId/statement')
 export class StatementController {
@@ -8,6 +8,7 @@ export class StatementController {
   }
 
   @Get(':statementId')
+  @UseInterceptors(ClassSerializerInterceptor)
   public getById(@Param('systemId', new ParseUUIDPipe()) systemId: string, @Param('statementId', new ParseUUIDPipe()) statementId: string): Promise<StatementEntity> {
     return this.statementReadService.selectById(systemId, statementId);
   }
