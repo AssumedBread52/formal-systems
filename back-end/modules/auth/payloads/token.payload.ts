560a752b7e6a9b2f933e706c2efc4c93d6e0ee4e
AssumedBread52 <asanchez16756@gmail.com>
2026-05-20 21:09:46 -0500

    Remove default from the token payload model

--- back-end/modules/auth/payloads/token.payload.ts
+++ back-end/modules/auth/payloads/token.payload.ts
@@ -2,11 +2,11 @@ import { IsInt, IsPositive, IsUUID } from 'class-validator';
 
 export class TokenPayload {
   @IsUUID()
-  public readonly userId: string = '';
+  public readonly userId!: string;
   @IsInt()
   @IsPositive()
-  public readonly iat: number = 0;
+  public readonly iat!: number;
   @IsInt()
   @IsPositive()
-  public readonly exp: number = 0;
+  public readonly exp!: number;
 };
