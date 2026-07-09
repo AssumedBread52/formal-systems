ce445099d3edd0a4ed686d3a4201da4c9e0a92aa
AssumedBread52 <asanchez16756@gmail.com>
2026-07-08 20:59:38 -0500

    Change mock value to adapt to new migrations

--- back-end/modules/common/tests/mocks/query.mock.ts
+++ back-end/modules/common/tests/mocks/query.mock.ts
@@ -1,4 +1,8 @@
 import { buildQueryResult } from '@/common/tests/helpers/build-query-result';
+import migrations from '@/migration/migrations';
+import { BaseMigration } from '@/migration/migrations/base.migration';
+import { Type } from '@nestjs/common';
+import { Migration } from 'typeorm';
 import { PostgresDriver } from 'typeorm/driver/postgres/PostgresDriver';
 import { PostgresQueryRunner } from 'typeorm/driver/postgres/PostgresQueryRunner';
 
@@ -35,78 +39,13 @@ export const queryMock = (): jest.SpyInstance<Promise<any>, [query: string, para
         commit_action: ''
       }
     ]);
-    query.mockResolvedValueOnce(buildQueryResult([
-      {
-        id: 1,
-        timestamp: 1,
-        name: 'AddUsersTable0000000000001'
-      },
-      {
-        id: 2,
-        timestamp: 2,
-        name: 'AddSystemsTable0000000000002'
-      },
-      {
-        id: 3,
-        timestamp: 3,
-        name: 'AddSymbolType0000000000003'
-      },
-      {
-        id: 4,
-        timestamp: 4,
-        name: 'AddSymbolsTable0000000000004'
-      },
-      {
-        id: 5,
-        timestamp: 5,
-        name: 'AddExpressionsTable0000000000005'
-      },
-      {
-        id: 6,
-        timestamp: 6,
-        name: 'AddExpressionTokensTable0000000000006'
-      },
-      {
-        id: 7,
-        timestamp: 7,
-        name: 'AddStatementsTable0000000000007'
-      },
-      {
-        id: 8,
-        timestamp: 8,
-        name: 'AddHypothesisType0000000000008'
-      },
-      {
-        id: 9,
-        timestamp: 9,
-        name: 'AddStatementHypothesesTable0000000000009'
-      },
-      {
-        id: 10,
-        timestamp: 10,
-        name: 'AddStatementDistinctVariablePairsTable0000000000010'
-      },
-      {
-        id: 11,
-        timestamp: 11,
-        name: 'AddProofsTable0000000000011'
-      },
-      {
-        id: 12,
-        timestamp: 12,
-        name: 'AddProofStepsTable0000000000012'
-      },
-      {
-        id: 13,
-        timestamp: 13,
-        name: 'AddProofStepSubstitutionsTable0000000000013'
-      },
-      {
-        id: 14,
-        timestamp: 14,
-        name: 'AddProofStepReferencesTable0000000000014'
-      }
-    ]));
+    query.mockResolvedValueOnce(buildQueryResult(Object.values(migrations).map((MigrationClass: Type<BaseMigration>, index: number): Migration => {
+      return {
+        id: index + 1,
+        timestamp: index + 1,
+        name: MigrationClass.name
+      };
+    })));
     query.mockResolvedValueOnce([]);
     queryRunnerConnect.mockResolvedValueOnce(undefined);
   });
