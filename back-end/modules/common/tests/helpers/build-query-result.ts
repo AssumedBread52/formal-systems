57a35ebb40c5b086f9ab512fcb3803f0cb30629d
AssumedBread52 <asanchez16756@gmail.com>
2026-07-08 20:58:54 -0500

    Type as object

--- back-end/modules/common/tests/helpers/build-query-result.ts
+++ back-end/modules/common/tests/helpers/build-query-result.ts
@@ -1,7 +1,7 @@
 import { QueryResult } from 'typeorm';
 
-export const buildQueryResult = (payload: Record<string, unknown>[]): QueryResult => {
-  const queryResult = new QueryResult();
+export const buildQueryResult = (payload: Object[]): QueryResult => {
+  const queryResult = new QueryResult<Object>();
 
   queryResult.raw = payload;
   queryResult.records = payload;
